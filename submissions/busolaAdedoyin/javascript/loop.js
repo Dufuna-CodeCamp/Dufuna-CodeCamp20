7e334c460888a6d8929517ff495c7ccee7112ade
elizabethbusola <lizzykemmie@gmail.com>
2020-08-24 18:20:26 +0100

    loop and conditional

---
+++ submissions/busolaAdedoyin/javascript/loop.js
@@ -0,0 +1,17 @@
+var x = 123 -7;
+var i;
+
+for(i = 1; i < x; i++){
+	if(i % 3 == 0 && i % 5 == 0){
+		console.log('Software Developer');
+	}
+	else if ( i % 3 == 0) {
+		console.log('Software');
+	}
+	else if (i % 5 == 0){
+		console.log('Developer');
+	}
+	else {
+		console.log(i);
+	}
+}
