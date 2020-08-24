91756d0acddd5190c446e74bf2e6420081469c0e
elizabethbusola <lizzykemmie@gmail.com>
2020-08-24 11:43:50 +0100

    changes made

--- submissions/busolaAdedoyin/javascript/index.js
+++ submissions/busolaAdedoyin/javascript/index.js
@@ -1,17 +1,16 @@
 
-var x = "";
 var productList = [
-    Fabric1  = {
+    {
         productName: 'Chiffon Fabric',
         imageUrl: 'img/chiffonfab.jpg',
         productPrice: 4500,
     },
-    Fabric2  = {
+     {
         productName: 'Lace Fabric',
         imageUrl: 'img/lacefab.jpg',
         productPrice: 12500,
     },
-    Fabric3 = {
+    {
         productName: 'Polish Fabric',
         imageUrl:'img/polishfab.jpg',
         productPrice:8750
@@ -21,7 +20,6 @@ var productList = [
     for(var  x in productList[i]){
         console.log(productList[i]['productName'] + " with " + productList[i]['imageUrl'] + " is sold for  " + productList[i]['productPrice'])
     };
-
  }
 
 
