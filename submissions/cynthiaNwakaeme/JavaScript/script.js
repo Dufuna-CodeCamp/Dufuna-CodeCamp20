aa078c19e2d0394b36050f17e6c55795dbb96c4e
Nwakaeme Cynthia Ebere <nwakaemecynthia@gmail.com>
2020-08-25 22:36:07 +0100

    [18] The Software Developer conditions

--- submissions/cynthiaNwakaeme/JavaScript/script.js
+++ submissions/cynthiaNwakaeme/JavaScript/script.js
@@ -35,3 +35,24 @@ for (let product = 0; product < productList.length; product++) {
 //     product++;
 // }
 // while (product < productList.length);
+
+// TASk 2
+const diff = 123 - 7;
+var  count = 1;
+
+while (count < diff) {
+    switch (true) {
+        case ((count % 3 === 0) && (count % 5 === 0)):
+            console.log("Software Developer");
+            break;
+        case ((count % 5) === 0):
+            console.log("Developer");
+            break;
+        case ((count % 3) === 0):
+            console.log("Software");
+            break;
+        default:
+            console.log(count);
+    }
+    count++;
+};
