d62224bed45492bc029d2d06f44daa107dab9f9a
faithakinbobola <faithakinbobola2@gmail.com>
2020-08-28 13:30:05 +0100

    used conditionals and loop for the javascript

---
+++ submissions/akinbobolaFaith/javaScript/conditionals.js
@@ -0,0 +1,15 @@
+let w = 0,
+    x = 7,
+    y = 123;
+z = (y - x)
+
+numberList: while (w < z) {
+    w += 1;
+    if ((w % 3 === 0) && (w % 5 === 0)) {
+        console.log("Software Developer");
+    } else if (w % 3 === 0) {
+        console.log("Software");
+    } else if (w % 5 === 0) {
+        console.log("Developer");
+    }
+}
