7924c748383120449fc7a9983465f87c3d93c871
faithakinbobola <faithakinbobola2@gmail.com>
2020-08-31 10:04:12 +0100

    made some requested changes

--- submissions/akinbobolaFaith/javaScript/conditionals.js
+++ submissions/akinbobolaFaith/javaScript/conditionals.js
@@ -1,9 +1,10 @@
-let w = 0,
-    x = 7,
-    y = 123;
-z = (y - x)
+let w = 0;
+let x = 7;
+let y = 123;
+let z = (y - x);
 
-numberList: while (w < z) {
+
+while (w < z) {
     w += 1;
     if ((w % 3 === 0) && (w % 5 === 0)) {
         console.log("Software Developer");
@@ -11,5 +12,7 @@ numberList: while (w < z) {
         console.log("Software");
     } else if (w % 5 === 0) {
         console.log("Developer");
+    } else {
+        console.log(w);
     }
 }
