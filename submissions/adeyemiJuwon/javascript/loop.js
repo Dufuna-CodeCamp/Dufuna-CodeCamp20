acda48d85b7970eb35d04db5fd706faa1eb23283
Adeyemi juwon <samju7778@gmail.com>
2020-09-02 10:11:33 +0100

    change the conditional loop

--- submissions/adeyemiJuwon/javascript/loop.js
+++ submissions/adeyemiJuwon/javascript/loop.js
@@ -4,14 +4,14 @@ let a = 123;
 
 for (var i = 1; i < c; i++){
 
-    if ( i % 3 == 0) {
+    if (i % 3 == 0 && i % 5 == 0){
+        console.log('Software Developr');
+    
+    }else if ( i % 3 == 0) {
         console.log('Software');
     
     }else if (i % 5 == 0){
         console.log('Developer');
-    
-    }else if (i % 3 == 0 && i % 5 == 0){
-        console.log('Software Developer');
     
     }else{
         console.log(i);
