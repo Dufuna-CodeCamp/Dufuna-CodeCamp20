292b8558250e5bada3da6e98ea2444c72a8dc2b7
Adeyemi juwon <samju7778@gmail.com>
2020-09-03 12:14:11 +0100

    typo correcton for developer

--- submissions/adeyemiJuwon/javascript/loop.js
+++ submissions/adeyemiJuwon/javascript/loop.js
@@ -5,7 +5,7 @@ let a = 123;
 for (var i = 1; i < c; i++){
 
     if (i % 3 == 0 && i % 5 == 0){
-        console.log('Software Developr');
+        console.log('Software Developer');
     
     }else if ( i % 3 == 0) {
         console.log('Software');
