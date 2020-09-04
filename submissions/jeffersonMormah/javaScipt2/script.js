db5d8a1170546b3a89b45f598ab03d9484392ca1
Jeffbuchi <buchisam32@gmail.com>
2020-09-04 11:01:40 +0100

    reinitializing in my conditional statement

--- submissions/jeffersonMormah/javaScipt2/script.js
+++ submissions/jeffersonMormah/javaScipt2/script.js
@@ -1,18 +1,18 @@
-var a = 1
+
 var b = 123;
 var c = 7;
-var d = b - a;
+var d = b - c;
 
-for (var a = 1; a < d; a++){
-  if ((a % 3) === 0) {
-      console.log("Software")
-  } else if ((a % 5) === 0){
-      console.log("Developer")
-  } else if ((a % 3) && (a % 5)) {
-      console.log("Software Developer")
-  } else {
-      console.log(a);
-  }
+for (var a = 1; a <= d; a++){
+if ((a % 3 === 0) && (a % 5 === 0)){
+    console.log ("Software Developer");
+} else if ((a % 5) === 0) {
+    console.log("Developer");
+} else if ((a % 3) === 0) {
+    console.log("Software");
+} else {
+    console.log(a);
+}
 };
 
 
