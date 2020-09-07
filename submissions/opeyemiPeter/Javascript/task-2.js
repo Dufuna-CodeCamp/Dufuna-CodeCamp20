bdcf4d0d94fa8826dbb523cec11784f5116acfe8
opeyemipeter1759 <63324192+opeyemipeter1759@users.noreply.github.com>
2020-09-07 07:27:14 -0700

    task 2

--- submissions/opeyemiPeter/Javascript/task-2.js
+++ submissions/opeyemiPeter/Javascript/task-2.js
@@ -3,12 +3,11 @@ var  num1 = 1;
 
 while (num1 < diff) {
     switch (true) {
-        
+      case ((num1 % 3) === 0):
+            console.log("Software");
+            break;
         case ((num1 % 5) === 0):
             console.log("Developer");
-            break;
-        case ((num1 % 3) === 0):
-            console.log("Software");
             break;
         case ((num1 % 3 === 0) && (count % 5 === 0)):
               console.log("Software Developer");
