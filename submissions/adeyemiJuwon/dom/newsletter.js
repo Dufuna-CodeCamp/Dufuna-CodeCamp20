436c183e6bb8d6969b1c0708624633936be99b9c
Adeyemi juwon <samju7778@gmail.com>
2020-09-07 10:54:16 +0100

    correction to newsletter js

--- submissions/adeyemiJuwon/dom/newsletter.js
+++ submissions/adeyemiJuwon/dom/newsletter.js
@@ -1,35 +1,22 @@
 let submitForm = document.getElementById('newsletter');
 let email = document.getElementById('email');
+let emailRegex = /^\w+[\w-\.]*\@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/;
 
 
-
-function required(field, event){
-    if(field.value == ""){
-        document.querySelector(".error").innerHTML = "Please enter your Email Address";
-        event.preventDefault();
-        return false;
-    }else{
-        document.querySelector(".error").innerHTML = "";
-        return true;
-    }
-    }
-    
-    function emailFieldValidation(event){
-        emailRegex = /^\w+[\w-\.]*\@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/;
-        if (!email.value.match(emailRegex)){
+    function validateForm(e){
+        if(!email.value.match(emailRegex)){
             document.querySelector(".error").innerHTML = "Please enter a valid email";
             event.preventDefault();
             return false;
-        } else{
-            document.querySelector(".error").innerHTML =  "";
+        }
+         else if(email.value === ""){
+            document.querySelector(".error").innerHTML = "Please enter your Email Address";
+            event.preventDefault();
+            return false;
+        }else{
+            document.querySelector(".error").innerHTML = "";
             return true;
         }
-    }
-
-    function validateForm(e){
-        required(email, e); 
-        emailFieldValidation(e);
        
-        return true;
     }
     submitForm.addEventListener('submit', validateForm);
