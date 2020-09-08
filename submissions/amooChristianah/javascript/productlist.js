dc9f5e12b516d698de45578bdaf7515096c0c488
pelumi454 <amoochristianah454@gmail.com>
2020-09-08 14:25:13 +0100

    First commit

--- submissions/amooChristianah/javascript/productlist.js
+++ submissions/amooChristianah/javascript/productlist.js
@@ -20,3 +20,23 @@ const productList = [
 productList.forEach((product) => {
     console.log(product.productName + ' with '+ product.imageUrl + ' is sold for ' + product.productPrice)
   });
+
+  /* TASK 2 */
+
+  var x = 123 - 7;
+
+
+for(var i = 1; i < x; i++){
+	if(i % 3 == 0 && i % 5 == 0){
+		console.log('Software Developer');
+	}
+	else if ( i % 3 == 0) {
+		console.log('Software');
+	}
+	else if (i % 5 == 0){
+		console.log('Developer');
+	}
+	else {
+		console.log(i);
+	}
+}
