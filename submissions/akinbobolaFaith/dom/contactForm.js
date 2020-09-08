0429efce84207939175795eaf3115ccd9bd30f47
faithakinbobola <faithakinbobola2@gmail.com>
2020-09-08 10:51:51 +0100

    i modified contactform.js and made necessary changes

--- submissions/akinbobolaFaith/dom/contactForm.js
+++ submissions/akinbobolaFaith/dom/contactForm.js
@@ -5,29 +5,23 @@ let sendForm = document.querySelector('.informate'),
     message = document.getElementById('input')
 ;
 
-function fullNameValidation(event) {
-    let fullNameRegex = /^([a-zA-Z0-9]+|[a-zA-Z0-9]+\s{1}[a-zA-Z0-9]{1,}|[a-zA-Z0-9]+\s{1}[a-zA-Z0-9]{3,}\s{1}[a-zA-Z0-9]{1,})$/;
-    if(fullName.value === '') {
-        fullName.nextElementSibling.innerHTML = "Please enter your name";
+function inputValidation(event, input, inputRegex, innerHTML, innerHTMLTwo) {
+    if (input.value === '') {
+        input.nextElementSibling.innerHTML = innerHTML;
         event.preventDefault();
         return false;
-    } else if (!fullName.value.match(fullNameRegex)) {
-        fullName.nextElementSibling.innerHTML = "Please enter your name";
+    } else if (!input.value.match(inputRegex)) {
+        input.nextElementSibling.innerHTML = innerHTMLTwo;
         event.preventDefault();
         return false;
     } else {
-        fullName.nextElementSibling.innerHTML = "";
-        return true;
+        input.nextElementSibling.innerHTML = "";
     }
 }
 
-function fieldValidation(event, field, fieldRegex, innerHTML, innerHTMLTwo) {
+function fieldValidation(event, field, innerHTML) {
     if (field.value === '') {
         field.nextElementSibling.innerHTML = innerHTML;
-        event.preventDefault();
-        return false;
-    } else if (!field.value.match(fieldRegex)) {
-        field.nextElementSibling.innerHTML = innerHTMLTwo || "";
         event.preventDefault();
         return false;
     } else {
@@ -35,14 +29,15 @@ function fieldValidation(event, field, fieldRegex, innerHTML, innerHTMLTwo) {
     }
 }
 
-function informate(e) {
-    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,6})+$/;
-    const fieldRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[._$,/;:+=~`'"<>^#?!@$%^&*-])$/;
 
-    fullNameValidation(e);
-    fieldValidation(e, email, emailRegex, "Please enter your Email Address", "Please enter a valid mail");
-    fieldValidation(e, subject, fieldRegex, "Please enter a message subject");
-    fieldValidation(e, message, fieldRegex, "Please enter your Message");
+function informate(e) {
+    const fullNameRegex = /^([a-zA-Z0-9]+|[a-zA-Z0-9]+\s{1}[a-zA-Z0-9]{1,}|[a-zA-Z0-9]+\s{1}[a-zA-Z0-9]{3,}\s{1}[a-zA-Z0-9]{1,})$/;
+    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,6})+$/;
+
+    inputValidation(e, fullName, fullNameRegex, "Please enter your name", "Please enter your name");
+    inputValidation(e, email, emailRegex, "Please enter your Email Address", "Please enter a valid mail");
+    fieldValidation(e, subject, "Please enter a message subject");
+    fieldValidation(e, message, "Please enter your Message");
     return true;
 }
 
