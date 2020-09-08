d330ab54e32c2f206037b3ff27661df96a621b9e
tomike19 <kolajoelizabeth@gmail.com>
2020-09-07 23:01:54 -0700

    commit

--- submissions/kolajoTomike/Javascript/app.js
+++ submissions/kolajoTomike/Javascript/app.js
@@ -1,28 +1,23 @@
 var productList=[
 {
   productName:"STRAWBERRY ICE-CREAM SANDWICHES",
-  ImageUrl:"../html/img/hand.png",
+  imageUrl:"../html/img/hand.png",
   productPrice:"N300.00",
 
 },
 
 {
   productName:"MATCHA BANANA ICE CREAM",
-  ImageUrl:"../html/img/ice4.png",
+  imageUrl:"../html/img/ice4.png",
   productPrice:"N500.00",
 },
 
 {
   productName:"COCONUT MILK AND AVOCADO ICE CREAM",
-  ImageUrl:"../html/img/ice3.png",
+  imageUrl:"../html/img/ice3.png",
   productPrice:"N900.00",
 },
 ];
  productList.forEach((product)=>{
-    console.log(product.productName + ' with '+ product.ImageUrl + ' is sold for ' + product.productPrice)
+    console.log(product.productName + ' with '+ product.imageUrl + ' is sold for ' + product.productPrice)
  });
-
-
-
-
-
