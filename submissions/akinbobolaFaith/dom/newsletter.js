cfcd934e7835d0ef09d1abafe1ed7becb0aa53e6
faithakinbobola <faithakinbobola2@gmail.com>
2020-09-09 12:45:51 +0100

    corrected the typo in the error message

--- submissions/akinbobolaFaith/dom/newsletter.js
+++ submissions/akinbobolaFaith/dom/newsletter.js
@@ -5,7 +5,7 @@ let submitForm = document.querySelector('.newsletterSignup'),
 function newsletterValidation (event) {
     let newsletterRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,6})+$/;
     if (newsletter.value === '') {
-        newsletter.nextElementSibling.innerHTML = "Please enetr your Email Address";
+        newsletter.nextElementSibling.innerHTML = "Please enter your Email Address";
         event.preventDefault();
     } else if (!newsletter.value.match(newsletterRegex)) {
         newsletter.nextElementSibling.innerHTML = "Please enter a valid mail";
