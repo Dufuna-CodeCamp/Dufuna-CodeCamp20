b01f331d9cef2fbdc26e387a98abaeccc75b5549
Toluwalase <tolu@cottacush.com>
2020-09-09 14:49:18 +0100

    errors files to test

---
+++ submissions/javscript.js
@@ -0,0 +1,22 @@
+
+var productList = [
+    {
+        productName: 'Chiffon Fabric'
+        ImageUrl: 'img/chiffonfab.jpg',
+        productPrices: 4500,
+    },
+     {
+        productName: 'Lace Fabric',
+        imageUrl: 'img/lacefab.jpg'
+        productPrices: 12500,
+    },
+    {
+        productName: 'Polish Fabric'
+        imageUrl:'img/polishfab.jpg',
+        "productPrices":
+    }
+];
+    for(var in productList){
+        console.log(productList[x]['productName'] + " with " + productList[x]['imageUrl'] + " is sold for  " + productList[x]['productPrice'])
+    };
+ 
