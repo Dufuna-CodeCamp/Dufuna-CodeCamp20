c22dbecb0a0b864880c60e0bda5af41af203b9b0
opeyemipeter1759 <63324192+opeyemipeter1759@users.noreply.github.com>
2020-09-11 02:40:07 -0700

    validation for newsletter form

--- submissions/opeyemiPeter/DOM/script.js
+++ submissions/opeyemiPeter/DOM/script.js
@@ -3,18 +3,19 @@
 
 const submitForm = document.getElementById('newsletter');
 const email = document.getElementById('email');
-const emailRegex = /^\w+[\w-\.]*\@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/;
+const emailRegex = "/^\w+[\w-\.]*\@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/";
 
 
     function validateForm(e){
-        e.preventDefault();
         if(email.value === ""){
             document.querySelector(".err").innerHTML = "Please enter your Email Address";
+            e.preventDefault();
             return false;
         }
         
          if(!email.value.match(emailRegex)){
             document.querySelector(".err").innerHTML = "Please enter a valid email";
+            e.preventDefault();
             return false;
         }
          else{
