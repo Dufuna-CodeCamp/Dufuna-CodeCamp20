4cdabdfadc800377ed9fd9f2595f720f040b11ac
Jeffbuchi <buchisam32@gmail.com>
2020-09-14 11:57:42 +0100

    writing a function to validate form signup

--- submissions/jeffersonMormah/domManipulation/signup.js
+++ submissions/jeffersonMormah/domManipulation/signup.js
@@ -1,95 +1,65 @@
-const submitBtn = document.getElementById("submit");
+let signupForm = document.getElementById("form-signup");
+let firstName = document.getElementById("FirstName");
+let lastName = document.getElementById("LastName");
+let phoneNum = document.getElementById("Phone");
+let emailAddress = document.getElementById("email");
+let passWord = document.getElementById("Password");
+let confPassword = document.getElementById("conf-password");
+let nameCheck = /^[A-za-z]/;
+let checkNum =  /^[\+0-9]{8,15}$/;
+let validEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,6})+$/;
+let pwdVal = /(?=.*\d)(?=.*[a-z])(?=.*[a-zA-Z]).{6,}$/;
+let firstNameInput = "Please Enter Your First Name";
+let firstNameError = "It can contain only letters";
+let lastNameInput = "Please Enter Your Last Name";
+let LastNameError = "Letters Only";
+let phoneInput = "Please Enter Your Phone Number";
+let phoneError = "Please enter a  valid phone number";
+let emailInput = "Please enter your phone number";
+let emailError = "Please enter a valid email address";
+let passWordInput = "Please Enter your password";
+let passwordError = "Your Password should have a minimum of 6 characters, 1 capiyal letter, 1 special character and 1 number";
 
-const validation = (e) => {
-    
-    const firstName = document.getElementById("FirstName");
-    const lastName = document.getElementById("LastName");
-    const phoneNum = document.getElementById("Phone");
-    const emailAddress = document.getElementById("email");
-    const passWord = document.getElementById("Password");
-    const confPassword = document.getElementById("conf-password");
-
- 
-
-    if (firstName.value === "") {
-        firstName.nextElementSibling.innerHTML = "Please Enter Your First Name";
+const fieldVal = (field, allRegex, output, result, e) => {
+    if (field.value === "") {
+        field.nextElementSibling.innerHTML = output;
         e.preventDefault();
         return false;
-    } 
-
-    if (lastName.value === "") {
-        lastName.nextElementSibling.innerHTML = "Please Enter Your Last Name";
-        e.preventDefault();
-        return false;
+    } else if (!field.value.match(allRegex)) {
+       field.nextElementSibling.innerHTML = result;
+       e.preventDefault();
+       return false;
+    } else {
+        field.nextElementSibling.innerHTML = "";
+        return true;
     }
-
-    if (phoneNum.value === "") {
-        phoneNum.nextElementSibling.innerHTML = "Please Enter Your Phone Number";
-        phoneNum.focus();
-        return false;
-    } 
-    if (!checkNum(phoneNum.value)) {
-        phoneNum.nextElementSibling.innerHTML = "Please Enter a valid Phone Number";
-        e.preventDefault();
-        return false;
     }
-    if (emailAddress.value === "") {
-        emailAddress.nextElementSibling.innerHTML = "Please Enter Your Email Address";
-        emailAddress.focus();
-        return false;
-    }
-
-    if (!validEmail(emailAddress.value)) {
-        emailAddress.nextElementSibling.innerHTML = "Please Enter a Valid Email address";
-        e.preventDefault();
-        return false;
-    }
-
-    if (passWord.value === "") {
-        passWord.nextElementSibling.innerHTML = "Please Enter Your Password";
-        e.preventDefault();
-        return false;
-    }
-
-    if(!pwdVal(passWord.value)) {
-        passWord.nextElementSibling.innerHTML = "Your password should have a minimum of 6 characters, 1 capital letter, 1 special character and 1 number."
-        e.preventDefault();
-        return false;
-    }
-
-    if(confPassword.value === "") {
+const confPwdVal = (e) => {
+    if (confPassword.value === "") {
         confPassword.nextElementSibling.innerHTML = "Please Confirm Your Password";
         e.preventDefault();
-        return false;
-    }
-
-    if (password.value !== confirmPassword.value) {
-        confirmPassword.nextElementSibling.innerHTML = "Your password don't match";
-        e.preventDefault();
-        return false;
-    }
-
+            return false;
+    } else if (!(passWord.value == confPassword.value)){
+            confPassword.nextElementSibling.innerHTML = "Password Mismatch";
+            e.preventDefault();
+            return false;
+        }else {
+    confPassword.nextElementSibling.innerHTML = "";
     return true;
+        }
+    }
 
+const signupVal = (e) => {
+fieldVal(firstName, nameCheck, firstNameInput, firstNameError, e);
+fieldVal(lastName,nameCheck, lastNameInput, LastNameError, e);
+fieldVal(phoneNum,checkNum, phoneInput, phoneError, e);
+fieldVal(emailAddress, validEmail, emailInput, emailError, e);
+fieldVal(passWord, pwdVal, passWordInput, passwordError, e);
+confPwdVal(e);
+return true;
 }
 
-const checkNum = phoneNum => {
-    return /^[\+0-9]{8,15}$/.match(phoneNum);
-}
-
-const validEmail = emailAddress => {
-    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,6})+$/.match(emailAddress);
-}
-
-const pwdVal = passWord => {
-    return /(?=.*\d)(?=.*[a-z])(?=.*[a-zA-Z]).{6,}$/.match(passWord);
-}
-
-const confPwd = confPassword => {
-    return /(?=.*\d)(?=.*[a-z])(?=.*[a-zA-Z]).{6,}$/.match(confPassword);
-}
-
-submitBtn.addEventListener('click', validation);
+signupForm.addEventListener("submit", signupVal);
 
 
 
