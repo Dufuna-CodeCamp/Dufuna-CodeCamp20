6742c69c6bbf1be9b679a6191a84858d0e33a56d
Heyeoler <peteriyiola@gmail.com>
2020-09-14 12:43:06 +0100

    edited

--- submissions/peterIyiola/JS/index2.js
+++ submissions/peterIyiola/JS/index2.js
@@ -1,12 +1,12 @@
 for (let i = 1; i <= 116; i++) 
 {
     
-if( i % 3 ==0)   
+if (i % 3 ==0 && i % 5 ==0)
+    console.log("Software" + " " + "Developer");
+   
+else if( i % 3 ==0)   
     console.log("software");
 
-if(i % 5 ==0)
+else  (i % 5 ==0)
     console.log("Developer");
-
-if (i % 3 ==0 && i % 5 ==0)
-    console.log("Software" + " " + "Developer")
 }
