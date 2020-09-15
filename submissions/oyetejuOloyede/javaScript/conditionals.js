eccf9c277de9f8241a634c02f20f3ba3b593538f
Oyeteju Oloyede <oyetejuoloyede@gmail.com>
2020-09-15 22:15:03 +0100

    add update

--- submissions/oyetejuOloyede/javaScript/conditionals.js
+++ submissions/oyetejuOloyede/javaScript/conditionals.js
@@ -1,16 +1,17 @@
-// conditionals.js
-const diff = 123 - 7;
-for (let i = 1; i <= diff; i++) {
-  let output = "";
-  if (i % 3 !== 0 && i % 5 !== 0) {
-    output = i;
-  } else {
-    if (i % 3 === 0) {
-      output += "software ";
-    }
-    if (i % 5 === 0) {
-      output += "development";
-    }
-  }
-  console.log(output);
+var x = 123 -7;
+
+
+for(var i = 1; i < x; i++){
+	if(i % 3 == 0 && i % 5 == 0){
+		console.log('Software Developer');
+	}
+	else if ( i % 3 == 0) {
+		console.log('Software');
+	}
+	else if (i % 5 == 0){
+		console.log('Developer');
+	}
+	else {
+		console.log(i);
+	}
 }
