b72ab4c9d87a6eb7d4ec8c8faa8dfee9eb81077e
Heyeoler <peteriyiola@gmail.com>
2020-09-15 22:23:22 +0100

    new editssss

--- submissions/peterIyiola/JS/index2.js
+++ submissions/peterIyiola/JS/index2.js
@@ -2,11 +2,11 @@ for (let i = 1; i <= 116; i++)
 {
     
 if (i % 3 ==0 && i % 5 ==0)
-    console.log(i + " " + "Software Developer");
+    console.log(i + " " + "Software Development");
    
 else if( i % 3 ==0)   
     console.log(i + " " + "software");
 
 else  (i % 5 ==0)
-    console.log(i + " " + "Developer");
+    console.log(i);
 }
