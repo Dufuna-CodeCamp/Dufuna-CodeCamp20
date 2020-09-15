07fdf04f16c6a3708b92384e1a2375fc870e3ae8
Nwakaeme Cynthia Ebere <nwakaemecynthia@gmail.com>
2020-09-15 12:51:22 +0100

    [22] Newsletter validation

---
+++ submissions/cynthiaNwakaeme/domManipulation/newsletter.js
@@ -0,0 +1,22 @@
+let sendForm = document.getElementById("requiredField"),
+    newsletter = document.getElementById("newsletter"),
+    errorMessage = document.querySelector(".errorMessage");
+
+// To validate the Newsletter
+sendForm.addEventListener("submit", require = (event) => {
+    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,10})+$/;
+
+    if (newsletter.value === "") {
+        errorMessage.innerHTML = "Please enter a your email";
+        event.preventDefault();
+        return false;
+    } else
+    if (!newsletter.value.match(emailRegex)) {
+        errorMessage.innerHTML = "Please enter a valid email";
+        event.preventDefault();
+        return false;
+    } else {
+        errorMessage.innerHTML = "";
+        return true;
+    }
+});
