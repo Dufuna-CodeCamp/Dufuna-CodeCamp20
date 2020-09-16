7a6f4053313b15af34a73e1eaf78d518e77d6924
israel-force <oladosuisraelolayinka@gmail.com>
2020-09-16 14:06:17 +0100

    did some adjustment to onInput Event

--- submissions/israelOladosu/DomManipulation/index.js
+++ submissions/israelOladosu/DomManipulation/index.js
@@ -7,37 +7,40 @@ let submitForm = document.getElementById("validateForm"),
 const RegEx = /^[0-9]$/;
 
 document.addEventListener("DOMContentLoaded", function () {
-  if (+price.lastChild.innerText < 1000) {
+  localStorage.setItem("price", JSON.stringify(price.lastChild.innerText));
+});
+
+function shippingCost(cost) {
+  if (+cost < 1000) {
     shipping_cost.lastChild.innerHTML = `<span>100</span>`;
-  } else if (
-    +price.lastChild.innerText >= 1000 &&
-    +price.lastChild.innerText < 20000
-  ) {
+  } else if (+cost >= 1000 && +cost < 20000) {
     shipping_cost.lastChild.innerHTML = `<span>${
       (15 / 100) * +price.lastChild.innerText
     }</span>`;
-  } else if (
-    +price.lastChild.innerText >= 2000 &&
-    +price.lastChild.innerText < 100000
-  ) {
+  } else if (+cost >= 2000 && +cost < 100000) {
     shipping_cost.lastChild.innerHTML = `<span>${
       (20 / 100) * +price.lastChild.innerText
     }</span>`;
   } else {
     shipping_cost.lastChild.innerHTML = `<span>25000</span>`;
   }
-  total_cost.lastChild.innerHTML = `<span>${
-    +price.lastChild.innerText + +shipping_cost.lastChild.innerText
-  }</span>`;
-});
+}
 
 qty.addEventListener("input", function () {
   if (+qty.value > 1) {
+    console.dir(price.lastChild);
+    price.lastChild.innerHTML = `<span>${
+      +qty.value * +price.lastChild.innerText
+    }</span>`;
+    shippingCost(price.lastChild.innerText);
     total_cost.lastChild.innerHTML = `<span>${
-      +qty.value *
-      (+price.lastChild.innerText + +shipping_cost.lastChild.innerText)
+      +price.lastChild.innerText + +shipping_cost.lastChild.innerText
     }</span>`;
   } else {
+    const initialPrice = localStorage.getItem("price");
+    const oldPrice = JSON.parse(initialPrice);
+    price.lastChild.innerHTML = `<span>${oldPrice}</span>`;
+    shippingCost(oldPrice);
     total_cost.lastChild.innerHTML = `<span>${
       +price.lastChild.innerText + +shipping_cost.lastChild.innerText
     }</span>`;
