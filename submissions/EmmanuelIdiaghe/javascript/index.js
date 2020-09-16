fd64c23a1ca8ff1b817af54c138907dd2d3ba508
Emmanuel Idiaghe <emmanuelidiaghe@gmail.com>
2020-09-16 14:53:06 +0100

    created javascript objects within an array

---
+++ submissions/EmmanuelIdiaghe/javascript/index.js
@@ -0,0 +1,27 @@
+/*
+    Task: Successively display each of the property of the array in the console.
+*/
+
+var productList = [
+     {
+        productName: "Shoes for Men",
+        imageUrl: "images/men-shoes-pic.jpeg",
+        productPrice: "$350",
+    },
+    {
+        productName: "Shoes for Women",
+        imageUrl: "images/women-shoes-pic.jpg",
+        productPrice: "$535",
+    },
+    {
+        productName: "Generic Watch",
+        imageUrl: "images/watch-pic.jpg",
+        productPrice: "$120",
+    }
+];
+
+//For Loop to successively display each property.
+
+for (index=0; index < productList.length; index++) {
+    console.log(productList[index].productName + " with " + productList[index].imageUrl + " is sold for " + productList[index].productPrice);
+}
