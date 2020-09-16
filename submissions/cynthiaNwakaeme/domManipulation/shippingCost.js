c3fe896cc025dceebb35242dfa7b9311e235f530
Nwakaeme Cynthia Ebere <nwakaemecynthia@gmail.com>
2020-09-16 10:44:33 +0100

    [40] Calculate Product Shipping Cost

---
+++ submissions/cynthiaNwakaeme/domManipulation/shippingCost.js
@@ -0,0 +1,48 @@
+let quantity = document.getElementById("quantityNumber"),
+    price = document.getElementById("price"),
+    shippingCost = document.getElementById("shippingCost"),
+    totalCost = document.getElementById("totalCost"),
+    button = document.getElementById("buyNow");
+
+
+
+let shippingAnalysis = (event) => {
+    if (quantity.value == "" || quantity.value <= 0) {
+        button.disabled = true;
+        button.style.backgroundColor = "rgba(218, 165, 32, 0.55)";
+        event.preventDefault();
+        return false;
+
+    } else {
+        button.disabled = false;
+        let productPrice = (quantity.value * price.innerHTML);
+
+        if (productPrice <= 1000) {
+            shippingCost.innerHTML = parseInt(100);
+            totalCost.innerHTML = parseInt(productPrice) + parseInt(shippingCost.innerHTML);
+            event.preventDefault();
+            return false;
+
+        } else if (productPrice > 1000 && productPrice <= 20000) {
+            shippingCost.innerHTML = parseInt(0.15 * productPrice);
+            totalCost.innerHTML = parseInt(productPrice) + parseInt(shippingCost.innerHTML);
+            event.preventDefault();
+            return false;
+
+        } else if (productPrice > 20000 && productPrice <= 100000) {
+            shippingCost.innerHTML = parseInt(0.20 * productPrice);
+            totalCost.innerHTML = parseInt(productPrice) + parseInt(shippingCost.innerHTML);
+            event.preventDefault();
+            return false;
+
+        } else {
+            shippingCost.innerHTML = 25000;
+            totalCost.innerHTML = parseInt(productPrice) + parseInt(shippingCost.innerHTML);
+            return true;
+        }
+    }
+};
+
+button.addEventListener("click", buttonValidate = (event) => {
+    shippingAnalysis(event);
+});
