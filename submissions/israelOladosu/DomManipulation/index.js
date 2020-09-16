4930b334b4b47af09a61b08e97aa023a59a586d8
israel-force <oladosuisraelolayinka@gmail.com>
2020-09-16 16:24:58 +0100

    removed back ticks and clg

--- submissions/israelOladosu/DomManipulation/index.js
+++ submissions/israelOladosu/DomManipulation/index.js
@@ -8,34 +8,27 @@ const RegEx = /^[0-9]$/;
 
 function shippingCost(cost) {
   if (+cost < 1000) {
-    shipping_cost.lastChild.innerHTML = `100`;
+    shipping_cost.lastChild.innerHTML = 100;
   } else if (+cost >= 1000 && +cost < 20000) {
-    shipping_cost.lastChild.innerHTML = `${
-      (15 / 100) * +price.lastChild.innerText
-    }`;
+    shipping_cost.lastChild.innerHTML = (15 / 100) * +price.lastChild.innerText;
   } else if (+cost >= 2000 && +cost < 100000) {
-    shipping_cost.lastChild.innerHTML = `${
-      (20 / 100) * +price.lastChild.innerText
-    }`;
+    shipping_cost.lastChild.innerHTML = (20 / 100) * +price.lastChild.innerText;
   } else {
-    shipping_cost.lastChild.innerHTML = `25000`;
+    shipping_cost.lastChild.innerHTML = 25000;
   }
 }
 
 qty.addEventListener("input", function () {
   if (+qty.value > 1) {
-    console.dir(price.lastChild);
-    price.lastChild.innerHTML = `${+qty.value * +price.lastChild.innerText}`;
+    price.lastChild.innerHTML = +qty.value * +price.lastChild.innerText;
     shippingCost(price.lastChild.innerText);
-    total_cost.lastChild.innerHTML = `${
-      +price.lastChild.innerText + +shipping_cost.lastChild.innerText
-    }`;
+    total_cost.lastChild.innerHTML =
+      +price.lastChild.innerText + +shipping_cost.lastChild.innerText;
   } else {
-    price.lastChild.innerHTML = `900000`;
+    price.lastChild.innerHTML = 900000;
     shippingCost(price.lastChild.innerHTML);
-    total_cost.lastChild.innerHTML = `${
-      +price.lastChild.innerText + +shipping_cost.lastChild.innerText
-    }`;
+    total_cost.lastChild.innerHTML =
+      +price.lastChild.innerText + +shipping_cost.lastChild.innerText;
   }
 });
 
