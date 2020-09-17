131ff406cce0f7273877613f42a5b74cda856486
faithakinbobola <faithakinbobola2@gmail.com>
2020-09-17 11:12:10 +0100

    made requested changes

--- submissions/akinbobolaFaith/dom/index.js
+++ submissions/akinbobolaFaith/dom/index.js
@@ -1,19 +1,17 @@
-var quantity = document.getElementById('quantity'),
+let quantity = document.getElementById('quantity'),
     price = document.getElementById('price'),
     shipping = document.getElementById('shipping'),
     cost = document.getElementById('cost'),
-    button = document.getElementById('myBtn')
-    unitPrice = 14000,
-    productPrice = unitPrice * quantity.value,
-    shipping = 0.15 * productPrice
+    button = document.getElementById('myBtn'),
+    x = price.innerHTML,
+    productPrice = x * quantity.value
 ;
-console.dir(quantity);
-function quantityField(event) {
+
+function quantityField() {
     let quantityRegex = /^[1-9]+[0-9]*$/;
     if (!quantity.value.match(quantityRegex))  {
         button.disabled = true;
         quantity.nextElementSibling.innerHTML = "Please enter a valid quantity";
-        event.preventDefault();
     }else {
         button.disabled = false;
         quantity.nextElementSibling.innerHTML = "";
@@ -21,11 +19,11 @@ function quantityField(event) {
 }
 
 function priceField() {
-    if (quantity.value > 0) {
-        productPrice = unitPrice * quantity.value;
-        price.innerHTML = productPrice;        
+        if (quantity.value > 0) {
+        productPrice = x * quantity.value;
+        price.innerHTML = productPrice;
     }else {
-        return true;
+        return false;
     }
 }
 
@@ -33,13 +31,13 @@ function shippingAmount() {
     if (productPrice < 1000) {
         shipping = 100;
         shippingCost.innerHTML = shipping;
-    } else if ((productPrice > 1000) && (productPrice < 20000)) {
+    } else if ((productPrice >= 1000) && (productPrice <= 20000)) {
         shipping = 0.15 * productPrice;
         shippingCost.innerHTML = shipping;
-    } else if ((productPrice > 20000) && (productPrice < 100000)) {
+    } else if ((productPrice >= 20000) && (productPrice <= 100000)) {
         shipping = 0.2 * productPrice;
         shippingCost.innerHTML = shipping;
-    } else if (productPrice > 100000) {
+    } else if (productPrice >= 100000) {
         shipping = 25000;
         shippingCost.innerHTML = shipping;
     } else {
@@ -55,11 +53,11 @@ function totalCost() {
     }
 }
 
-function indexHandle(e) {
-    quantityField(e);
-    priceField(e);
-    shippingAmount(e);
-    totalCost(e);
+function indexHandle() {
+    quantityField();
+    priceField();
+    shippingAmount();
+    totalCost();
 }
 
-quantity.addEventListener("change", indexHandle);
+quantity.addEventListener("input", indexHandle);
