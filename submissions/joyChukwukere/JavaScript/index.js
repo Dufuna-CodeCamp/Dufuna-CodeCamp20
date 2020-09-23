f64144e2671b2bfb8c881e8154ccf2a9bd38f168
Joy Chukwukere <chukwukerejoy@gmail.com>
2020-09-23 12:47:48 +0100

    rendered product list

--- submissions/joyChukwukere/JavaScript/index.js
+++ submissions/joyChukwukere/JavaScript/index.js
@@ -1,148 +1,29 @@
-// key operatores(comment)
-// let name = "Ada";
-// console.log(name);
+    var productList = [
+    {
+        productName: "APPLE IPHONE 8 PLUS 5.5'' 64GB+3GB",
+        imageUrl: "../html/images/iphn.jpg",
+        productPrice: "#198,650",
+    },
 
-// let age = 22;
-// console.log(age);
+    {
+        productName: "APPLE IPHONE 11 PLUS 5.5'' 64GB+3GB",
+        imageUrl: "../html/images/iphone11jpg.jpg",
+        productPrice: "#200,650",
+    },
 
-// console.log(x);
-// var x = 5;
-// console.log(x);
+    {
+        productName: "APPLE IPHONE 11 pro 5.5'' 64GB+3GB",
+        imageUrl: "../html/images/iphone11-pro.jpg",
+        productPrice: "#300,650",
+    },
+    ];
 
-// let price = 50;
-// console.log(price);
-
-// price = 100
-// console.log(price);
-
-// const height = "joseph";
-// console.log(height);
-
-// let x = 2,
-//     y = 4,
-//     z = 5;
-// z++
-// console.log(z);
-
-// let firstname = "Joy"
-//     surname = "Chukwukere"
-// console.log(firstname + " " + surname)
-
-// let price = 50;
-// console.log(price);
-// price -= 10;
-// console.log(price);
-
-// let hour = new Date() .getHours();
-// console.log(hour);
-// if (hour > 12) {
-//     console.log("Good Day")
-// }   else if (hour < 12) {
-//     console.log("good Morning")
-// }
-// else {
-//     console.log ("Good Night")
-// }
-
-//comparison operator
-// let fruit = "mangoes";
-
-// switch(fruit) {
-//     case "oranges":
-//         console.log("I love oranges");
-//         break;
-//     case "papayas":
-//         console.log("I love papayas");
-//         break;
-//     default:
-//         console.log("I don't have a favourite fruit");
-// }
-
-// to add age restriction to your site
-
-// var age = 15;
-// if (age >= 18) {
-//   console.log("access granted");
-// } else {
-//   console.log("access denied");
-// }
-
-// logical operators
-// && And
-// returbs true if both the operands are true
-// let name = "Joy";
-//     age = 23;
-// if  (name == "Joy" && age <= 18) {
-//     console.log ("I am Joy");
-// }
-
-// var age = 23;
-//     name = "Joy'"
-
-// if (age >=18 && age <=35 && name == "Love")  {
-//     console.log("you can apply");
-// }   else {
-//     console.log(" you cannot apply");
-//     // here it returns false(you cannot apply because the name is Joy not Love)
-// }
-
-// for "OR" logical operator, if atleast one condition is true
-
-// Loop****************
-// For Loop
-// var str = "";
-// for (var i = 0; i < 9; i++) {
-//     str = str + i;
-// }
-// console.log(str);
-
-// while loop
-
-// var n = 0;
-// var str = "";
-// while (n < 10) {
-//     str = str + n; n++;
-// }
-// console.log(str)
-
-// Do..while. Loop
-//  var str = "";
-//     i = 0;
-
-// do {
-//     i++;
-//     str += i;
-// }
-
-// while (i <= 9)
-// console.log(str);
-
-// for.. in  Loop
-// var str = "";
-//     person = {
-//         name: "John",
-//         age: 25
-//     };
-
-// for( x in person) {
-//     str += person[x];
-// }
-
-// console.log(str);
-
-// break statement****
-
-// var n = 0;
-// var str = "";
-// while (n < 10) {
-//     if(n === 5){
-//         break;
-//     }
-//     str = str + n; n++;
-// }
-// console.log(str)
-
-// 
-
-
-
+    productList.forEach(function (product) {
+    console.log(
+        product.productName +
+        " with " +
+        product.imageUrl +
+        " is sold for " +
+        product.productPrice
+    );
+    });
