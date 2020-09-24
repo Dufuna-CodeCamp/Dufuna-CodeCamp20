b72d17bba7c806a3869de37c06cce1a1e1498203
Chabu Chabu <chabuchabu32@gmail.com>
2020-09-24 15:39:50 +0100

    Render product List

---
+++ submissions/chabuChabu/javaScript/index.js
@@ -0,0 +1,24 @@
+var productList = [
+   {
+    productName: "Red Tennis Shoes",
+    imageUrl: "../html/images/product-02.jpg",
+    productPrice: "$200",
+   },
+   {
+    productName: "Black Office shoes",
+    imageUrl: "../html/images/product-03.jpg",
+    productPrice: "$350",
+   },
+   {
+    productName: "Red High-heeled shoes",
+    imageUrl: "../html/images/product-04.jpg",
+    productPrice: "$230",
+   }
+]
+console.log(productList[0].productName+" with "+productList[0].imageUrl+" is sold for "+productList[0].productPrice);
+console.log(productList[1].productName+" with "+productList[1].imageUrl+" is sold for "+productList[1].productPrice);
+console.log(productList[2].productName+" with "+productList[2].imageUrl+" is sold for "+productList[2].productPrice);
+   
+   
+
+
