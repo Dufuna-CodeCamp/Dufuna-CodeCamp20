2b9cc0f7ebad6a3be040c1019c90a7d5e4f54e20
Adeyemi juwon <samju7778@gmail.com>
2020-10-16 10:05:58 +0100

    change sql file

--- submissions/adeyemiJuwon/database/init.sql
+++ submissions/adeyemiJuwon/database/init.sql
@@ -29,7 +29,7 @@ CREATE TABLE `admin_users` (
   `password` varchar(250) COLLATE utf8mb4_general_ci NOT NULL,
   `phone` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`admin_user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,12 +38,7 @@ CREATE TABLE `admin_users` (
 
 LOCK TABLES `admin_users` WRITE;
 /*!40000 ALTER TABLE `admin_users` DISABLE KEYS */;
-INSERT INTO `admin_users` VALUES (1,'juwon','samju7778@gmail.com','ade100','23480-667-76598'),
-(2,'samuel','samju6677@gmail.com','sam100','23480-667-76598'),
-(3,'nuru','nuru@gmail.com','nuru100','23480-667-87598'),(4,'itunu','itunu@gmail.com','itunu100','23480-667-66598'),
-(5,'feyi','feyi@gmail.com','feyi100','23480-567-76598'),
-(6,'temidayo','temi@gmail.com','temi100','23480-667-76598'),
-(7,'seye','seye@gmail.com','seye100','23480-667-76690');
+INSERT INTO `admin_users` VALUES (1,'juwon','samju7778@gmail.com','ade100','23480-667-76598'),(2,'samuel','samju6677@gmail.com','sam100','23480-667-76598'),(3,'nuru','nuru@gmail.com','nuru100','23480-667-87598'),(4,'itunu','itunu@gmail.com','itunu100','23480-667-66598'),(5,'feyi','feyi@gmail.com','feyi100','23480-567-76598'),(6,'temidayo','temi@gmail.com','temi100','23480-667-76598'),(7,'seye','seye@gmail.com','seye100','23480-667-76690');
 /*!40000 ALTER TABLE `admin_users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -58,7 +53,7 @@ CREATE TABLE `categorys` (
   `category_id` int(11) NOT NULL AUTO_INCREMENT,
   `category` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`category_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,12 +62,7 @@ CREATE TABLE `categorys` (
 
 LOCK TABLES `categorys` WRITE;
 /*!40000 ALTER TABLE `categorys` DISABLE KEYS */;
-INSERT INTO `categorys` VALUES (1,'Laptop Accessiories '),
-(2,'Baby Care & Wear'),
-(3,'Women Collection'),
-(4,'Electronic Accessiories'),
-(5,'Smart Home & Garden'),
-(6,'Lights & Lighting');
+INSERT INTO `categorys` VALUES (1,'Laptop Accessiories '),(2,'Baby Care & Wear'),(3,'Women Collection'),(4,'Electronic Accessiories'),(5,'Smart Home & Garden'),(6,'Lights & Lighting');
 /*!40000 ALTER TABLE `categorys` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -97,7 +87,7 @@ CREATE TABLE `customers` (
   `state` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   `country` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`customer_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -106,11 +96,7 @@ CREATE TABLE `customers` (
 
 LOCK TABLES `customers` WRITE;
 /*!40000 ALTER TABLE `customers` DISABLE KEYS */;
-INSERT INTO `customers` VALUES 
-(1,'Samuel','Adeniyi','ade@gmail.com','adew200',' 234-80775-67589','5 Ogunmefun Somolu Lagos','Ogunmefun',27704,'Lagos','Lagos','Nigeria'),
-(2,'Ronke','Adeyemi','ronke@gmail.com','ronke200','234-80665-67589','10 Ring Road Ibadan Oyo','Ring Road',27705,'Ibadan','Oyo','Nigeria'),
-(3,'Itunu','Adekoya','itunu@gmail.com','itunu200','234-80675-67589','11 wale Somolu Lagos','wale',27704,'Lagos','Lagos','Nigeria'),
-(4,'Wale','Ejigboye','wale@gmail.com','wale200','234-80775-67579','5 Ogonbiade Kunlede Ilorin ','Ogonbiade',27703,'Ilorin','Kwara','Nigeria');
+INSERT INTO `customers` VALUES (1,'Samuel','Adeniyi','ade@gmail.com','adew200',' 234-80775-67589','5 Ogunmefun Somolu Lagos','Ogunmefun',27704,'Lagos','Lagos','Nigeria'),(2,'Ronke','Adeyemi','ronke@gmail.com','ronke200','234-80665-67589','10 Ring Road Ibadan Oyo','Ring Road',27705,'Ibadan','Oyo','Nigeria'),(3,'Itunu','Adekoya','itunu@gmail.com','itunu200','234-80675-67589','11 wale Somolu Lagos','wale',27704,'Lagos','Lagos','Nigeria'),(4,'Wale','Ejigboye','wale@gmail.com','wale200','234-80775-67579','5 Ogonbiade Kunlede Ilorin ','Ogonbiade',27703,'Ilorin','Kwara','Nigeria');
 /*!40000 ALTER TABLE `customers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -158,7 +144,7 @@ CREATE TABLE `orders` (
   PRIMARY KEY (`order_id`),
   KEY `fk_orders_customers_idx` (`customer_id`),
   CONSTRAINT `fk_orders_customers` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -193,7 +179,7 @@ CREATE TABLE `products` (
   KEY `fk_products_categorys_idx` (`category`),
   CONSTRAINT `fk_products_admin_users` FOREIGN KEY (`admin_user`) REFERENCES `admin_users` (`admin_user_id`) ON UPDATE CASCADE,
   CONSTRAINT `fk_products_categorys` FOREIGN KEY (`category`) REFERENCES `categorys` (`category_id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -202,11 +188,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES 
-(1,'Xiaomi Mi Notebook Pro 2019 15.6 inch Windows 10','Chinese Language Home Edition Intel Core i5 - 8250U Quad Core 1.6GHz CPU 8GB RAM 256GB SSD 1.0MP Front Camera Fingerprint Sensor',NULL,1485.83,1,2,50,'low stock'),
-(2,'Xiaomi Redmibook','Chinese Language Home Edition Intel Core i5 - 8250U Quad Core 1.6GHz CPU 8GB RAM 256GB SSD 1.0MP Front Camera Fingerprint Sensor',NULL,819.52,1,1,100,'low stock'),
-(3,'Charging power','1.8m DC Charging power ',NULL,4.26,1,4,2000,'in stock'),
-(4,'Laptop Bag','11 Inch Laptop Bag Tablet Zipper Pouch Sleeve for MacBook Air',NULL,6.95,1,6,2000,'in stock');
+INSERT INTO `products` VALUES (1,'Xiaomi Mi Notebook Pro 2019 15.6 inch Windows 10','Chinese Language Home Edition Intel Core i5 - 8250U Quad Core 1.6GHz CPU 8GB RAM 256GB SSD 1.0MP Front Camera Fingerprint Sensor',NULL,1485.83,1,2,50,'low stock'),(2,'Xiaomi Redmibook','Chinese Language Home Edition Intel Core i5 - 8250U Quad Core 1.6GHz CPU 8GB RAM 256GB SSD 1.0MP Front Camera Fingerprint Sensor',NULL,819.52,1,1,100,'low stock'),(3,'Charging power','1.8m DC Charging power ',NULL,4.26,1,4,2000,'in stock'),(4,'Laptop Bag','11 Inch Laptop Bag Tablet Zipper Pouch Sleeve for MacBook Air',NULL,6.95,1,6,2000,'in stock');
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
