2519ebfb801e94b9c803fa2d218779b7024c6edb
Victor-Aienobe <viktornobe@gmail.com>
2020-10-18 13:22:38 +0100

    updated database

--- submissions/victorAienobe/database/init.sql
+++ submissions/victorAienobe/database/init.sql
@@ -8,18 +8,21 @@ CREATE TABLE products (
 	name VARCHAR(50) NOT NULL,
 	description VARCHAR(250) NOT NULL,
 	image BLOB,
+    category_id INT,
+    admin_id INT,
 	unit_price INT NOT NULL,
     stock_level INT NOT NULL,
     status VARCHAR(20) NOT NULL,
-	PRIMARY KEY (product_id)
+	PRIMARY KEY (product_id),
+    FOREIGN KEY (category_id) REFERENCES categories (category_id) ON DELETE CASCADE,
+    FOREIGN KEY (admin_id) REFERENCES admins (admin_id) ON DELETE CASCADE
 );
 
 -- Created the categories table
 CREATE TABLE categories (
+	category_id INT NOT NULL AUTO_INCREMENT,
 	category_name VARCHAR(20) NOT NULL,
-	category_id INT,
-	PRIMARY KEY (category_name),
-	FOREIGN KEY (category_id) REFERENCES products (product_id) ON DELETE CASCADE
+	PRIMARY KEY (category_id)
 );
 
 -- Created the customers table
@@ -70,28 +73,27 @@ CREATE TABLE order_items (
 -- Created the admins table
 CREATE TABLE admins (
 	admin_id INT NOT NULL AUTO_INCREMENT,
-    product_id INT,
 	name VARCHAR(50) NOT NULL,
 	email VARCHAR(50) NOT NULL,
 	password VARCHAR(15) NOT NULL,
 	phone_number VARCHAR(15) NOT NULL,
-	PRIMARY KEY (admin_id),
-	FOREIGN KEY (product_id) REFERENCES products (product_id) ON DELETE CASCADE
+	PRIMARY KEY (admin_id)
 );
-
+drop table admins;
+select * from admin;
 -- Inserted values into the products table
-INSERT INTO products (name, description, image, unit_price, stock_level, status)
-VALUES ('blue white collar tee shirt', 'This is a quality shirt you can put on all day', LOAD_FILE('C:\Users\Aienobe\Desktop\Dufuna-CodeCamp20\submissions\victorAienobe\html\images\shirts.png'), '250', '70', 'in stock'),
-('play station 5 console', 'awesome game console that guarantees amazing gaming experience', LOAD_FILE('C:\Users\Aienobe\Desktop\Dufuna-CodeCamp20\submissions\victorAienobe\html\images\Capture3.jpg'), '700', '400', 'in stock'),
-('PSG Nike Kit 20/21 Season', 'brand new quality jerseys for the new season', LOAD_FILE('C:\Users\Aienobe\Desktop\Dufuna-CodeCamp20\submissions\victorAienobe\html\images\Capture2.jpg'), '50', '10', 'low stock'),
-('Nike-Jordan Mercurial Boots', 'perfect soccer boots', LOAD_FILE('C:\Users\Aienobe\Desktop\Dufuna-CodeCamp20\submissions\victorAienobe\html\images\Jordan-x-PSG-x-Nike-PhantomVSN.jpg'), '350', '0', 'out of stock');
+INSERT INTO products (name, description, image, category_id, admin_id, unit_price, stock_level, status)
+VALUES ('blue white collar tee shirt', 'This is a quality shirt you can put on all day', LOAD_FILE('C:\Users\Aienobe\Desktop\Dufuna-CodeCamp20\submissions\victorAienobe\html\images\shirts.png'), 2, 1, '250', '70', 'in stock'),
+('play station 5 console', 'awesome game console that guarantees amazing gaming experience', LOAD_FILE('C:\Users\Aienobe\Desktop\Dufuna-CodeCamp20\submissions\victorAienobe\html\images\Capture3.jpg'), 1, 2, '700', '400', 'in stock'),
+('PSG Nike Kit 20/21 Season', 'brand new quality jerseys for the new season', LOAD_FILE('C:\Users\Aienobe\Desktop\Dufuna-CodeCamp20\submissions\victorAienobe\html\images\Capture2.jpg'), 4, 3, '50', '10', 'low stock'),
+('Nike-Jordan Mercurial Boots', 'perfect soccer boots', LOAD_FILE('C:\Users\Aienobe\Desktop\Dufuna-CodeCamp20\submissions\victorAienobe\html\images\Jordan-x-PSG-x-Nike-PhantomVSN.jpg'), 3, 4, '350', '0', 'out of stock');
 
 -- Inserted values into the categories table
-INSERT INTO categories (category_id, category_name)
-VALUES (2, 'game console'),
-(1, 'Mens-shirt'),
-(4, 'soccer boots'),
-(3, 'club jerseys');
+INSERT INTO categories (category_name)
+VALUES ('game console'),
+('Mens-shirt'),
+('soccer boots'),
+('club jerseys');
 
 -- Inserted values into the customers table
 INSERT INTO customers (first_name, last_name, email_address, password)
@@ -118,8 +120,8 @@ VALUES (100, 1, 33, 250, 8250.00),
 (102, 3, 230, 50, 11500.00);
 
 -- Inserted values into the admins table
-INSERT INTO admins (product_id, name, email, password, phone_number) 
-VALUES (1, 'victor aienobe', 'viktornobe@gmail.com', 'smoke&mirrors', +2348108088144),
-(2, 'benedit ejike', 'ben@gmail.com', 'thanos', +2349025725272),
-(3, 'chidi okpala', 'chidi@gmail.com', 'drstrange', +2347026372527),
-(4, 'simon alvin peters', 'alvinpeters@gmail.com', 'aryastark', +2348025745262);
+INSERT INTO admins (name, email, password, phone_number) 
+VALUES ('victor aienobe', 'viktornobe@gmail.com', 'smoke&mirrors', +2348108088144),
+('benedit ejike', 'ben@gmail.com', 'thanos', +2349025725272),
+('chidi okpala', 'chidi@gmail.com', 'drstrange', +2347026372527),
+('simon alvin peters', 'alvinpeters@gmail.com', 'aryastark', +2348025745262);
