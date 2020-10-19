76609c362c3732cae8f3219d54dfcb90bcf61729
Victor-Aienobe <viktornobe@gmail.com>
2020-10-19 13:56:23 +0100

    rearranged queries and used id to identify tables

--- submissions/victorAienobe/database/init.sql
+++ submissions/victorAienobe/database/init.sql
@@ -2,9 +2,26 @@
 CREATE DATABASE e_commerce_store;
 USE e_commerce_store;
 
+-- Created the categories table
+CREATE TABLE categories (
+	id INT NOT NULL AUTO_INCREMENT,
+	category_name VARCHAR(20) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+-- Created the admins table
+CREATE TABLE admins (
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(50) NOT NULL,
+	email VARCHAR(50) NOT NULL,
+	password VARCHAR(15) NOT NULL,
+	phone_number VARCHAR(15) NOT NULL,
+	PRIMARY KEY (id)
+);
+
 -- created the products table
 CREATE TABLE products (
-	product_id INT NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(50) NOT NULL,
 	description VARCHAR(250) NOT NULL,
 	image BLOB,
@@ -13,16 +30,9 @@ CREATE TABLE products (
 	unit_price INT NOT NULL,
     stock_level INT NOT NULL,
     status VARCHAR(20) NOT NULL,
-	PRIMARY KEY (product_id),
-    FOREIGN KEY (category_id) REFERENCES categories (category_id) ON DELETE CASCADE,
-    FOREIGN KEY (admin_id) REFERENCES admins (admin_id) ON DELETE CASCADE
-);
-
--- Created the categories table
-CREATE TABLE categories (
-	category_id INT NOT NULL AUTO_INCREMENT,
-	category_name VARCHAR(20) NOT NULL,
-	PRIMARY KEY (category_id)
+	PRIMARY KEY (id),
+    FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE,
+    FOREIGN KEY (admin_id) REFERENCES admins (id) ON DELETE CASCADE
 );
 
 -- Created the customers table
@@ -37,6 +47,7 @@ CREATE TABLE customers (
 
 -- Created the customer addresses table
 CREATE TABLE customer_addresses (
+	id INT NOT NULL AUTO_INCREMENT,
 	customer_id INT,
 	street_address VARCHAR(100) NOT NULL,
 	city VARCHAR(20) NOT NULL,
@@ -44,16 +55,17 @@ CREATE TABLE customer_addresses (
 	zip_code INT,
 	country VARCHAR(20) NOT NULL,
 	phone_number BIGINT NOT NULL,
+    PRIMARY KEY (id),
 	FOREIGN KEY (customer_id) REFERENCES customers (id) ON DELETE CASCADE
 );
 
 -- Created the orders table
 CREATE TABLE orders (
-	order_id INT NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL AUTO_INCREMENT,
 	customer_orders INT,
 	order_amount DECIMAL(7,2) NOT NULL,
 	order_created_at DATETIME,
-	PRIMARY KEY (order_id),
+	PRIMARY KEY (id),
 	FOREIGN KEY (customer_orders) REFERENCES customers (id) ON DELETE CASCADE
 );
 -- Auto increment count is set to start from 100
@@ -61,32 +73,16 @@ ALTER TABLE orders AUTO_INCREMENT = 100;
 
 -- Created the order items table
 CREATE TABLE order_items (
+	id INT NOT NULL AUTO_INCREMENT,
 	order_id INT,
 	product_id INT,
 	quantity INT NOT NULL,
 	unit_price INT NOT NULL,
 	total_amount DECIMAL(10,2) NOT NULL,
-	FOREIGN KEY (order_id) REFERENCES orders (order_id) ON DELETE CASCADE,
-	FOREIGN KEY (product_id) REFERENCES products (product_id) ON DELETE CASCADE
+    PRIMARY KEY (id),
+	FOREIGN KEY (order_id) REFERENCES orders (id) ON DELETE CASCADE,
+	FOREIGN KEY (product_id) REFERENCES products (id) ON DELETE CASCADE
 );
-
--- Created the admins table
-CREATE TABLE admins (
-	admin_id INT NOT NULL AUTO_INCREMENT,
-	name VARCHAR(50) NOT NULL,
-	email VARCHAR(50) NOT NULL,
-	password VARCHAR(15) NOT NULL,
-	phone_number VARCHAR(15) NOT NULL,
-	PRIMARY KEY (admin_id)
-);
-
-
--- Inserted values into the products table
-INSERT INTO products (name, description, image, category_id, admin_id, unit_price, stock_level, status)
-VALUES ('blue white collar tee shirt', 'This is a quality shirt you can put on all day', LOAD_FILE('C:\Users\Aienobe\Desktop\Dufuna-CodeCamp20\submissions\victorAienobe\html\images\shirts.png'), 2, 1, '250', '70', 'in stock'),
-('play station 5 console', 'awesome game console that guarantees amazing gaming experience', LOAD_FILE('C:\Users\Aienobe\Desktop\Dufuna-CodeCamp20\submissions\victorAienobe\html\images\Capture3.jpg'), 1, 2, '700', '400', 'in stock'),
-('PSG Nike Kit 20/21 Season', 'brand new quality jerseys for the new season', LOAD_FILE('C:\Users\Aienobe\Desktop\Dufuna-CodeCamp20\submissions\victorAienobe\html\images\Capture2.jpg'), 4, 3, '50', '10', 'low stock'),
-('Nike-Jordan Mercurial Boots', 'perfect soccer boots', LOAD_FILE('C:\Users\Aienobe\Desktop\Dufuna-CodeCamp20\submissions\victorAienobe\html\images\Jordan-x-PSG-x-Nike-PhantomVSN.jpg'), 3, 4, '350', '0', 'out of stock');
 
 -- Inserted values into the categories table
 INSERT INTO categories (category_name)
@@ -94,6 +90,20 @@ VALUES ('game console'),
 ('Mens-shirt'),
 ('soccer boots'),
 ('club jerseys');
+
+-- Inserted values into the admins table
+INSERT INTO admins (name, email, password, phone_number) 
+VALUES ('victor aienobe', 'viktornobe@gmail.com', 'smoke&mirrors', +2348108088144),
+('benedit ejike', 'ben@gmail.com', 'thanos', +2349025725272),
+('chidi okpala', 'chidi@gmail.com', 'drstrange', +2347026372527),
+('simon alvin peters', 'alvinpeters@gmail.com', 'aryastark', +2348025745262);
+
+-- Inserted values into the products table
+INSERT INTO products (name, description, image, category_id, admin_id, unit_price, stock_level, status)
+VALUES ('blue white collar tee shirt', 'This is a quality shirt you can put on all day', LOAD_FILE('C:\Users\Aienobe\Desktop\Dufuna-CodeCamp20\submissions\victorAienobe\html\images\shirts.png'), 2, 1, '250', '70', 'in stock'),
+('play station 5 console', 'awesome game console that guarantees amazing gaming experience', LOAD_FILE('C:\Users\Aienobe\Desktop\Dufuna-CodeCamp20\submissions\victorAienobe\html\images\Capture3.jpg'), 1, 2, '700', '400', 'in stock'),
+('PSG Nike Kit 20/21 Season', 'brand new quality jerseys for the new season', LOAD_FILE('C:\Users\Aienobe\Desktop\Dufuna-CodeCamp20\submissions\victorAienobe\html\images\Capture2.jpg'), 4, 3, '50', '10', 'low stock'),
+('Nike-Jordan Mercurial Boots', 'perfect soccer boots', LOAD_FILE('C:\Users\Aienobe\Desktop\Dufuna-CodeCamp20\submissions\victorAienobe\html\images\Jordan-x-PSG-x-Nike-PhantomVSN.jpg'), 3, 4, '350', '0', 'out of stock');
 
 -- Inserted values into the customers table
 INSERT INTO customers (first_name, last_name, email_address, password)
@@ -118,10 +128,3 @@ INSERT INTO order_items (order_id, product_id, quantity, unit_price, total_amoun
 VALUES (100, 1, 33, 250, 8250.00),
 (101, 2, 40, 700, 28000.00),
 (102, 3, 230, 50, 11500.00);
-
--- Inserted values into the admins table
-INSERT INTO admins (name, email, password, phone_number) 
-VALUES ('victor aienobe', 'viktornobe@gmail.com', 'smoke&mirrors', +2348108088144),
-('benedit ejike', 'ben@gmail.com', 'thanos', +2349025725272),
-('chidi okpala', 'chidi@gmail.com', 'drstrange', +2347026372527),
-('simon alvin peters', 'alvinpeters@gmail.com', 'aryastark', +2348025745262);
