9ab625887240d37d2293db7cd397b9b94dfb3cd1
Oyeteju Oloyede <oyetejuoloyede@gmail.com>
2020-10-19 21:10:09 +0100

    add update

--- submissions/oyetejuOloyede/DOM/contactForm.js
+++ submissions/oyetejuOloyede/DOM/contactForm.js
@@ -1,36 +1,35 @@
-let submitForm = document.getElementById('submit');
+let submitForm = document.getElementById('validateForm');
 let fullName = document.getElementById('fullName');
 let email = document.getElementById('email');
 let subject = document.getElementById('subject');
-let textArea = document.getElementById('textArea');
-let fullNameMsg = 'Please enter your Name';
-let emailMsg = "Please enter your Email Address";
-let emailErrorMsg = "Please enter a valid email";
-let subjectMsg = "Please enter a message subject";
-let textAreaMsg = "Please enter your Message";
+let msgInput = document.getElementById('messageInput');
+let fullNameMsg = 'Please enter your name';
+let fullNameMsg2 = 'Please enter a valid name';
+let emailMsg = "Please enter your email address";
+let emailMsg2 = "Please enter a valid email address";
+let subjectMsg = 'Please enter a message subject';
+let msgInputError = "please enter your message";
+function contactFormValidation(field, regEx, errorMsg1, errorMsg2, event){
 
-function valid(regex, field, error1, error2, event) {
-    if(field.value === "") {
-        field.nextElementSibling.innerHTML = error1;
+	 if(field.value == ''){
+        field.nextElementSibling.nextElementSibling.innerHTML= errorMsg1;
         event.preventDefault();
         return false;
-      } else if (!(field.value.match(regex))) {
-          field.nextElementSibling.innerHTML = error2;
-          event.preventDefault();
-          return false;
-          }
-      else {
-        field.nextElementSibling.innerHTML = "";
+    } else if (!(field.value.match(regEx))){
+        field.nextElementSibling.nextElementSibling.innerHTML = errorMsg2;
+        event.preventDefault();
+        return false;
+    } else {
+        field.nextElementSibling.nextElementSibling.innerHTML = "";
         return true;
-      }
+    }
+}
+function validateForm(e){
+	contactFormValidation(fullName, /^[a-z]+$/i, fullNameMsg, fullNameMsg2, e);
+	contactFormValidation(email, /[a-z0-9]+@[a-z]+\.com$/i, emailMsg, emailMsg2, e);
+	contactFormValidation(subject, /^[a-z]+$/i, subjectMsg, subjectMsg, e);
+	contactFormValidation(msgInput, /^[a-z]+$/i, msgInputError, msgInputError, e);
+ 	 return true;
 }
 
-function validation(e) {
-    valid(/^[a-z]+$/i, fullName, fullNameMsg1, fullNameMsg2, e);
-    valid(/[a-z0-9]+@[a-z]+\.com$/i, email, emailMsg, emailErrorMsg, e);
-    valid(/^[a-z]+$/i, subject, subjectMsg, subjectMsg, e);
-    valid(/^[a-z0-9]+$/i, textArea, textAreaMsg, textAreaMsg, e);
-    return true;
-}
-
-submitForm.addEventListener('submit', validation);
+submitForm.addEventListener("submit", validateForm);
