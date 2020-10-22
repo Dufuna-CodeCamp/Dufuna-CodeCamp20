d0a37da7b9ff114ede5f268479d85837e20e8c55
Chabu Chabu <chabuchabu32@gmail.com>
2020-10-22 11:42:42 +0100

    regex for subject and message updated

--- submissions/chabuChabu/javaScript/contactForm.js
+++ submissions/chabuChabu/javaScript/contactForm.js
@@ -23,14 +23,13 @@ function contactValidation(field,regex,errorOne,errorTwo){
 function validateContact(event) {
   let fullNameRegex = /^([\w]{3,})+\s+([\w\s]{3,})+$/i,
       emailRegex =  /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
-      subjectRegex = /^[A-Za-z]+$/,
       messageRegex = /^[A-Za-z]+$/;
       
       contactValidation(fullName,fullNameRegex,
         "Please enter your Name","Please enter a valid Name",event);
       contactValidation(email,emailRegex,
         "Please enter your email","Please enter a valid email",event);
-      contactValidation(subject,subjectRegex,
+      contactValidation(subject,messageRegex,
         "Please enter your subject",null,event);
       contactValidation(textarea,messageRegex,
         "Please enter your message in the text area above",null,event);
