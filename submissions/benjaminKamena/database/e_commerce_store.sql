0e9c062fcbca5d4c004b19a6c90034b694cdabd5
kamena1994 <benjii1964@gmail.com>
2020-10-25 11:12:00 +0200

    modified the database file

--- submissions/benjaminKamena/database/e_commerce_store.sql
+++ submissions/benjaminKamena/database/e_commerce_store.sql
@@ -9,30 +9,22 @@
 --
 
 CREATE TABLE `admin_users` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `email_address` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `phone_number` varchar(13) NOT NULL
+  `phone_number` varchar(13) NOT NULL,
+  PRIMARY KEY (`id`)
 );
-
---
--- Dumping data for table `admin users`
---
-
-INSERT INTO `admin users` (`id`, `name`, `email_address`, `password`, `phone_number`) VALUES
-(1, 'benjamin', 'benj@gmail.com', '1234', '3456'),
-(2, 'benjamin', 'benj@gmail.com', '1234', '3456');
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `categories`
 --
 
 CREATE TABLE `categories` (
-  `id` int(11) NOT NULL,
-  `category_name` varchar(255) NOT NULL
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(255) NOT NULL.
+  PRIMARY KEY (`id`)
 );
 
 -- --------------------------------------------------------
@@ -42,13 +34,14 @@ CREATE TABLE `categories` (
 --
 
 CREATE TABLE `contact_details` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `street_address` text NOT NULL,
   `city` varchar(100) NOT NULL,
   `state` varchar(100) NOT NULL,
   `zip_code` int(100) NOT NULL,
   `phone_number` varchar(100) NOT NULL,
-  `country` varchar(100) NOT NULL
+  `country` varchar(100) NOT NULL.
+  PRIMARY KEY (`id`)
 );
 
 -- --------------------------------------------------------
@@ -58,12 +51,14 @@ CREATE TABLE `contact_details` (
 --
 
 CREATE TABLE `customers` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `fName` varchar(255) NOT NULL,
   `lName` varchar(255) NOT NULL,
   `email_address` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `contact_info` int(11) NOT NULL
+  `contact_info` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`contact_info`) REFERENCES `contact details` (`id`)
 );
 
 --
@@ -71,12 +66,14 @@ CREATE TABLE `customers` (
 --
 
 CREATE TABLE `orders` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `amount` int(100) NOT NULL,
   `customer_id` int(11) NOT NULL,
   `date` date NOT NULL,
   `qauntity` varchar(255) NOT NULL,
-  `total` int(11) NOT NULL
+  `total` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`)
 );
 
 -- --------------------------------------------------------
@@ -86,7 +83,7 @@ CREATE TABLE `orders` (
 --
 
 CREATE TABLE `products` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `product_name` varchar(100) NOT NULL,
   `description` text NOT NULL,
   `image` text NOT NULL,
@@ -94,7 +91,9 @@ CREATE TABLE `products` (
   `category` int(11) NOT NULL,
   `created_by` int(11) NOT NULL,
   `stock` int(11) NOT NULL,
-  `order_id` int(11) NOT NULL
+  `order_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`)
 );
 
 -- --------------------------------------------------------
@@ -104,9 +103,11 @@ CREATE TABLE `products` (
 --
 
 CREATE TABLE `stocks` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `total_quantity` int(11) NOT NULL,
-  `status` int(11) NOT NULL
+  `status` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`status`) REFERENCES `stock_status` (`id`)
 );
 
 -- --------------------------------------------------------
@@ -116,154 +117,10 @@ CREATE TABLE `stocks` (
 --
 
 CREATE TABLE `stock_status` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `in_stock` varchar(100) NOT NULL,
   `out_of_stock` varchar(100)NOT NULL,
-  `low_stock` int(11) NOT NULL
+  `low_stock` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`id`) REFERENCES `stocks` (`id`)
 );
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin users`
---
-ALTER TABLE `admin_users`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `categories`
---
-ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `contact details`
---
-ALTER TABLE `contact_details`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `customers`
---
-ALTER TABLE `customers`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `contact_info` (`contact_info`);
-
---
--- Indexes for table `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `customer_id` (`customer_id`);
-
---
--- Indexes for table `products`
---
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `created_by` (`created_by`),
-  ADD KEY `products_ibfk_1` (`stock`),
-  ADD KEY `category` (`category`),
-  ADD KEY `order_id` (`order_id`);
-
---
--- Indexes for table `stocks`
---
-ALTER TABLE `stocks`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `status` (`status`);
-
---
--- Indexes for table `stock_status`
---
-ALTER TABLE `stock_status`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admin users`
---
-ALTER TABLE `admin_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `categories`
---
-ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `contact details`
---
-ALTER TABLE `contact_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `customers`
---
-ALTER TABLE `customers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `orders`
---
-ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `products`
---
-ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `stocks`
---
-ALTER TABLE `stocks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `stock_status`
---
-ALTER TABLE `stock_status`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `customers`
---
-ALTER TABLE `customers`
-  ADD CONSTRAINT `customers_ibfk_1` FOREIGN KEY (`contact_info`) REFERENCES `contact details` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Constraints for table `orders`
---
-ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Constraints for table `products`
---
-ALTER TABLE `products`
-  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Constraints for table `stocks`
---
-ALTER TABLE `stocks`
-  ADD CONSTRAINT `stocks_ibfk_1` FOREIGN KEY (`status`) REFERENCES `stock_status` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Constraints for table `stock_status`
---
-ALTER TABLE `stock_status`
-  ADD CONSTRAINT `stock_status_ibfk_1` FOREIGN KEY (`id`) REFERENCES `stocks` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-COMMIT;
