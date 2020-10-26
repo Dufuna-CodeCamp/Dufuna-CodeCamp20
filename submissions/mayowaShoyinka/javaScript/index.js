ed69be34ce6a9ad29b7855d4c391c9f00e5cd570
MayowaShoyinka <47593314+MayowaShoyinka@users.noreply.github.com>
2020-10-26 14:34:52 +0500

    Update index.js
    indented my work well

--- submissions/mayowaShoyinka/javaScript/index.js
+++ submissions/mayowaShoyinka/javaScript/index.js
@@ -1,11 +1,8 @@
-var ProductList=[
-				{ProductName:'shoe1',imageUrl:'../html/img/pic1.jpg',ProductPrice:'$20'},
-				{ProductName:'shoe2',imageUrl:'../html/img/pic2.jpg',ProductPrice:'$40'},
-				{ProductName:'shoe3',imageUrl:'../html/img/pic3.jpg',ProductPrice:'$60'}
+var ProductList = [
+		{ProductName:'Blue Canvas Shoe',imageUrl:'../html/img/pic1.jpg',ProductPrice:'$50'},
+		{ProductName:'Fossil Watch',imageUrl:'../html/img/pic2.jpg',ProductPrice:'$100'},
+		{ProductName:'Balmoral Black Lace Shoe',imageUrl:'../html/img/pic3.jpg',ProductPrice:'$450'} 
 ]
-
-for (i=0; i<ProductList.length; i++){
-
-	console.log (ProductList[i].ProductName + " with url of " + ProductList[i].imageUrl 
-	        +" and price of " + ProductList[i].ProductPrice);
+for (i=0; i<ProductList.length; i++) {
+     console.log (ProductList[i].ProductName + " with url of " + ProductList[i].imageUrl +" and price of " + ProductList[i].ProductPrice);
 }
