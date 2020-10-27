b7d7b087e64464561a8e2ae1038b8aea8ef5a782
Heyeoler <peteriyiola@gmail.com>
2020-10-27 12:25:42 +0100

    code factor edit

--- submissions/peterIyiola/DOM/signup.js
+++ submissions/peterIyiola/DOM/signup.js
@@ -6,9 +6,9 @@ password = document.getElementById('password'),
 confirmPassword = document.getElementById('confirmPassword'),
 phoneNumber = document.getElementById('phoneNumber');
 
-const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,10})+$/;
+const emailRegex = /^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,10})+$/;
 const nameRegex = /^[a-zA-Z]/;
-const phoneNumberRegex = /^[\+]?[0-9]{8,14}$/i;
+const phoneNumberRegex = /^[]?[0-9]{8,14}$/i;
 const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[a-zA-Z]).{6,}$/;
 
 
