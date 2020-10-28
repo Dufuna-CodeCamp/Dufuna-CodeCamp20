c72d8295f4c47395a120626b18ad126b58d7a7bf
kamena1994 <benjii1964@gmail.com>
2020-10-28 13:56:18 +0200

    rearranged the code in the file

--- submissions/benjaminKamena/database/init.sql
+++ submissions/benjaminKamena/database/init.sql
@@ -5,82 +5,16 @@
 -- --------------------------------------------------------
 
 --
--- Table structure for table `categories`
+-- Table structure for table `user_admins`
 --
 
-CREATE TABLE `categories` (
-  `category_id` int(11) NOT NULL AUTO_INCREMENT,
-  `category_name` varchar(100) NOT NULL,
-  `category_desc` text NOT NULL,
-  `category_image` text NOT NULL,
-  PRIMARY KEY (`category_id`)
-);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `contact_details`
---
-
-CREATE TABLE `contact_details` (
-  `contact_details_id` int(11) NOT NULL AUTO_INCREMENT,
-  `street_address` varchar(255) NOT NULL,
-  `city` varchar(100) NOT NULL,
-  `state` varchar(100) NOT NULL,
-  `zip_code` varchar(100) DEFAULT NULL,
-  `country` varchar(100) NOT NULL,
-  `phone_number` varchar(100) NOT NULL,
-  PRIMARY KEY (`contact_details_id`)
-);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `customers`
---
-
-CREATE TABLE `customers` (
-  `customer_id` int(11) NOT NULL AUTO_INCREMENT,
-  `customer_first_name` varchar(100) NOT NULL,
-  `customer_last_name` varchar(100) NOT NULL,
-  `customer_email_address` varchar(100) NOT NULL,
-  `customer_password` varchar(100) NOT NULL,
-  `customer_contact_address_id` int(11) NOT NULL,
-  `customer_order_id` int(11) NOT NULL,
-  PRIMARY KEY (`customer_id`),
-  FOREIGN KEY (`customer_contact_address_id`) REFERENCES `contact_details` (`contact_details_id`),
-  FOREIGN KEY (`customer_order_id`) REFERENCES `orders` (`order_id`)
-);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `orders`
---
-
-CREATE TABLE `orders` (
-  `order_id` int(11) NOT NULL AUTO_INCREMENT,
-  `order_amount` decimal(10,2) NOT NULL,
-  `order_date` date NOT NULL,
-  PRIMARY KEY (`order_id`)
-);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `order_items`
---
-
-CREATE TABLE `order_items` (
-  `order_items_id` int(11) NOT NULL AUTO_INCREMENT,
-  `order_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  `order_item_quantity` int(11) NOT NULL,
-  `unit_price` decimal(10,2) NOT NULL,
-  `total_amount` decimal(10,2) NOT NULL,
-  PRIMARY KEY (`order_items_id`),
-  FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
-  FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`)
+CREATE TABLE `user_admins` (
+  `admin_id` int(11) NOT NULL AUTO_INCREMENT,
+  `admin_name` varchar(100) NOT NULL,
+  `email_address` varchar(100) NOT NULL,
+  `admin_password` varchar(200) NOT NULL,
+  `phone_number` varchar(20) NOT NULL,
+  PRIMARY KEY (`admin_id`)
 );
 
 -- --------------------------------------------------------
@@ -120,14 +54,79 @@ CREATE TABLE `stock_levels` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_admins`
+-- Table structure for table `categories`
 --
 
-CREATE TABLE `user_admins` (
-  `admin_id` int(11) NOT NULL AUTO_INCREMENT,
-  `admin_name` varchar(100) NOT NULL,
-  `email_address` varchar(100) NOT NULL,
-  `admin_password` varchar(200) NOT NULL,
-  `phone_number` varchar(20) NOT NULL,
-  PRIMARY KEY (`admin_id`)
+CREATE TABLE `categories` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(100) NOT NULL,
+  `category_desc` text NOT NULL,
+  `category_image` text NOT NULL,
+  PRIMARY KEY (`category_id`)
+);
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `customer_id` int(11) NOT NULL AUTO_INCREMENT,
+  `customer_first_name` varchar(100) NOT NULL,
+  `customer_last_name` varchar(100) NOT NULL,
+  `customer_email_address` varchar(100) NOT NULL,
+  `customer_password` varchar(100) NOT NULL,
+  `customer_contact_address_id` int(11) NOT NULL,
+  `customer_order_id` int(11) NOT NULL,
+  PRIMARY KEY (`customer_id`),
+  FOREIGN KEY (`customer_contact_address_id`) REFERENCES `contact_details` (`contact_details_id`),
+  FOREIGN KEY (`customer_order_id`) REFERENCES `orders` (`order_id`)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_details`
+--
+
+CREATE TABLE `contact_details` (
+  `contact_details_id` int(11) NOT NULL AUTO_INCREMENT,
+  `street_address` varchar(255) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `state` varchar(100) NOT NULL,
+  `zip_code` varchar(100) DEFAULT NULL,
+  `country` varchar(100) NOT NULL,
+  `phone_number` varchar(100) NOT NULL,
+  PRIMARY KEY (`contact_details_id`)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_amount` decimal(10,2) NOT NULL,
+  `order_date` date NOT NULL,
+  PRIMARY KEY (`order_id`)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `order_items_id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `order_item_quantity` int(11) NOT NULL,
+  `unit_price` decimal(10,2) NOT NULL,
+  `total_amount` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`order_items_id`),
+  FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
+  FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`)
 );
