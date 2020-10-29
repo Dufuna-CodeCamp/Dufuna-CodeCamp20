efccc2988e20e90c6f5ac8c840d8348627235827
Heyeoler <peteriyiola@gmail.com>
2020-10-29 09:47:59 +0100

    editsss

--- submissions/peterIyiola/DOM/signup.js
+++ submissions/peterIyiola/DOM/signup.js
@@ -29,37 +29,27 @@ if (field.value === '') {
 }
 //password function declaration
 function validatePassword(password, confirmPassword, event) {
-if ((password.value && confirmPassword.value) === "") {
-    password.nextElementSibling.innerHTML = "Please enter your password";
-    confirmPassword.nextElementSibling.innerHTML = "Please confirm your password";
-    event.preventDefault();
-    return false;
-} else if ((!password.value.match(passwordRegex)) && (!confirmPassword.value.match(passwordRegex))) {
-    password.nextElementSibling.innerHTML = "Your password should have a minimum of 6 characters, 1 capital letter, 1 special character eg @ and 1 number";
-    confirmPassword.nextElementSibling.innerHTML = "Your password should have a minimum of 6 characters, 1 capital letter, 1 special character eg @ and 1 number";
-    event.preventDefault();
-    return false;
 
-} else if (password.value !== confirmPassword.value) {
+  if (password.value !== confirmPassword.value) {
     password.nextElementSibling.innerHTML = "Password do not match!";
     confirmPassword.nextElementSibling.innerHTML = "Password do not match!";
     event.preventDefault();
     return false;
-} else {
-    password.nextElementSibling.innerHTML = "";
-    confirmPassword.nextElementSibling.innerHTML = "";
-    return true;
-}
+} 
+
 }
 
 
 //called out the functions for submission
 function validateForm(e) {
-validatePassword(password, confirmPassword, e);
-validation(phoneNumber, phoneNumberRegex, e, "please enter your phone number", "please enter a valid phone number");
-validation(email, emailRegex, e, "please enter your email address", "please enter a valid email address");
+
 validation(firstName, nameRegex, e, "please enter your first name", "please enter a valid first name");
 validation(lastName, nameRegex, e, "please enter your last name", "please enter a valid last name");
+validation(phoneNumber, phoneNumberRegex, e, "please enter your phone number", "please enter a valid phone number");
+validation(email, emailRegex, e, "please enter your email address", "please enter a valid email address");
+validation(password ,passwordRegex, e, "please enter your password", "Your password should have a minimum of 6 characte, 1 capital letter, 1 special character eg @ and 1 number");
+validation(confirmPassword,passwordRegex, e, "please confirm your password", "Your password should have a minimum of 6 characters, 1 capital letter, 1 special character eg @ and 1 number");
+validatePassword(password,confirmPassword,e)
 return true;
 
 }
