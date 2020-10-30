0b8cbbaffc5a5e6098722110abd55982503b8506
kamena1994 <benjii1964@gmail.com>
2020-10-30 13:45:29 +0200

    modified the database file

--- submissions/benjaminKamena/database/init.sql
+++ submissions/benjaminKamena/database/init.sql
@@ -3,11 +3,9 @@
 --
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `user_admins`
 --
-
 CREATE TABLE `user_admins` (
   `admin_id` int(11) NOT NULL AUTO_INCREMENT,
   `admin_name` varchar(100) NOT NULL,
@@ -18,11 +16,21 @@ CREATE TABLE `user_admins` (
 );
 
 -- --------------------------------------------------------
+--
+-- Table structure for table `categories`
+--
+CREATE TABLE `categories` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(100) NOT NULL,
+  `category_desc` text NOT NULL,
+  `category_image` text NOT NULL,
+  PRIMARY KEY (`category_id`)
+);
 
+-- --------------------------------------------------------
 --
 -- Table structure for table `products`
 --
-
 CREATE TABLE `products` (
   `product_id` int(11) NOT NULL AUTO_INCREMENT,
   `product_name` varchar(100) NOT NULL,
@@ -31,45 +39,44 @@ CREATE TABLE `products` (
   `product_unit_price` decimal(6,2) NOT NULL,
   `admin_id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL,
+  `total_quantity` int(11) NOT NULL,
+  `status` varchar(100) NOT NULL,
   `stock_id` int(11) NOT NULL,
   PRIMARY KEY (`product_id`),
-  FOREIGN KEY (`stock_id`) REFERENCES `stock_levels` (`stock_id`),
   FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`),
   FOREIGN KEY (`admin_id`) REFERENCES `user_admins` (`admin_id`)
 );
 
 -- --------------------------------------------------------
-
 --
--- Table structure for table `stock_levels`
+-- Table structure for table `contact_details`
 --
-
-CREATE TABLE `stock_levels` (
-  `stock_id` int(11) NOT NULL AUTO_INCREMENT,
-  `total_quantity` int(11) NOT NULL,
-  `status` varchar(100) NOT NULL,
-  PRIMARY KEY (`stock_id`)
+CREATE TABLE `contact_details` (
+  `contact_details_id` int(11) NOT NULL AUTO_INCREMENT,
+  `street_address` varchar(255) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `state` varchar(100) NOT NULL,
+  `zip_code` varchar(100) DEFAULT NULL,
+  `country` varchar(100) NOT NULL,
+  `phone_number` varchar(100) NOT NULL,
+  PRIMARY KEY (`contact_details_id`)
 );
 
 -- --------------------------------------------------------
-
 --
--- Table structure for table `categories`
+-- Table structure for table `orders`
 --
-
-CREATE TABLE `categories` (
-  `category_id` int(11) NOT NULL AUTO_INCREMENT,
-  `category_name` varchar(100) NOT NULL,
-  `category_desc` text NOT NULL,
-  `category_image` text NOT NULL,
-  PRIMARY KEY (`category_id`)
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_amount` decimal(10,2) NOT NULL,
+  `order_date` date NOT NULL,
+  PRIMARY KEY (`order_id`)
 );
--- --------------------------------------------------------
 
+-- --------------------------------------------------------
 --
 -- Table structure for table `customers`
 --
-
 CREATE TABLE `customers` (
   `customer_id` int(11) NOT NULL AUTO_INCREMENT,
   `customer_first_name` varchar(100) NOT NULL,
@@ -84,41 +91,9 @@ CREATE TABLE `customers` (
 );
 
 -- --------------------------------------------------------
-
---
--- Table structure for table `contact_details`
---
-
-CREATE TABLE `contact_details` (
-  `contact_details_id` int(11) NOT NULL AUTO_INCREMENT,
-  `street_address` varchar(255) NOT NULL,
-  `city` varchar(100) NOT NULL,
-  `state` varchar(100) NOT NULL,
-  `zip_code` varchar(100) DEFAULT NULL,
-  `country` varchar(100) NOT NULL,
-  `phone_number` varchar(100) NOT NULL,
-  PRIMARY KEY (`contact_details_id`)
-);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `orders`
---
-
-CREATE TABLE `orders` (
-  `order_id` int(11) NOT NULL AUTO_INCREMENT,
-  `order_amount` decimal(10,2) NOT NULL,
-  `order_date` date NOT NULL,
-  PRIMARY KEY (`order_id`)
-);
-
--- --------------------------------------------------------
-
 --
 -- Table structure for table `order_items`
 --
-
 CREATE TABLE `order_items` (
   `order_items_id` int(11) NOT NULL AUTO_INCREMENT,
   `order_id` int(11) NOT NULL,
