bdc0775dc2be6d221604c5dbcaaf4e11d6cf7f7c
Victor-Aienobe <viktornobe@gmail.com>
2020-11-04 13:27:37 +0100

    effected the necessary corrections

--- submissions/victorAienobe/database/transport.sql
+++ submissions/victorAienobe/database/transport.sql
@@ -30,30 +30,7 @@ CREATE TABLE trips (
 CREATE TABLE accidents (
 	id INT NOT NULL AUTO_INCREMENT,
     passenger_id INT NOT NULL,
-    survived VARCHAR(5) NOT NULL,
-    died VARCHAR(5) NOT NULL,
+    survived TINYINT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (passenger_id) REFERENCES passengers (id) ON DELETE CASCADE
 );
-
-
--- inserted values into the passengers table
-INSERT INTO passengers (full_name, sex, age) 
-VALUES ('jon snow', 'male', '25'),
-('arya stark', 'female', '18'),
-('petyr baelish', 'male', '40'),
-('jamie lannister', 'male', '35');
-
--- inserted values into the trips table
-INSERT INTO trips (class, ticket_number, trip_fare, cabin, passenger_id, number_of_parents_children, number_of_siblings_spouses, embarkation_point)
-VALUES ('1', '001', '2000', 'FOO1', '1', '1', '0', 'winterfell'),
-('2', '002', '4000', 'F002', '2', '0', '1', 'kings landing'),
-('3', '003', '8000', 'F003', '3', '1', '0', 'westeros'),
-('2', '004', '10000', 'F004', '4', '1', '0', 'casterly rock');
-
--- inserted values into the accidents table
-INSERT INTO accidents (passenger_id, survived, died) 
-VALUES ('1', 'yes', 'no'),
-('2', 'yes', 'no'),
-('3', 'no', 'yes'),
-('4', 'no', 'yes');
