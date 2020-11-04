2a862ac680a544bacf7fa97d64e5b3088cc45c1d
faithakinbobola <faithakinbobola2@gmail.com>
2020-11-04 12:02:57 +0100

    Queried the transportation company database

--- submissions/akinbobolaFaith/database/companyDatabase.sql
+++ submissions/akinbobolaFaith/database/companyDatabase.sql
@@ -6,7 +6,7 @@ CREATE TABLE passengers (
 	id INT NOT NULL AUTO_INCREMENT,
 	fullname VARCHAR(200) NOT NULL,
 	sex VARCHAR(50) NOT NULL,
-	age INT(50),
+	age INT,
 	PRIMARY KEY(id)
 );
 
@@ -15,11 +15,11 @@ CREATE TABLE passengers (
 CREATE TABLE travel_details (
 	id INT NOT NULL AUTO_INCREMENT,
 	passengers_id INT NOT NULL,
-	passenger_class INT(10) NOT NULL,
+	passenger_class INT NOT NULL,
 	ticket_number VARCHAR(100) NOT NULL,
 	trip_fare DECIMAL(20,2) NOT NULL,
-	parents_or_children_abroad INT(50) NOT NULL,
-	siblings_or_spouse_abroad INT(50) NOT NULL,
+	parents_or_children_abroad INT NOT NULL,
+	siblings_or_spouse_abroad INT NOT NULL,
 	cabin_class VARCHAR(50) NOT NULL,
 	embarkation_point VARCHAR(200) NOT NULL,
 	PRIMARY KEY (id),
@@ -31,8 +31,8 @@ CREATE TABLE travel_details (
 CREATE TABLE accidents (
 	id INT NOT NULL AUTO_INCREMENT,
 	passengers_id INT NOT NULL,
-	passenger_status BOOLEAN NOT NULL,
+	survived BOOLEAN NOT NULL,
 	PRIMARY KEY (id),
-	FOREIGN KEY (passengers_id) REFERENCES passengers(id)
+	FOREIGN KEY (passengers_id) REFERENCES passengers (id)
 );
 
