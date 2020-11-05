fa900ad6e1af86b74fb084933f85208f551fd2ea
israel-force <oladosuisraelolayinka@gmail.com>
2020-11-05 13:31:10 +0100

    used left join to replace multiple select

--- submissions/israelOladosu/database/transportation.sql
+++ submissions/israelOladosu/database/transportation.sql
@@ -2,13 +2,6 @@ CREATE DATABASE transporation;
 
 use transporation;
 
--- classes table
-CREATE TABLE classes (
-    id INT NOT NULL AUTO_INCREMENT,
-    class_level INT NOT NULL,
-    PRIMARY KEY(id)
-);
-
 -- passangers table
 CREATE TABLE passengers (
     id INT NOT NULL AUTO_INCREMENT,
@@ -30,7 +23,6 @@ CREATE TABLE trips (
     cabin VARCHAR(20),
     embarkation VARCHAR(255) NOT NULL,
     PRIMARY KEY(id),
-    FOREIGN KEY(passenger_class) REFERENCES classes(id) ON DELETE CASCADE,
     FOREIGN KEY(passenger_id) REFERENCES passengers(id) ON DELETE CASCADE
 );
 
@@ -43,27 +35,3 @@ CREATE TABLE accidents (
     PRIMARY KEY(id),
     FOREIGN KEY(passenger_id) REFERENCES passengers(id) ON DELETE CASCADE
 );
-
-
---------------------------------INSERT INTO TABLES----------------------------------
-
--- insert into classes table 
-INSERT INTO classes (class_level)
-VALUES(1),(2),(3);
-
---insert into passangers table
-INSERT INTO passengers (name, sex)
-VALUES('Dele Amid', 'Male'),
-      ('Rita Godspower', 'Female');
-      
-
--- insert into trips table
-INSERT INTO trips (passenger_id, passenger_class, passenger_ticket, trip_fare, number_of_parents_children, number_of_siblings_spouse, point_of_embarkation, cabin)
-VALUES(1, 1, 'PC174599', 500000, 4, 0, 'Abuja', 'C103'),
-      (2, 1, 'PC56785', 500000, 0, 2, 'Abuja', 'C104');
-
-
---insert into accidents
-INSERT INTO accidents ( passenger_id, survived)
-VALUES(1, 1),
-      (2, 1);
