2944c8671d99331169f6fcfd1c1892dbc422428f
Adeyemi juwon <samju7778@gmail.com>
2020-11-06 23:44:53 +0100

    remove values

--- submissions/adeyemiJuwon/database/transportation.sql
+++ submissions/adeyemiJuwon/database/transportation.sql
@@ -16,61 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `passengers`
---
-
-DROP TABLE IF EXISTS `passengers`;
-CREATE TABLE `passengers` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `Full_name` varchar(150) NOT NULL,
-  `sex` varchar(6) NOT NULL,
-  `age` int(10)  NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `passengers`
---
-INSERT INTO `passengers` VALUES 
-(1,'juwon adeyemi','male',29),
-(2,'samuel owoso','male'),
-(3,'nuru kalijah','male'),
-(4,'itunu adekoya','male',28),
-(5,'feyi olufemi','female'),
-(6,'temidayo alara','male',30),
-(7,'seye omodiji','female',25);
-
---
--- Table structure for table `trips`
---
-
-DROP TABLE IF EXISTS `trips`;
-CREATE TABLE `trips` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `passenger_class` int(3) NOT NULL,
-  `passenger_ticket_number` varchar(100) NOT NULL,
-  `trip_fare` decimal(20,4) NOT NULL,
-  `assigned_cabin` varchar(50) NOT NULL,
-  `number_of_parents_children` int(11) NOT NULL,
-  `number_of_siblings_spouses` int(11) NOT NULL,
-  `point_of_embarkation` varchar(50) NOT NULL,
-  `passenger_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_trips_passengers_idx` (`passenger_id`),
-  CONSTRAINT `fk_trips_passengers_idx` FOREIGN KEY (`passenger_id`) REFERENCES `passengers` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `trips`
---
-
-INSERT INTO `trips` VALUES
- (1,1,'112301',300000.0000,'A102', 4, 1,'Lagos',1),
- (2,2,'112302',400000.0000, 'B104', 4, 2,'Lagos',2),
- (3,3,'112303',500000.0000, 'A104', 4, 0,'lagos',3),
- (4,1,'112304',300000.0000, 'B104', 4,1,'Lagos',4);
-
---
 -- Table structure for table `accidents`
 --
 
@@ -83,15 +28,46 @@ CREATE TABLE `accidents` (
   KEY `fk_accidents_passengers_idx` (`passenger_id`),
   CONSTRAINT `fk_accidents_passengers` FOREIGN KEY (`passenger_id`) REFERENCES `passengers` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `accidents`
+-- Table structure for table `passengers`
 --
 
-INSERT INTO `accidents` VALUES 
-(1,'yes',1),
-(2,'yes',2),
-(3,'no',4);
+DROP TABLE IF EXISTS `passengers`;
+CREATE TABLE `passengers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Full_name` varchar(150) NOT NULL,
+  `sex` varchar(6) NOT NULL,
+  `age` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `trips`
+--
+
+DROP TABLE IF EXISTS `trips`;
+CREATE TABLE `trips` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `passenger_class` int(3) NOT NULL,
+  `passenger_ticket_number` varchar(100) NOT NULL,
+  `trip_fare` decimal(20,4) NOT NULL,
+  `assigned_cabin` varchar(50) DEFAULT NULL,
+  `number_of_parents_children` int(11) NOT NULL,
+  `number_of_siblings_spouses` int(11) NOT NULL,
+  `point_of_embarkation` varchar(50) NOT NULL,
+  `passenger_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_trips_passengers_idx` (`passenger_id`),
+  CONSTRAINT `fk_trips_passengers_idx` FOREIGN KEY (`passenger_id`) REFERENCES `passengers` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -101,4 +77,4 @@ INSERT INTO `accidents` VALUES
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-10-30 19:55:21
+-- Dump completed on 2020-11-06 23:33:03
