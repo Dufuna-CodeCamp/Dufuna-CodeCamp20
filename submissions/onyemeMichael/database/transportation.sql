23343e2de2c76f0bab20e202266a217b5057782f
=MYKEL <=onyememykel@gmail.com>
2020-11-06 01:00:08 -0800

    modified file

--- submissions/onyemeMichael/database/transportation.sql
+++ submissions/onyemeMichael/database/transportation.sql
@@ -3,15 +3,15 @@ CREATE DATABASE transportation;
 USE transportation;
 
 -- Classes Table
-CREATE TABLE classes (
-	id INT NOT NULL AUTO_INCREMENT,
-    class_level INT NOT NULL,
-    PRIMARY KEY(id)
-);
+-- CREATE TABLE classes (
+-- 	id INT NOT NULL AUTO_INCREMENT,
+--     class_level INT NOT NULL,
+--     PRIMARY KEY(id)
+-- );
 
 -- Insert into classes table 
-INSERT INTO classes (class_level)
-VALUES(1),(2),(3);
+-- INSERT INTO classes (class_level)
+-- VALUES(1),(2),(3);
 
 -- Passengers Table
 CREATE TABLE passengers (
@@ -24,11 +24,11 @@ CREATE TABLE passengers (
 );
 
 -- Insert into passangers table
-INSERT INTO passengers (firstname, lastname, sex, age)
-VALUES('Isreal', 'John', 'Male', 22),
-      ('Roah', 'Godswill', 'Female', 20),
-      ('Paul', 'Uche', 'Male', 24),
-      ('Mayowa', 'Bayo', 'Female', 19);
+-- INSERT INTO passengers (firstname, lastname, sex, age)
+-- VALUES('Isreal', 'John', 'Male', 22),
+--       ('Roah', 'Godswill', 'Female', 20),
+--       ('Paul', 'Uche', 'Male', 24),
+--       ('Mayowa', 'Bayo', 'Female', 19);
 
 -- Trips Table
 CREATE TABLE trips (
@@ -46,42 +46,40 @@ CREATE TABLE trips (
 );
 
 -- Insert into trips table
-INSERT INTO trips (passenger_class, passenger_ticket, trip_fare, number_of_parents_children, number_of_siblings_spouses, point_of_embarkation, passenger_id)
-VALUES(1, 'DC43569', 70000.0000, 2, 0, 'Lagos', 1),
-      (2, 'DC76682', 50000.0000, 0, 1, 'Lagos', 2),
-      (1, 'DC36844', 70000.0000, 4, 1, 'Lagos', 3),
-      (2, 'DC56483', 50000.0000, 2, 1, 'Lagos', 4);
+-- INSERT INTO trips (passenger_class, passenger_ticket, trip_fare, number_of_parents_children, number_of_siblings_spouses, point_of_embarkation, passenger_id)
+-- VALUES(1, 'DC43569', 70000.0000, 2, 0, 'Lagos', 1),
+--       (2, 'DC76682', 50000.0000, 0, 1, 'Lagos', 2),
+--       (1, 'DC36844', 70000.0000, 4, 1, 'Lagos', 3),
+--       (2, 'DC56483', 50000.0000, 2, 1, 'Lagos', 4);
 
 
 -- Cabin classes Table
-CREATE TABLE cabin_classes (
-	id INT NOT NULL AUTO_INCREMENT,
-    cabin_code VARCHAR(15) NOT NULL,
-    trip_id INT NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(trip_id) REFERENCES trips(id) ON DELETE CASCADE
-);
+-- CREATE TABLE cabin_classes (
+-- 	id INT NOT NULL AUTO_INCREMENT,
+--     cabin_code VARCHAR(15) NOT NULL,
+--     trip_id INT NOT NULL,
+--     PRIMARY KEY(id),
+--     FOREIGN KEY(trip_id) REFERENCES trips(id) ON DELETE CASCADE
+-- );
 
 -- Insert into cabin_classes
-INSERT INTO cabin_classes(cabin_code, trip_id)
-VALUES('D65', 1),
-	  ('D54', 2),
-      ('D39', 3),
-      ('D42', 4);
+-- INSERT INTO cabin_classes(cabin_code, trip_id)
+-- VALUES('D65', 1),
+-- 	  ('D54', 2),
+--       ('D39', 3),
+--       ('D42', 4);
 
 -- Accident Table
 CREATE TABLE accidents (
 	id INT NOT NULL AUTO_INCREMENT,
     survived BOOLEAN NOT NULL,
     died BOOLEAN NOT NULL,
-    passenger_Id INT NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(passenger_id) REFERENCES passengers(id) ON DELETE CASCADE
+    PRIMARY KEY(id)
 );
 
 -- Insert into accidents
-INSERT INTO accidents (survived, died, passenger_id)
-VALUES(4, 0, 1),
-      (4, 0, 2),
-      (4, 0, 3),
-      (4, 0, 4);
+-- INSERT INTO accidents (survived, died, passenger_id)
+-- VALUES(4, 0, 1),
+--       (4, 0, 2),
+--       (4, 0, 3),
+--       (4, 0, 4);
