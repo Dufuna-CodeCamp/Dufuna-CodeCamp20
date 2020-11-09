f71b1bf088b7eb4b73aef7459d392619499bd7f5
kamena1994 <benjii1964@gmail.com>
2020-11-09 16:04:11 +0200

    modified the transportation.sql file

--- submissions/benjaminKamena/database/transportation_company.sql
+++ submissions/benjaminKamena/database/transportation_company.sql
@@ -7,14 +7,14 @@ SHOW TABLES;
 
 -- --------------------------------------------------------
 --
--- Table structure for table `passagers`
+-- Table structure for table `passegers`
 --
-CREATE TABLE `passagers` (
-  `passager_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `passegers` (
+  `passeger_id` int(11) NOT NULL AUTO_INCREMENT,
   `full_name` varchar(100) NOT NULL,
   `sex` varchar(10) NOT NULL,
   `age` int(11) NULL,
-  PRIMARY KEY (`passager_id`)
+  PRIMARY KEY (`passeger_id`)
 );
 
 -- --------------------------------------------------------
@@ -23,16 +23,16 @@ CREATE TABLE `passagers` (
 --
 CREATE TABLE `travel_details` (
   `travel_detail_id` int(11) NOT NULL AUTO_INCREMENT,
-  `passanger_class` int(11) NOT NULL,
-  `passanger_ticket_number` varchar(50) NOT NULL,
-  `passager_trip_fare` decimal(10,2) NOT NULL,
+  `passenger_class` int(11) NOT NULL,
+  `passenger_ticket_number` varchar(50) NOT NULL,
+  `passeger_trip_fare` decimal(10,2) NOT NULL,
   `number_of_parent_or_children aboard` int(11) NOT NULL,
   `number_of_siblings_or_spouses aboard` int(11) NOT NULL,
   `assigned_cabin` varchar(10) NOT NULL,
   `point_of_embarkation` varchar(100) NOT NULL,
-  `passagerID` int(11) NOT NULL,
+  `passegerID` int(11) NOT NULL,
   PRIMARY KEY (`travel_detail_id`),
-  FOREIGN KEY (`passagerID`) REFERENCES `passagers` (`passager_id`)
+  FOREIGN KEY (`passegerID`) REFERENCES `passegers` (`passeger_id`)
 );
 
 -- --------------------------------------------------------
@@ -41,9 +41,8 @@ CREATE TABLE `travel_details` (
 --
 CREATE TABLE `flight_statuses` (
   `flight_statuses_id` int(11) NOT NULL AUTO_INCREMENT,
-  `passanger_survived` varchar(50) NOT NULL,
-  `passanger_not_survived` varchar(50) NOT NULL,
-  `passagerID` int(11) NOT NULL,
+  `passenger_survived` BOOLEAN,
+  `passegerID` int(11) NOT NULL,
   PRIMARY KEY (`flight_statuses_id`),
-  FOREIGN KEY (`passagerID`) REFERENCES `passagers` (`passager_id`)
+  FOREIGN KEY (`passegerID`) REFERENCES `passegers` (`passeger_id`)
 );
