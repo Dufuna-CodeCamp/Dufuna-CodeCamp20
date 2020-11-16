3a77adc406ce225c5884bdc20b51bcccf9a6aea9
Onyinye101 <64909274+Onyinye101@users.noreply.github.com>
2020-11-16 12:38:24 +0100

    updated softwaredeveloper.js

--- submissions/onyinyeChukwuka/javascript/softwaredeveloper.js
+++ submissions/onyinyeChukwuka/javascript/softwaredeveloper.js
@@ -1,4 +1,4 @@
-const positiveDifference = 123 - 7;
+const positiveDifference = Math.abs(7 - 123);
 for (let element = 1; element < positiveDifference; element++) {
     if (element % 3 === 0 && element % 5 === 0) {
         console.log("Software Developer"); 
