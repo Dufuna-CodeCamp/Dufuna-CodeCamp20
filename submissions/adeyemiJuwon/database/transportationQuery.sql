4305679e3cc0c12e8e8597888538db22a9bdf715
Adeyemi juwon <samju7778@gmail.com>
2020-11-17 11:21:07 +0100

    remove sql default mode to complete importation of my data

--- submissions/adeyemiJuwon/database/transportationQuery.sql
+++ submissions/adeyemiJuwon/database/transportationQuery.sql
@@ -4,13 +4,13 @@ USE transportation;
 SELECT COUNT(*) 
 FROM accidents 
 WHERE survived = 1;
--- answer = 290
+-- answer = 342
 
 -- total number of passengers that did not survive
 SELECT COUNT(*) 
 FROM accidents 
 WHERE survived = 0;
--- answer = 424
+-- answer = 549
 
 -- the name and sex of passengers under the age of 27 that embarked Queenstown and Cherbourg
 SELECT passengers.full_name, passengers.sex 
@@ -27,7 +27,7 @@ LEFT JOIN trips
 ON trips.passenger_id = accidents.passenger_id 
 WHERE point_of_embarkation = 'S' 
 AND survived = 1;
--- answer = 202
+-- answer = 218
 
 -- The id and name of passengers who paid a fare greater than $100 and above the age of 35 with siblings or spouses on board
 SELECT passengers.id, passengers.full_name 
@@ -46,3 +46,4 @@ ON passengers.id = trips.passenger_id
 WHERE trip_fare > 100 
 AND age > 35 
 AND number_of_siblings_spouses != 0;
+-- answer = 9
