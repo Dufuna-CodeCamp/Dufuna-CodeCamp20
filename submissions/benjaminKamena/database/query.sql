f4f36db922f6ef00fd41104f6c576335228e878d
kamena1994 <benjii1964@gmail.com>
2020-11-19 13:54:05 +0200

    made changes on my query.sql file

--- submissions/benjaminKamena/database/query.sql
+++ submissions/benjaminKamena/database/query.sql
@@ -19,10 +19,10 @@ WHERE survived = 0;
 -- After running the query above the answer is 549.
 
 -- Q3. Get the name and sex of passengers under the age of 27 that embarked at Queenstown and Cherbourg?
-SELECT passeger.name, passeger.sex 
-FROM passeger
+SELECT passenger.name, passenger.sex 
+FROM passenger
 INNER JOIN traveldetail
-ON passeger.PassegerId = traveldetail.PassegerId
+ON passenger.PassengerId = traveldetail.PassengerId
 WHERE age < 27 
 AND Embarked = 'Q'
 OR Embarked = 'C';
@@ -31,21 +31,18 @@ OR Embarked = 'C';
 SELECT COUNT(*)
 FROM traveldetail
 RIGHT JOIN survival
-ON survival.PassegerId = traveldetail.PassegerId
+ON survival.PassengerId = traveldetail.PassengerId
 WHERE Embarked = 'S'
-AND survived = 1;
--- After running the query above the answer is 0.
+OR survived = 1;
+-- After running the query above the answer is 342.
 
 -- Q5. Get the id, name and the total number of passengers who paid a fare greater 
 -- than $100 and above the age of 35 had siblings or spouses on board?
-SELECT passeger.PassegerId, passeger.Name
-FROM passeger
+SELECT passenger.PassengerId, passenger.Name
+FROM passenger
 INNER JOIN traveldetail
-ON passeger.PassegerId = traveldetail.PassegerId
+ON passenger.PassengerId = traveldetail.PassengerId
 WHERE Fare > 100
 AND age > 35
   OR SibSp = 0;
   
-
-
-
