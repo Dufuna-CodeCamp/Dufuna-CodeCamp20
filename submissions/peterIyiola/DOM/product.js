5a2d05791fe82a9dc0ec01eceb10b339600a9462
Heyeoler <peteriyiola@gmail.com>
2020-11-20 19:30:27 +0100

    editts

--- submissions/peterIyiola/DOM/product.js
+++ submissions/peterIyiola/DOM/product.js
@@ -4,24 +4,24 @@ let quantity = document.getElementById('quantity'),
     totalCost = document.getElementById('totalCost'),
     buy = document.getElementById('buy'),
     summation = price.innerHTML * quantity.value;
-    totalCost = shippingCost.innerHTML + summation;
     
     function productCost(event)    {
         let regex = /^[0-9]+$/;
-        if(quantity.value <= 0 || !(quantity.value.match(regex)))   {
+        if(quantity.value <= 0 || !(quantity.value.match(regex)))  {
             quantity.nextElementSibling.innerHTML= "please enter a valid quantity";
             buy.disabled = true;
             event.preventDefault();
             return false;
         }
         else  {
-            
+
             quantity.nextElementSibling.innerHTML = '';
             buy.disabled = false;
             return true;
         }
 
     }
+
     function productPrice ()  {
         if (summation < 1000){
             shippingCost.innerHTML= 100;
@@ -29,13 +29,14 @@ let quantity = document.getElementById('quantity'),
         else if(summation >= 1000 && summation <= 20000){
             shippingCost.innerHTML = 0.15 * summation;
         }
-        else if (summation >=20000 && summation <= 100000 ){
+        else if (summation >20000 && summation <= 100000 ){
             shippingCost.innerHTML = 0.20 * summation;
         }
         else  {
-            shippingCost.innerHTML = 25000
+            shippingCost.innerHTML = 25000;
         }
-       
+        totalCost = shippingCost.innerHTML + summation;
+    
     }
 
     function validation(e) {
