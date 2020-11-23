dfcbf5edf67aa3c4c6871821cf4af7c5baaf6fe4
Heyeoler <peteriyiola@gmail.com>
2020-11-23 11:04:32 +0100

    productPrice

--- submissions/peterIyiola/DOM/product.js
+++ submissions/peterIyiola/DOM/product.js
@@ -2,44 +2,45 @@ let quantity = document.getElementById('quantity'),
     price = document.getElementById('price'),
     shippingCost = document.getElementById('shippingCost'),
     totalCost = document.getElementById('totalCost'),
-    summation = price.innerHTML * quantity.value ;
     buy = document.getElementById('buy');
-
+ 
    
-    function productCount(event) {
-        let regex =/^[0-9]+$/;
-        if (quantity.value < 0 || (!quantity.value.match(regex))) {
-        quantity.innerHTML = "Please enter a valid quantity";
+
+function  validation  (event)    {
+    let regex = /[^0-9]/g;
+    if (quantity.value < 1 || quantity.value == "" || (quantity.value.match(regex))){
         buy.disabled = true;
-        event.preventDefault();
+        shippingCost.innerHTML = 0;
+        totalCost.innerHTML = 0;
         return false;
-    }
-    else {
-        quantity.innerHTML = "";
+      } 
+      else {
         buy.disabled = false;
-        return true;
-    }
-        
-    }
-   
+        let summation = (price.innerHTML * quantity.value) ;
+        if (summation <= 1000) {
+          shippingCost.innerHTML = parseFloat(100);
+          totalCost.innerHTML =
+            parseFloat(summation) + parseFloat(shippingCost.innerHTML);
+          return true;
 
-    function shipping() {
-        if(summation < 1000) {
-            shippingCost.innerHTML = 100;
-        } else if(summation >= 1000 && sum <= 20000) {
-            shippingCost.innerHTML = 0.15 * sum;
-        } else if(summation > 20000 && sum <= 100000) {
-            shippingCost.innerHTML = 0.2 * sum;
-        } else {
-            shippingCost.innerHTML = 25000;
+        } else if (summation > 1000 && summation <= 20000) {
+          shippingCost.innerHTML = parseFloat(0.15 * summation  );
+          totalCost.innerHTML =
+            parseFloat(summation) + parseFloat(shippingCost.innerHTML);
+          return true;
+        } 
+        else if (summation > 20000 && summation <= 100000) {
+          shippingCost.innerHTML = parseFloat(0.2 * summation );
+          totalCost.innerHTML =
+            parseFloat(summation) + parseFloat(shippingCost.innerHTML);
+          return true;
+        } 
+        else {
+          shippingCost.innerHTML = 25000;
+          totalCost.innerHTML = parseFloat(summation) + parseFloat(shippingCost.innerHTML);
+          return true;
         }
-        totalCost = shippingCost.innerHTML + summation;
-    }
+      }
+    };
     
-    function validation(e) {
-        productCount(e);
-        shipping();
-        return true;
-    }
-    
-    quantity.addEventListener('input', validation);
+    quantity.addEventListener("input", validation);
