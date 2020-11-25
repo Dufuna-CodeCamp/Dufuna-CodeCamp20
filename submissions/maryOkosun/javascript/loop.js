8156991b706beaaa17787fb57d7027dec475c276
marynoir <okosunmaryeghonghon@gmail.com>
2020-11-25 22:24:08 +0100

    loop task

---
+++ submissions/maryOkosun/javascript/loop.js
@@ -0,0 +1,14 @@
+for (var i = 1; i < 116; i++) {
+	if (i % 3 === 0 && i % 5 === 0){
+		console.log("Software Developer");
+}
+	else if (i % 5 === 0){
+		console.log("Developer");
+}
+	else if (i % 3 === 0 ){
+		console.log("Software");
+}
+	else{
+		console.log(i);
+	}
+}
