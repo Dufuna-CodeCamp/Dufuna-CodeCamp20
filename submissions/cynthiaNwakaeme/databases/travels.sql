238ed528fd92c7ba18edba433bd0a904624a6e7e
Nwakaeme Cynthia Ebere <nwakaemecynthia@gmail.com>
2020-11-26 14:30:23 +0100

    Update travels.sql
    Modified Age datatypes to it call allow for months

--- submissions/cynthiaNwakaeme/databases/travels.sql
+++ submissions/cynthiaNwakaeme/databases/travels.sql
@@ -6,29 +6,29 @@ USE travel_company;
 CREATE TABLE passengers (
 	passenger_id INT NOT NULL AUTO_INCREMENT,
 	full_name VARCHAR(50) NOT NULL,
-	sex VARCHAR(6) NOT NULL,
-	age SMALLINT,
+	sex ENUM('male', 'female') NOT NULL,
+	age FLOAT(3,2), -- allows for records with months sepcified as decimals
 	PRIMARY KEY (passenger_id)
 );
 CREATE TABLE trips (
 	trip_id INT NOT NULL AUTO_INCREMENT,
-    passenger_id INT NOT NULL,
-	class TINYINT NOT NULL,
-    ticket_number VARCHAR(20) NOT NULL,
-    trip_fare DECIMAL(10,2) NOT NULL,
-    cabin VARCHAR(10),
-    number_of_parents_children INT NOT NULL,
-    number_of_siblings_spouses INT NOT NULL,
-    embarkation_point VARCHAR(20) NOT NULL,
-    PRIMARY KEY (trip_id),
-    FOREIGN KEY (passenger_id) REFERENCES passengers (passenger_id)
+	passenger_id INT NOT NULL,
+	class ENUM('1', '2', '3') NOT NULL,
+	ticket_number VARCHAR(20) NOT NULL,
+	trip_fare DECIMAL(10,2) NOT NULL,
+	cabin VARCHAR(10),
+	number_of_parents_children INT NOT NULL,
+	number_of_siblings_spouses INT NOT NULL,
+	embarkation_point VARCHAR(20) NOT NULL,
+	PRIMARY (trip_id),
+	FOREIGN KEY (passenger_id) REFERENCES passengers (passenger_id)
 );
 -- created table for event of accident
 
 CREATE TABLE accidents (
 	id INT NOT NULL AUTO_INCREMENT,
-    passenger_id INT NOT NULL,
-    status TINYINT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (passenger_id) REFERENCES passengers (passenger_id)
+	passenger_id INT NOT NULL,
+	survival_status TINYINT NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (passenger_id) REFERENCES passengers (passenger_id)
 );
