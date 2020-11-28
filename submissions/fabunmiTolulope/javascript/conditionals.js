ba8683cb99e8933e3ae8ca76f4c8e95bb2e15cb6
ToluEnny24 <ogunronbitolulope@gmail.com>
2020-11-28 15:47:13 +0100

    I have effected the changes

--- submissions/fabunmiTolulope/javascript/conditionals.js
+++ submissions/fabunmiTolulope/javascript/conditionals.js
@@ -1,6 +1,9 @@
 // The positive difference between 7 and 123 is 116 so this program will print numbers between 1 and 116
+let w = 7;
+let y = 123;
+z = y-w;
 
-for (var x = 1; x <= 116; x++) 
+for (var x = 1; x <= z; x++) 
 {
 	if (x% 3 === 0 && x% 5 === 0) 
 	{
