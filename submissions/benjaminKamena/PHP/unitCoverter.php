8ca6035abe05d9a24d18c76e7276ff4a75082200
kamena1994 <benjii1964@gmail.com>
2020-11-30 11:43:05 +0200

    changed the parameter name

--- submissions/benjaminKamena/PHP/unitCoverter.php
+++ submissions/benjaminKamena/PHP/unitCoverter.php
@@ -2,18 +2,18 @@
 
 <?php
 
- function unitCoverter($unit){
+ function unitCoverter($value){
 	 //kilos to pound
-	 $pound = 2.20462 * $unit;
-	 echo ("$unit kilo is equal to  $pound pound") . "<br>";
+	 $pound = 2.20462 * $value;
+	 echo ("$value kilo is equal to  $pound pound") . "<br>";
 	 
 	 //meter to yard
-	$yard = 1.09361 * $unit;
-	 echo ("$unit meter is equal to  $yard yard"). "<br>";
+	$yard = 1.09361 * $value;
+	 echo ("$value meter is equal to  $yard yard"). "<br>";
 	 
 	 //celcius to fahrenheit
-	 $fahrenheit = $unit * (9 / 5) + 32;
-	 echo ("$unit celcius is equal to  $fahrenheit fahrenheit") . "<br>";
+	 $fahrenheit = $value * (9 / 5) + 32;
+	 echo ("$value celcius is equal to  $fahrenheit fahrenheit") . "<br>";
  }
  unitCoverter(9);
 
