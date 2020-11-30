825c4413b965b57be48cb94f6c6a0d3610168a78
Onyeme Michael <onyememykel@gmail.com>
2020-11-30 11:01:50 +0100

    modified file

--- submissions/onyemeMichael/database/transportationQuery.sql
+++ submissions/onyemeMichael/database/transportationQuery.sql
@@ -1,16 +1,16 @@
 /** Total number of passengers who suvrvived **/
 SELECT COUNT(survived) FROM accidents WHERE survived = 1;
-/** answer: 269 **/
+/** answer: 342 **/
 
 /** Total number of passengers who did not survive **/
 SELECT COUNT(survived) FROM accidents WHERE survived = 0;
-/** answer: 419 **/
+/** answer: 549 **/
 
 /** name and sex of passengers under 27 that embarked at Queenstown and Cherbourg **/
 SELECT passengers.fullname, passengers.sex 
 FROM passengers 
 LEFT JOIN trips 
-ON passengers.id = trips.passenger_id
+ON passengers.passenger_id = trips.passenger_id
 WHERE age < 27 
 AND point_of_embarkation != 'S';
 
@@ -21,13 +21,13 @@ LEFT JOIN trips
 ON accidents.passenger_id = trips.passenger_id 
 WHERE trips.point_of_embarkation = 'S' 
 AND accidents.survived = 1;
-/** answer: 179 **/
+/** answer: 218 **/
 
 /** name, id and total number of passngers who paid a fee greater than $100 and above age of 35 had siblings or spouse on board **/
-SELECT passengers.id, passengers.fullname 
+SELECT passengers.passenger_id, passengers.fullname 
 FROM passengers
 LEFT JOIN trips 
-ON passengers.id = trips.passenger_id
+ON passengers.passenger_id = trips.passenger_id
 WHERE trips.trip_fare > 100 
 AND trips.number_of_siblings_spouses > 0 
 AND passengers.age > 35;
@@ -35,8 +35,8 @@ AND passengers.age > 35;
 /** the total number of passengers who paid a fee greater than $100 and above the age of 35 with siblings or spouses on board **/
 SELECT COUNT(*) FROM passengers
 LEFT JOIN trips
-ON passengers.id = trips.passenger_id
+ON passengers.passenger_id = trips.passenger_id
 WHERE trip_fare > 100
 AND age > 35 
 AND number_of_siblings_spouses  != 0;
-/** answer: 7 **/
+/** answer: 9 **/
