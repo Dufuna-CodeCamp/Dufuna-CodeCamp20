00924f961f91857c013335f6392082281faaed14
faithakinbobola <faithakinbobola2@gmail.com>
2020-12-02 23:42:18 +0100

    modified PHP unit converter

--- submissions/akinbobolaFaith/php/converter.php
+++ submissions/akinbobolaFaith/php/converter.php
@@ -1,17 +1,17 @@
 <?php
 function convert($value) {
     $pounds = $value * 2.205;
-    echo $pounds . "lb" . "<br>";
+    echo $pounds . "lb <br>";
 
     $yards = $value * 1.094;
-    echo $yards . "yards" . "<br>";
+    echo $yards . "yards <br>";
 
     $farenheit = ($value * 9/5) + 32;
-    echo $farenheit . "&deg" . "F" . "<br>";
+    echo $farenheit . "&deg F <br>";
 }
 
 convert(20);
 //44.1lb
-//21.28yards
+//21.88yards
 //68F
 ?>
