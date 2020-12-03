adfd7a11597dace1054f6f1826daeedeaf3aee57
kamena1994 <benjii1964@gmail.com>
2020-12-03 16:48:36 +0200

    reformated the code and removed the unnessary codes

--- submissions/benjaminKamena/PHP/hobby_array.php
+++ submissions/benjaminKamena/PHP/hobby_array.php
@@ -1,10 +1,8 @@
 <?php
 $eight_people = array("Memory","Esther","Rose","Baron","Friday","Mathews","Kebby","Wendy");
 $hobbies = array("Social media","Singing Gospel","Watching Movies","Window Shopping","Making Money","Driving","Making friends","Playing Pc Games");
-$result = array();
-foreach($eight_people as $key=>$value){
-	$val = $hobbies[$key];
-	$result[$key]=array($value, $val);
-	echo "My name is {$value}. I love {$val} <br>";
+$result = array_combine($eight_people, $hobbies);
+foreach($result as $eight_people => $hobbies){
+	echo "My name is $eight_people. I love $hobbies <br>";
 	}
 ?>
