7070167aba9c2709fc1b2ae38c22d6b44b8b05c1
Tocyn <okegtosin@gmail.com>
2020-12-03 12:46:57 +0100

    contact Form

---
+++ submissions/oluwatosinOke/domManipulation/contactForm.js
@@ -0,0 +1,61 @@
+let form = document.getElementById('validateForm'),
+    formEmail = document.getElementById('formEmail'),
+    fullName = document.getElementById('fullName'),
+    subject = document.getElementById('subject'),
+    message = document.getElementById('message');
+
+document.addEventListener('DOMContentLoaded', function () {
+    form.addEventListener('submit', formValidation, false);
+});
+
+function required(field, error, event) {
+    if (field.value === "") {
+        field.nextElementSibling.innerHTML = error;
+        event.preventDefault();
+        return false;
+    }
+    else {
+        field.nextElementSibling.innerHTML = "";
+        return true;
+    }
+}
+
+function validation(field, regExp, errorMess1, errorMess2, event) {
+    if (field.value === "") {
+        field.nextElementSibling.innerHTML = errorMess1;
+        event.preventDefault();
+        return false;
+    }
+    if (!field.value.match(regExp)) {
+        field.nextElementSibling.innerHTML = errorMess2;
+        event.preventDefault();
+        return false;
+    } else {
+        field.nextElementSibling.innerHTML = "";
+        return true;
+    }
+}
+function formValidation(event) {
+    const emailRegex = /^\w+([-]?\w+)*@\w+([-]?\w+)*(\w{2,10})+$/,
+        nameRegex = /^[a-zA-Z]/;
+
+    validation(
+        fullName,
+        nameRegex,
+        "Please enter your Name",
+        "Please enter a valid name",
+        event
+    );
+
+    validation(
+        formEmail,
+        emailRegex,
+        "Please enter your email",
+        "Please enter a valid email",
+        event
+    );
+
+    required(subject, "Please enter a message subject", event);
+    required(message, "Please enter your Message", event);
+    return true;
+}
