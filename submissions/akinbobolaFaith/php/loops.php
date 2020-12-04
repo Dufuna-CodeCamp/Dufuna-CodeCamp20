7003d5334cd06a0fdebaa4dd7267510d176c3bdf
faithakinbobola <faithakinbobola2@gmail.com>
2020-12-04 11:28:28 +0100

    created a loop of numbers

---
+++ submissions/akinbobolaFaith/php/loops.php
@@ -0,0 +1,15 @@
+<?php
+$i = 101;
+while ($i < 150) {
+    echo $i . "<br>";
+    $i++;
+}
+
+for($i = 1; $i < 50; $i++) {
+    if($i % 2 === 0) {
+        echo $i . "<br>";
+    } else {
+        "Invalid input";
+    }
+}
+?>
