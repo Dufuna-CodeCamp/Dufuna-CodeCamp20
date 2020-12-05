44e2399a1d6e4d260dee811e55542527fb1696a4
kamena1994 <benjii1964@gmail.com>
2020-12-05 20:15:29 +0200

    created php file

---
+++ submissions/benjaminKamena/PHP/numbers.php
@@ -0,0 +1,13 @@
+<?php
+//PHP program that displays every number between 100 and 150 each on a new line using a loop
+for($x = 100; $x <= 150; $x++){
+ echo $x. "<br>";
+ }
+ 
+ //PHP program that displays all even number between 0 and 50 each on a new line using a loop
+ for($i = 0; $i <= 50; $i++){
+	 if($i % 2 == 0){
+		 echo $i. "<br>";
+	 }
+ }
+?>
