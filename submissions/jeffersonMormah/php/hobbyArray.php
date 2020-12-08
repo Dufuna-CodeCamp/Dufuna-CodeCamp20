e86ed05037e1f2defee2d3631db88287fadb56aa
Jeffbuchi <buchisam32@gmail.com>
2020-12-08 17:32:19 +0100

    using array_combine function to take an array of key and array of values

--- submissions/jeffersonMormah/php/hobbyArray.php
+++ submissions/jeffersonMormah/php/hobbyArray.php
@@ -3,7 +3,7 @@
 $firstNames = ["Jeff","Michelle","Matthew","Kelly","Wayne","Favor","Juliet","Daniel"];
 $resHobbies = ["Coding","Music","Football","Dancing","Trading","Singing","Fashion","Acting"];
 
-foreach(data_array($firsNames,$resHobbies) as $firstname => $hobby){
+foreach(array_combine($firsNames,$resHobbies) as $firstname => $hobby){
     echo 'My name is ' .$firstname . ", I love " . $hobby . "<br>";
 }
 
