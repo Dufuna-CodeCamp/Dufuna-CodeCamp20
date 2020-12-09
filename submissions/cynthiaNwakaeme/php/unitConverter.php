a369039e8c2df73abc58f38c3157675e5082ac54
Cynthia Nwakaeme <nwakaemecynthia@gmail.com>
2020-12-09 13:55:24 +0100

    [176] PHP unit coverter

--- submissions/cynthiaNwakaeme/php/unitConverter.php
+++ submissions/cynthiaNwakaeme/php/unitConverter.php
@@ -1,28 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-   <form action="submit.php" method="post">
- <h2>UNIT CONVERTER</h2>
-   <!-- ALLOWS USER INPUT ANY VALUE -->
-    <label for="units">Units: Kindly Choose Unit of conversion</label><br><br>
-    <select name="units" id="units">
-        <option value="kilograms">Kilograms</option>
-        <option value="meters">Meters</option>
-        <option value="celsius">Celsius</option>
-    </select>
-    <br>
-    <br>
-    <label for="given_value">Input your value of interest</label><br>
-    <input type="number" name="given_value" id="given_value">
-    <br>
-    <br>
+<?php
 
-    <input type="submit" value="Submit">
-   </form> 
-</body>
-</html>
+    function unit_converter($units, $given_value)
+    {
+        switch ($units) {
+            case 'kilograms':
+                $pounds = $given_value * 2.20462262185;
+                echo round($pounds, 5).' lbs <br>';
+            break;
+            case 'meters':
+                $yards = $given_value * 1.09361;
+                echo "$yards yards <br>";
+            break;
+            case 'celsius':
+                $farenheit = 32 + ($given_value * 9 / 5);
+                echo "$farenheit  &deg F <br>";
+            break;
+            default:
+                echo 'The unit provided is undefined';
+        }
+    }
+
+    unit_converter('kilograms', 40);
+    unit_converter('meters', 40);
+    unit_converter('celsius', 40);
