e1ee76b05bd5382d29d961983cd3b7080df68467
ToluEnny24 <ogunronbitolulope@gmail.com>
2020-12-09 11:06:51 +0100

    I have resolved the conflicts and modified the files

--- submissions/fabunmiTolulope/dom/productPage.js
+++ submissions/fabunmiTolulope/dom/productPage.js
@@ -17,42 +17,24 @@ function quantityValue(){
     }
 }
 
-function priceValue() {
-        if (quantity.value > 0) {
-        productPrice = pricePerUnit * quantity.value;
-        price.innerHTML = productPrice;
-    }else {
-        return false;
-    }
-}
-
 function shippingAmount() {
+    productPrice = pricePerUnit * quantity.value;
+    price.innerHTML = productPrice;
     if (productPrice < 1000) {
         shippingCost.innerHTML = parseFloat(100);
     } else if ((productPrice >= 1000) && (productPrice <= 20000)) {
-        shippingCost.innerHTML = parseFloat(0.15 * productPrice);
+        shippingCost.innerHTML = parseInt(0.15 * productPrice);
     } else if ((productPrice >= 20000) && (productPrice <= 100000)) {
         shippingCost.innerHTML = parseFloat(0.2 * productPrice);
     } else if (productPrice >= 100000) {
         shippingCost.innerHTML = parseFloat(25000);
-    } else {
-        return true;
-    }
-}
-
-function totalCostValue() {
-    if (quantity.value > 0) {
-        totalCost.innerHTML = parseFloat(shippingCost.innerHTML) + parseFloat(productPrice);
-    } else{
-        return true;
-    }
+    } totalCost.innerHTML = parseInt(shippingCost.innerHTML) + parseInt(productPrice);
 }
 
 function totalCostAmount() {
+
     quantityValue();
-    priceValue();
     shippingAmount();
-    totalCostValue();
     return true;
 }
 
