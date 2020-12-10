be98b6e76be2b1effa9e5dd45738986f781ab37f
Onyinye101 <64909274+Onyinye101@users.noreply.github.com>
2020-12-10 19:41:10 +0100

    refactored signup.js

--- submissions/onyinyeChukwuka/javascript/signup.js
+++ submissions/onyinyeChukwuka/javascript/signup.js
@@ -5,104 +5,55 @@ let email = document.getElementById('email');
 let password = document.getElementById('password');
 let confirmPassword = document.getElementById('cpassword');
 let signupbutton = document.getElementById('signupbutton');
+const emailRegex = /^(([^<>()[\]\\.,;:\s@]+(\.[^<>()[\]\\.,;:\s@]+)*)|(.+))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const nameRegex = /[^a-zA-Z]/;
+const phoneRegex = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s/0-9]*$/g;
+const passwordRegex = (/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/);
 
 // to trigger the required error messages when the user clicks the signup button
-signupbutton.addEventListener('submit', () => {
-    validateEmail(email);
-    validateNumber(phoneNumber);
-    validatePassword(password);
-    validateConfirmPassword(confirmPassword, password);
-    validateFirstName(firstName);
-    validateLastName(lastName);
+signupbutton.addEventListener('click', () => {
+    validateInput(email, "Please enter your Email Address", "Please enter a valid email", emailRegex);
+    validateInput(firstName, "Please enter your First Name", "Please enter a valid first name", nameRegex);
+    validateInput(lastName, "Please enter your Last Name", "Please enter a valid last name", nameRegex);
+    validateInput(phoneNumber, "Please enter your Phone Number", "Please enter a valid phone number", phoneRegex);
+    validateInput(password, "Please enter your Password", "Your password should have a minimum of 6 characters, 1 capital letter, 1 special character eg @ and 1 number", passwordRegex);
+    validateInput(confirmPassword, "Please confirm your Password", "Your passwords don't match", passwordRegex);
 });
 
 
 // to print out individual error messages according to the specific text fields
-validateFirstName = (firstName) => {
-    const firstNameRegex = /[^a-zA-Z]/;
-    if (firstName.value === "") {
-        firstName.nextElementSibling.innerHTML = "Please enter your First Name";
+validateInput = (element, emptyElementMessage, validationFailedMessage, validationRegex) => {
+    if (element.value === "") {
+        element.nextElementSibling.innerHTML = emptyElementMessage;
         return false;
     }
-    if (firstNameRegex.test(firstName.value)) {
-        firstName.nextElementSibling.innerHTML = "Please enter a valid first name";
-        return false;
-    } else {
-        firstName.nextElementSibling.innerHTML = "";
-        return true;
+    if (element === phoneNumber || element === email || element === password) {
+        if (!validationRegex.test(element.value)) {
+            element.nextElementSibling.innerHTML = validationFailedMessage;
+            return false;
+        } else {
+            element.nextElementSibling.innerHTML = "";
+            return true;
+        }
+    } else if (element === confirmPassword) {
+        if (confirmPassword.value !== password.value) {
+            element.nextElementSibling.innerHTML = validationFailedMessage;
+            return false;
+        } else {
+            element.nextElementSibling.innerHTML = "";
+            return true;
+        }
     }
-}
+    else {
+        if (element === firstName || element === lastName) {
+            if (validationRegex.test(element.value)) {
+                element.nextElementSibling.innerHTML = validationFailedMessage;
+                return false;
+            } else {
+                element.nextElementSibling.innerHTML = "";
+                return true;
+            }
+        }
+    }
 
-validateLastName = (lastName) => {
-    const lastNameRegex = /[^a-zA-Z]/;
-    if (lastName.value === "") {
-        lastName.nextElementSibling.innerHTML = "Please enter your Last Name";
-        return false;
-    }
-    if (lastNameRegex.test(lastName.value)) {
-        lastName.nextElementSibling.innerHTML = "Please enter a valid last name";
-        return false;
-    } else {
-        lastName.nextElementSibling.innerHTML = "";
-        return true;
-    }
-}
-
-validateNumber = (phoneNumber) => {
-    const phoneRegex = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s/0-9]*$/g;
-    if (phoneNumber.value === "") {
-        phoneNumber.nextElementSibling.innerHTML = "Please enter your Phone Number";
-        return false;
-    }
-    if (!phoneRegex.test(phoneNumber.value)) {
-        phoneNumber.nextElementSibling.innerHTML = "Please enter a valid phone number";
-        return false;
-    } else {
-        phoneNumber.nextElementSibling.innerHTML = "";
-        return true;
-    }
-}
-
-validateEmail = (email) => {
-    const emailRegex = /^(([^<>()[\]\\.,;:\s@]+(\.[^<>()[\]\\.,;:\s@]+)*)|(.+))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (email.value === "") {
-        email.nextElementSibling.innerHTML = "Please enter your Email Address";
-        return false;
-    }
-    if (!emailRegex.test(email.value)) {
-        email.nextElementSibling.innerHTML = "Please enter a valid email";
-        return false;
-    } else {
-        email.nextElementSibling.innerHTML = "";
-        return true;
-    }
-}
-
-validatePassword = (password) => {
-    const passwordRegex = (/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/);
-    if (password.value === "") {
-        password.nextElementSibling.innerHTML = "Please enter your Password";
-        return false;
-    }
-    if (!passwordRegex.test(password.value)) {
-        password.nextElementSibling.innerHTML = "Your password should have a minimum of 6 characters, 1 capital letter, 1 special character eg @ and 1 number";
-        return false;
-    } else {
-        password.nextElementSibling.innerHTML = "";
-        return true;
-    }
-}
-
-validateConfirmPassword = (confirmPassword, password) => {
-    if (confirmPassword.value === "") {
-        confirmPassword.nextElementSibling.innerHTML = "Please confirm your Password";
-        return false;
-    }
-    if (confirmPassword.value !== password.value) {
-        confirmPassword.nextElementSibling.innerHTML = "Your passwords don't match";
-        return false;
-    } else {
-        confirmPassword.nextElementSibling.innerHTML = "";
-        return true;
-    }
 }
