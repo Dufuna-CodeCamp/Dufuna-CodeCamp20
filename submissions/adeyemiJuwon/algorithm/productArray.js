cb31eb9c2cac6edbbd0eb75c88409b8c517dba5f
Adeyemi juwon <samju7778@gmail.com>
2020-12-13 16:23:19 +0100

    correction to the product and price arrray

--- submissions/adeyemiJuwon/algorithm/productArray.js
+++ submissions/adeyemiJuwon/algorithm/productArray.js
@@ -1,11 +1,48 @@
 const products = [
-  'Xiaomi Mi Notebook Pro 2019 15.6 inch Windows 10',
-  'Xiaomi Redmibook',
-  'Charging power',
-  'Laptop Bag',
+  'Canon Kit Lens 15-55mm',
+  'Nikon 3100',
+  'Samsung NX300',
+  'Canon EOS 80D',
+  'Sony Alpha A7s',
+  'Sony NX 300M',
+  'Panasonic Lumix GH4',
+  'Panasonic Lumix DC-GH5',
+  'Xiaomi Y1-M1',
+  'Fujifilm X-A10',
+  'Canon DSLR Rebel T7',
+  'Canon DSLR EOS 2000D',
+  'Sony A7ii',
+  'Sony A6500 Mirrorless',
+  'Sony Alpha A7r III',
+  'Sony NEX-3N',
+  'Nikon V1',
+  'Sony NEX-F3',
+  'Sony NEX-5',
+  'Olympus PEN E-PL6',
 ];
 
-const product_prices = [1485.83, 819.52, 4.26, 6.95];
+const product_prices = [
+  500,
+  200,
+  750,
+  2000,
+  1600,
+  300,
+  800,
+  5000,
+  500,
+  600,
+  1200,
+  1200,
+  2000,
+  2000,
+  5000,
+  750,
+  200,
+  800,
+  900,
+  750,
+];
 
 function checkArray(array1, array2) {
   for (let i = 0; i <= array1.length - 1; i++) {
