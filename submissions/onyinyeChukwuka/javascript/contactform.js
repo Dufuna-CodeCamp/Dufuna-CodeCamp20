29a42a04d7c6cbbc4059db79af1ac32583df9904
Onyinye101 <64909274+Onyinye101@users.noreply.github.com>
2020-12-17 10:12:43 +0100

    updated the html and js

--- submissions/onyinyeChukwuka/javascript/contactform.js
+++ submissions/onyinyeChukwuka/javascript/contactform.js
@@ -2,7 +2,7 @@ let fullName = document.getElementById('fullname');
 let email = document.getElementById('email');
 let subject = document.getElementById('subject');
 let message = document.getElementById('message');
-let sendbutton = document.getElementById('sendbutton');
+let sendbutton = document.getElementById('form');
 const emailRegex = /^(([^<>()[\]\\.,;:\s@]+(\.[^<>()[\]\\.,;:\s@]+)*)|(.+))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const nameRegex = /^[a-zA-Z]/;
 
