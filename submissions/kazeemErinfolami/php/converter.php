8f98838f4f372cb45ea98753a334e4946add75cd
Kazeem Erinfolami <erinfolamibolaji2010@gmail.com>
2021-01-04 22:33:21 +0100

    [176]PHP_unit_Converter

--- submissions/kazeemErinfolami/php/converter.php
+++ submissions/kazeemErinfolami/php/converter.php
@@ -1,28 +1,29 @@
 <?php
-
-function Converter ($converter, $value){
+function Converter($converter, $value)
+{
     $pounds = 2.20462;
     $yard = 1.09361;
     $fahrenheit =  32;
 
-    if(getType($value) === "integer"){
-       
-        switch($converter){
+    if (getType($value) === "integer") {
+
+        switch ($converter) {
             case "kilo";
-            return $value * $pounds . " " . "pounds";
-            break;
+                return $value * $pounds . " " . "pounds";
+                break;
             case "meter";
-            return $value * $yard . " " . "yard";
-            break;
+                return $value * $yard . " " . "yard";
+                break;
             case "celsius";
-            return ($value * 1.8) + $fahrenheit . " " . "fahrenheit";
-            break;
+                return ($value * 1.8) + $fahrenheit . " " . "fahrenheit";
+                break;
             default:
-            return "unit undefine for value:" . " " . $value;
-        }     
-    }else{
+                return "unit undefine for value:" . " " . $value;
+        }
+    } else {
         return "<h3> Invalid Input </h3>";;
     }
 }
 
 echo Converter("celsius", 0)
+?>
