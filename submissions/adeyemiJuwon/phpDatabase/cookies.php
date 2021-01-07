946dbe5087473866c49684d0b9c9b5ddbde8740e
Adeyemi juwon <samju7778@gmail.com>
2021-01-07 10:08:27 +0100

    remove cookie

--- submissions/adeyemiJuwon/phpDatabase/cookies.php
+++ submissions/adeyemiJuwon/phpDatabase/cookies.php
@@ -16,27 +16,5 @@ class queryDatabase {
             die("ERROR: Could not execute $sql. " . $e->getMessage());
         }
     }
-    function getOrders($id){
-        try {
-
-            $sql = 'SELECT products.name,customers.street_address, customers.city,customers.state, 
-            customers.country,order_items.quantity, order_items.unit_price, 
-            order_items.total_amount, orders.order_date, orders.id 
-            FROM order_items 
-            LEFT JOIN orders ON orders.id = order_items.order_id
-            LEFT JOIN customers on orders.customer_id = customers.id
-            LEFT JOIN products ON products.id = order_items.product_id 
-            WHERE orders.customer_id = customers.id';
-
-            $stmt  = $this->connect()->prepare($sql);
-            $stmt->execute([$id]);
-            $orders = $stmt->fetchAll();
-
-            $this->storeData('orders', $orders, time() + 3600);
-            return $orders;
-        } catch (PDOException $e) {
-            die("ERROR: Could not execute $sql. " . $e->getMessage());
-        }
-    }
 }
 ?>
