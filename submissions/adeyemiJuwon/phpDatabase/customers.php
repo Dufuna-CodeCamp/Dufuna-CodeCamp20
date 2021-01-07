f41be1927719ef44a385c354587e64298088f87f
Adeyemi juwon <samju7778@gmail.com>
2021-01-07 10:00:50 +0100

    order items

--- submissions/adeyemiJuwon/phpDatabase/customers.php
+++ submissions/adeyemiJuwon/phpDatabase/customers.php
@@ -33,4 +33,48 @@ try{
 }
 unset($pdo);
 
+// another method to set the cookies without the cookies file
+
+// include('connect.php');
+
+// try{
+//     $sql = "SELECT * FROM customers";
+//     $results = $pdo->query($sql);
+//     setcookie('customers',  json_encode($results), time() + 3600);
+//     if($results ->rowCount() > 0){
+//         echo "<Table>";
+//             echo "<tr>";
+//                 echo "<th>ID</th>";
+//                 echo "<th>Full Name</th>";
+//                 echo "<th>Email</th>";
+//                 echo "<th>Create At </th>";
+//                 echo "<th>Actions </th>";
+//             echo "</tr>";
+//             while($row = $results -> fetch()){
+//             echo "<tr>";
+//                 echo "<td>" . $row['id'] . "</td>";
+//                 echo "<td>" . $row['first_name'] . " " . $row['last_name'] . "</td>";
+//                 echo "<td>" . $row['email_address'] . "</td>";
+//                 echo "<td>" . $row['create_at'] . "</td>";
+//                 echo "<td>" . '<button> <a href ="insert.php"> Veiw </a> </button>' ."</td>";
+//             echo "</tr>";
+//             }
+//         echo "</table>";
+//             unset($results);
+//     }else{
+//         echo "No records matching your query were found";
+//     }
+// }catch(PDOException $e){
+//     die("ERROR: Could not excute $sql." .$e->getMessage());
+
+// }
+
+// if(isset($_COOKIE['customers'])) {
+//     echo "customers '" . $cookie_name . "' is set!<br>";
+  
+// } else {
+//   echo "customers '" . 'customers' . "' is not set!";
+// }
+// unset($pdo);
+
 ?>
--- submissions/adeyemiJuwon/phpDatabase/orders.php
+++ submissions/adeyemiJuwon/phpDatabase/orders.php
@@ -1,10 +1,20 @@
 <?php
 
-include('cookies.php');
+include('connect.php');
 
-$orders = isset($_COOKIE['orders']) ? unserialize($_COOKIE['orders']) : $queryDatabase->getOrders($id);
+try{
+     $sql = 'SELECT products.name,customers.street_address, customers.city,customers.state, 
+            customers.country,order_items.quantity, order_items.unit_price, 
+            order_items.total_amount, orders.order_date, orders.id 
+            FROM order_items 
+            LEFT JOIN orders ON orders.id = order_items.order_id
+            LEFT JOIN customers on orders.customer_id = customers.id
+            LEFT JOIN products ON products.id = order_items.product_id 
+            WHERE orders.customer_id = customers.id';
+            $results = $pdo->query($sql);
+            setcookie('order_items',  json_encode($results), time() + 3600);
 
-if (count($orders) > 0) {
+if($results ->rowCount() > 0) {
     echo "<table>";
         echo "<tr>";
             echo "<td>S/N</td>";
@@ -15,7 +25,7 @@ if (count($orders) > 0) {
             echo "<td>Order Date</td>";
             echo "<td>Customer Location</td>";
     echo "</tr>";
-    foreach ($orders as $row) {
+     while($row = $results -> fetch()) {
         echo "<tr>";
             echo "<td>" . $row['id'] . "</td>";
             echo "<td>" . $row['name'] . "</td>";
@@ -31,4 +41,14 @@ if (count($orders) > 0) {
 } else {
     echo 'No records matching your query were found';
 };
+}catch(PDOException $e){
+    die("ERROR: Could not excute $sql." .$e->getMessage());
+}
+if(isset($_COOKIE['customers'])) {
+    echo "customers '" . $cookie_name . "' is set!<br>";
+  
+} else {
+  echo "customers '" . 'customers' . "' is not set!";
+}
+unset($pdo);
 ?>
