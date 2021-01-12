5607dff7fc854ed7de2d13709ed37a9a3d20a447
Adeyemi juwon <samju7778@gmail.com>
2021-01-12 10:57:09 +0100

    done

--- submissions/adeyemiJuwon/phpDatabase/orders.php
+++ submissions/adeyemiJuwon/phpDatabase/orders.php
@@ -17,39 +17,38 @@ try{
             $results = $pdo->query($sql);
             setcookie('orders',  json_encode($results), time() + 3600);
 
-            if(isset($_COOKIE['orders'])) {
-                echo "orders is set!<br>";
-            }else {
-                echo "orders is not set!";
-            }
+    if(isset($_COOKIE['orders'])) {
 
-if($results ->rowCount() > 0) {
-    echo "<table>";
-        echo "<tr>";
-            echo "<td>S/N</td>";
-            echo "<td>Product Name</td>";
-            echo "<td>Unit Price</td>";
-            echo "<td>Quantity</td>";
-            echo "<td>Total Price</td>";
-            echo "<td>Order Date</td>";
-            echo "<td>Customer Location</td>";
-    echo "</tr>";
-     while($row = $results -> fetch()) {
-        echo "<tr>";
-            echo "<td>" . $row['id'] . "</td>";
-            echo "<td>" . $row['name'] . "</td>";
-            echo "<td>" . $row['unit_price'] . "</td>";
-            echo "<td>" . $row['quantity'] . "</td>";
-            echo "<td>" . $row['total_amount'] . "</td>";
-            echo "<td>" . $row['order_date'] . "</td>";
-            echo "<td>" . $row['street_address'] . ", " . $row['city'] . ", " . $row['state'] . ", "  . $row['country'] ."</td>";
-        echo "</tr>";
-    };
-    echo "</table>";
-    unset($orders);
-} else {
-    echo 'No records matching your query were found';
-};
+        if($results ->rowCount() > 0) {
+        echo "<table>";
+            echo "<tr>";
+                echo "<td>S/N</td>";
+                echo "<td>Product Name</td>";
+                echo "<td>Unit Price</td>";
+                echo "<td>Quantity</td>";
+                echo "<td>Total Price</td>";
+                echo "<td>Order Date</td>";
+                echo "<td>Customer Location</td>";
+            echo "</tr>";
+        while($row = $results -> fetch()) {
+            echo "<tr>";
+                echo "<td>" . $row['id'] . "</td>";
+                echo "<td>" . $row['name'] . "</td>";
+                echo "<td>" . $row['unit_price'] . "</td>";
+                echo "<td>" . $row['quantity'] . "</td>";
+                echo "<td>" . $row['total_amount'] . "</td>";
+                echo "<td>" . $row['order_date'] . "</td>";
+                echo "<td>" . $row['street_address'] . ", " . $row['city'] . ", " . $row['state'] . ", "  . $row['country'] ."</td>";
+            echo "</tr>";
+        };
+        echo "</table>";
+        unset($results);
+        } else {
+            echo 'No records matching your query were found';
+        };         
+    }else {
+        echo "orders is not set!";
+     }
 }catch(PDOException $e){
     die("ERROR: Could not excute $sql." .$e->getMessage());
 }
