a55fef3bf8a6100b839b4b384dfa1cdf0fac43f8
Adeyemi juwon <samju7778@gmail.com>
2021-01-12 22:02:46 +0100

    json decode etc

--- submissions/adeyemiJuwon/phpDatabase/orders.php
+++ submissions/adeyemiJuwon/phpDatabase/orders.php
@@ -1,7 +1,9 @@
 <?php
 
 include('connect.php');
-
+ if( isset($_GET['id']) && strlen($_GET['id']) > 0 ) {
+ $id =  $_GET['id'];
+}
 
 try{
      $sql = "SELECT order_items.quantity, order_items.unit_price, 
@@ -12,25 +14,34 @@ try{
             LEFT JOIN customers on orders.customer_id = customers.id
             LEFT JOIN products ON products.id = order_items.product_id 
             WHERE orders.customer_id = '$id' ";
-            $results = $pdo->query($sql);
-            setcookie("orders",  json_encode($results), time() + 3600);
+            $stmt = $pdo->query($sql);
+             $stmt->execute([$id]);
+            $orders = $stmt->fetchAll();
+
+            //set cookies with different method
+            // setcookie("orders",  json_encode($orders), time() + 3600);
+             setcookie("orders",  serialize($orders), time() + 3600 );
+            // setcookie("orders",  explode($orders), time() + 3600); need two parameter
 
 
-if( isset($_GET['id'] )){
- $id =  $_GET['id'];
- $orders =  json_decode($_COOKIE['orders']);
- 
-if (count($orders) > 0) {
-    echo "<table>";
-        echo "<tr>";
-            echo "<td>S/N</td>";
-            echo "<td>Product Name</td>";
-            echo "<td>Unit Price</td>";
-            echo "<td>Quantity</td>";
-            echo "<td>Total Price</td>";
-            echo "<td>Order Date</td>";
-            echo "<td>Customer Location</td>";
-    echo "</tr>";
+    if( isset($_GET['id']) && strlen($_GET['id']) > 0 ) {
+        $id =  $_GET['id'];
+     // $orders =  implode($_COOKIE['orders']);
+        $orders =  unserialize($_COOKIE['orders']);
+     // $orders = json_decode (stripslashes($_COOKIE['orders']), true );
+
+
+    if (count($orders) > 0) {
+        echo "<table>";
+            echo "<tr>";
+                echo "<td>S/N</td>";
+                echo "<td>Product Name</td>";
+                echo "<td>Unit Price</td>";
+                echo "<td>Quantity</td>";
+                echo "<td>Total Price</td>";
+                echo "<td>Order Date</td>";
+                echo "<td>Customer Location</td>";
+        echo "</tr>";
     foreach ($orders as $row) {
         echo "<tr>";
             echo "<td>" . $row['id'] . "</td>";
@@ -42,13 +53,13 @@ if (count($orders) > 0) {
             echo "<td>" . $row['street_address'] . ", " . $row['city'] . ", " . $row['state'] . ", "  . $row['country'] ."</td>";
         echo "</tr>";
     };
-    echo "</table>";
-    unset($orders);
+        echo "</table>";
+        unset($orders);
 } else {
     echo 'No records matching your query were found';
 }
 }
-} catch (PDOException $e) {
-            die("ERROR: Could not execute $sql. " . $e->getMessage());
+}catch (PDOException $e) {
+    die("ERROR: Could not execute $sql. " . $e->getMessage());
 }
 ?>
