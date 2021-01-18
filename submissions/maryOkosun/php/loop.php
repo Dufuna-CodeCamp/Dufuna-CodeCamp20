ec10746c552b6f8b37025e67210339f85410110a
marynoir <okosunmaryeghonghon@gmail.com>
2021-01-18 08:18:59 +0100

    loop and array php file created

---
+++ submissions/maryOkosun/php/loop.php
@@ -0,0 +1,13 @@
+<?php                               //A program to return every number between 100 ad 150
+    for($i = 100; $i  <=150; $i++ ){
+        echo "$i <br>";
+    }
+?>
+
+<?php                              //A program to return every even number between 0 and 50
+    for($i = 0; $i  <=50; $i++ ){
+        if($i % 2 == 0){
+            echo "$i <br>";
+        }   
+    }
+?>
