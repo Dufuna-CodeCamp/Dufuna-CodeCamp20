47c44d07196364bd7bec40478026715c86d88d46
Chabu Chabu <chabuchabu32@gmail.com>
2021-01-19 11:52:28 +0000

    query-ammended

--- submissions/chabuChabu/database/trips.sql
+++ submissions/chabuChabu/database/trips.sql
@@ -10,16 +10,15 @@ primary key (id)
 
 /*The following query is used to create a table for storing trip details*/
 /*It references the passanger table  via passanger ID*/
+use trips;
 create table trip_details(
 id int not null auto_increment,
 passanger_class int(11) not null,
 ticket_number varchar (50) not null,
 trip_fare decimal(10,2)not null,
 cabin varchar (50),
-parents int (11),
-children int (11),
-spouses int (11),
-siblings int (11),
+parents_childrens int (11),
+spouses_siblings int (11),
 embarkation_point varchar(100) not null,
 passanger_id int not null,
 primary key(id),
