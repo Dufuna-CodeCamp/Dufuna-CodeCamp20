fa3cc82e0856e1587e3e0d61ef4c382d21590157
marynoir <okosunmaryeghonghon@gmail.com>
2021-01-19 06:50:31 +0100

    loops and number modified

--- submissions/maryOkosun/php/loop.php
+++ submissions/maryOkosun/php/loop.php
@@ -1,11 +1,10 @@
-<?php                               //A program to return every number between 100 ad 150
-    for($i = 100; $i  <=150; $i++ ){
+<?php        
+    //A program to return every number between 100 and 150
+    for($i = 101; $i  < 150; $i++ ){
         echo "$i <br>";
     }
-?>
-
-<?php                              //A program to return every even number between 0 and 50
-    for($i = 0; $i  <=50; $i++ ){
+    //A program to return every even number between 0 and 50
+    for($i = 2; $i  < 50; $i++ ){
         if($i % 2 == 0){
             echo "$i <br>";
         }   
