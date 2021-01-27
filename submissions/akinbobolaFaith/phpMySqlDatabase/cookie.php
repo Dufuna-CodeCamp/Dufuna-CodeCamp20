293dcd59b3b316760cc3bcc74686443f95af2477
faithakinbobola <faithakinbobola2@gmail.com>
2021-01-27 21:48:22 +0100

    modified my file

--- submissions/akinbobolaFaith/phpMySqlDatabase/cookie.php
+++ submissions/akinbobolaFaith/phpMySqlDatabase/cookie.php
@@ -6,7 +6,7 @@ class Customer extends Dbconnect {
             $sql = "SELECT * FROM customers";
             $result = $this->connect()->query($sql);
             $data = $result->fetchAll();
-            setcookie("customers", json_encode($data), time() + 20);
+            setcookie("customers", json_encode($data), time() + 3600);
             return $data;
         } catch (PDOExeption $e) {
             die("ERROR: Could not execute $sql. " . $e->getMessage());
