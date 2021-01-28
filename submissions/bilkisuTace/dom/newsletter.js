378023e49c0c224f6ac25cba3f1f49bb06575b51
bilkisutace <tace.bilkisu@gmail.com>
2021-01-28 10:56:08 +0100

    [CSFS-13] Newsletter Form Validation

--- submissions/bilkisuTace/dom/newsletter.js
+++ submissions/bilkisuTace/dom/newsletter.js
@@ -5,11 +5,12 @@ let newsEmailRegex = /^\w+([-]?\w+)*@\w+([-]?\w+)*(.\w{3,4})+$/;
 
 function fieldValidation(event) {
     if (newsEmail.value === "") {
+    	
       newsEmail.nextElementSibling.innerHTML = "Please enter your Email Address";
       event.preventDefault();
       return false;
-    }
-    if (!newsEmail.value.match(newsEmailRegex)) {
+    } else if (!newsEmail.value.match(newsEmailRegex)) {
+
       newsEmail.nextElementSibling.innerHTML = "Please enter a Valid Email";
       event.preventDefault();
       return false;
