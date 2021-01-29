d6feef04cc4187a045d16156712b1b508f65d789
=MYKEL <=onyememykel@gmail.com>
2021-01-29 14:56:07 +0100

    modified file

--- submissions/onyemeMichael/database/init.sql
+++ submissions/onyemeMichael/database/init.sql
@@ -73,6 +73,21 @@ INSERT INTO customers (firstname, lastname, email, customer_password)
 VALUES ('Paul', 'Onyeme', 'paulonyeme@email.com', 'paul'),
 	   ('Mayowa', 'Ojo', 'mayawaojo@email.com', 'mayowa');
 
+-- customers_datails table
+CREATE TABLE customer_details(
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        full_name VARCHAR(30) NOT NULL,
+        email_address VARCHAR(50) NOT NULL UNIQUE,
+        created_at DATETIME
+    );
+
+-- Insert into customers_details table
+    INSERT INTO customer_details(full_name, email_address, created_at) VALUES
+        ('Onyeme Florence', 'onyeme@gmail.com', now()),
+        ('Tolu Chanda', 'tolu@gahoo.com', now()),
+        ('Paul Zuck', 'zuck@gmail.com', now()),
+        ('Victoria Philip', 'vicky@gmail.com', now());
+        
 -- Rigistered Customers Addresses
 CREATE TABLE addresses (
 	id INT NOT NULL AUTO_INCREMENT,
