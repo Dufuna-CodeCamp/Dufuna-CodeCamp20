5d5a086b94a34e01d969b83c88ec781f7689e02c
Kazeem Erinfolami <erinfolamibolaji2010@gmail.com>
2021-02-01 15:52:47 +0100

    Loops and Numbers

--- submissions/kazeemErinfolami/PHP/loops.php
+++ submissions/kazeemErinfolami/PHP/loops.php
@@ -1,10 +1,12 @@
 <?php
-// A for loop that displays every number between 100 and 150
+// number between 100 and 150
+
 for ($i = 101; $i < 150; $i++) {
     echo $i . "<br>";
 }
 
-// A for loop that  displays all even numbers between 0 and 50
+
+// even numbers between 0 and 50
 
 for ($j = 2; $j < 50; $j++) {
     if ($j % 2 == 0) {
