46161b0ca7c757d2d56e57abfd2d1cddeff9fe30
Victor-Aienobe <viktornobe@gmail.com>
2021-02-01 13:00:58 +0100

    modification

--- submissions/victorAienobe/phpDatabase/saveCookie.php
+++ submissions/victorAienobe/phpDatabase/saveCookie.php
@@ -37,7 +37,7 @@
                         echo "<td>" . $row['first_name'] . ' ' . $row['last_name'] . "</td>";
                         echo "<td>" . $row['email_address'] . "</td>";
                         echo "<td>" . $row['created_at'] . "</td>";
-                        echo "<td>" . '<button><a href="./listView.php?id=?'. $row['id'] .'">View</a></button>' . "</td>";
+                        echo "<td>" . '<button><a href="./listView.php?id='. $row['id'] .'">View</a></button>' . "</td>";
                     echo "</tr>";
                 }
             echo "</table>";
