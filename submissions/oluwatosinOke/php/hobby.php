bec1dcfafad5f23e65394fe5d3465e1f2ac5b588
Tocyn <okegtosin@gmail.com>
2021-02-05 17:38:08 +0100

    Hobby Array

---
+++ submissions/oluwatosinOke/php/hobby.php
@@ -0,0 +1,10 @@
+<?php
+// Create an array called names
+$names = ["Tosin", "Ayotomiwa", "Thompson", "Charlotte", "Janneya", "Tiara", "Judah", "Havilah"];
+// Create an array called Hobby
+$hobby = ["eating", "Reading", "Talking", "Playing", "Writting", "Coding", "Sewing", "Dancing"];
+// creating a loop using foreach
+foreach ($names as $index => $value) { 
+echo "My Name is " .$names[$index]. ", I Love " .$hobby{$index}. "<br />";
+}
+?>
