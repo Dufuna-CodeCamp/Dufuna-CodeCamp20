b3a9973c5e0ae3d82567a2b906d6ae26dc4a12b3
Emmanuel Idiaghe <emmanuelidiaghe@gmail.com>
2021-02-11 12:00:30 +0100

    updating table with either query or cookie

--- submissions/EmmanuelIdiaghe/php-database/admin.php
+++ submissions/EmmanuelIdiaghe/php-database/admin.php
@@ -10,14 +10,33 @@ function load() {
     $result = $pdo->query($sql);
     $details = $result->fetchAll();
     setcookie("show", serialize($details), time() + (86400 * 30), "/");
-    $_COOKIE["show"] = serialize($details);
+    return $details;
 }
 
 
 try {
     if ($notSet) {
-        load(); //set cookie
-        echo "Please <b>REFRESH PAGE</b>";
+        $info = load(); //Fetch query and set cookie
+        if (count($info) > 0) {
+            echo "<table style= 'border: 1px solid black'>";
+                echo "<tr>";
+                    echo "<th style= 'border: 1px solid black'>S/N</th>";
+                    echo "<th style= 'border: 1px solid black'>Full Name</th>";
+                    echo "<th style= 'border: 1px solid black'>Email Address</th>";
+                    echo "<th style= 'border: 1px solid black'>Created At</th>";
+                    echo "<th style= 'border: 1px solid black'>Actions</th>";
+                echo "</tr>";
+                foreach ($info as $value) {
+                    echo "<tr>";
+                        echo "<td style= 'border: 1px solid black'>" . $value["id"] . "</td>";
+                        echo "<td style= 'border: 1px solid black'>" . $value["first_name"] . "  " . $value["last_name"] . "</td>";
+                        echo "<td style= 'border: 1px solid black'>" . $value["email_address"] . "</td>";
+                        echo "<td style= 'border: 1px solid black'>" . $value["created_at"] . "</td>";
+                        echo "<td style= 'border: 1px solid black'><form method='get' action='view.php'><button style= 'color: black; background-color: #DAF943; border-radius: 20%; margin-top: 19px;' type='submit' name='view' value=\"$value[id]\">View</button></form></td>";
+                    echo "</tr>";
+                }
+            echo "</table";
+        } else echo "No record found!";
     }
     else {
         $display = unserialize($_COOKIE["show"]);
