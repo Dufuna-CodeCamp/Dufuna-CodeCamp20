896c1e1c3fda7db822390ed9e8615335468c33ff
ToluEnny24 <ogunronbitolulope@gmail.com>
2021-02-12 06:51:52 +0100

    I have created a PHP program for numbers and loops

---
+++ submissions/fabunmiTolulope/php/Number_loops.php
@@ -0,0 +1,17 @@
+<?php
+
+// This program will display every number between 100 and 150 using while loop
+$x = 100;
+while ($x <= 150) {
+	echo "Number: $x <br>";
+	$x++;
+}
+
+// This program will display even numbers between 0 and 50 using for loop
+for($y = 0; $y < 50; $y++){
+    if($y > 0 && $y % 2 === 0){
+        echo "Number: $y <br>";
+    };
+}
+
+?>
