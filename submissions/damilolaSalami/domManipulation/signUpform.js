c8998cdbae4709c1c1ed908502d4ac740283dfce
queensaldam <58794483+queensaldam@users.noreply.github.com>
2021-02-16 00:59:46 +0100

    [CSFE-11 Sign-Up Form Validation

---
+++ submissions/damilolaSalami/domManipulation/signUpform.js
@@ -0,0 +1,42 @@
+let submitform = document.getElementById("validateForm");
+    firstname = document.getElementById("firstname");
+    lastname = document.getElementById("lastname");
+    othernames = document.getElementById("othernames");
+    phonenumber = document.getElementById("phoneNumber");
+    email = document.getElementById("email");
+    password = document.getElementById("password");
+    confirmpassword= document.getElementById("confirmpassword");
+
+var nameRegex =  /^[a-zA-Z ]+$/;
+var pnumberRegex = /^[+]*[0-9]+$/;
+var emailRegex = /^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,6})+$/;
+var passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[._$,/;:+=~`'"<>^#?!@$%^&*-]).{6,}$/;
+
+function formValidation (field, Regex, error_one, error_two, event) {
+    if (field.value === "") {
+        field.nextElementSibling.innerHTML = error_one;
+        event.preventDefault();
+        return false;
+    }
+
+    if (!field.value.match(Regex)) {
+        field.nextElementSibling.innerHTML = error_two;
+        event.preventDefault();
+        return false;
+    } else {
+        field.nextElementSibling.innerHTML = "";
+        return true;
+    }
+}
+
+function validateForm (e) {
+    formValidation(firstname, nameRegex, "Please enter your first name", "No special characters", e);
+    formValidation(lastname, nameRegex, "Please enter your last name", "No special characters", e);
+    formValidation(phonenumber, pnumberRegex, "Please enter your Phone Number", "Please enter a valid phone number", e);
+    formValidation(email, emailRegex, "Please enter your Email Address", "Please enter a valid email", e);
+    formValidation(password, passwordRegex, "Please enter your Password", "Your password should have a minimum of 6 characters, 1 capital letter, 1 special character e.g @ and 1 number.", e);
+    formValidation(confirmpassword, passwordRegex, "Please confirm your Password", "Your password don't match", e);
+    return true;
+}
+
+submitform.addEventListener("submit", validateForm);
