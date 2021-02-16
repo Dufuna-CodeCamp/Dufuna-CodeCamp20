4fb2340882896464b5b30b2356ef6e526ccaec18
queensaldam <58794483+queensaldam@users.noreply.github.com>
2021-02-16 23:27:34 +0100

    [CSFE-11] Sign Up Form Validation

--- submissions/damilolaSalami/domManipulation/signUpform.js
+++ submissions/damilolaSalami/domManipulation/signUpform.js
@@ -6,10 +6,10 @@ const submitform = document.getElementById("validateForm"),
     password = document.getElementById("password"),
     confirmpassword= document.getElementById("confirmpassword");
 
-const nameRegex =  /^[a-zA-Z ]+$/,
-    pnumberRegex = /^[+]*[0-9]+$/,
-    emailRegex = /^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,6})+$/,
-    passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[._$,/;:+=~`'"<>^#?!@$%^&*-]).{6,}$/;
+const nameval =  /^[a-zA-Z ]+$/,
+    pnumberval = /^[+]*[0-9]+$/,
+    emailval = /^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,6})+$/,
+    passwordval = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[._$,/;:+=~`'"<>^#?!@$%^&*-]).{6,}$/;
 
 function formValidation (field, Regex, error_one, error_two, event) {
     if (field.value === "") {
@@ -26,14 +26,32 @@ function formValidation (field, Regex, error_one, error_two, event) {
     }
 }
 
-function validateForm (e) {
-    formValidation(firstname, nameRegex, "Please enter your first name", "No special characters", e);
-    formValidation(lastname, nameRegex, "Please enter your last name", "No special characters", e);
-    formValidation(phonenumber, pnumberRegex, "Please enter your Phone Number", "Please enter a valid phone number", e);
-    formValidation(email, emailRegex, "Please enter your Email Address", "Please enter a valid email", e);
-    formValidation(password, passwordRegex, "Please enter your Password", "Your password should have a minimum of 6 characters, 1 capital letter, 1 special character e.g @ and 1 number.", e);
-    formValidation(confirmpassword, passwordRegex, "Please confirm your Password", "Your password don't match", e);
+function pword(event) {
+    pasword = password.value;
+    cpasword = confirmpassword.value;
+    if (cpasword == "") {
+        confirmpassword.nextElementSibling.innerText = "Please confirm your password";
+        event.preventDefault();
+        return false;
+    } else if(pasword !== cpasword) {
+        confirmpassword.nextElementSibling.innerText = "Your password don't match";
+        event.preventDefault();
+        return false;
+    } else {
+        confirmpassword.nextElementSibling.innerText = "";
+        return true;
+    }
+}
+
+function validatedForm (e) {
+    formValidation(firstname, nameval, "Please enter your first name", "No special characters", e);
+    formValidation(lastname, nameval, "Please enter your last name", "No special characters", e);
+    formValidation(phonenumber, pnumberval, "Please enter your Phone Number", "Please enter a valid phone number", e);
+    formValidation(email, emailval, "Please enter your Email Address", "Please enter a valid email", e);
+    formValidation(password, passwordval, "Please enter your Password", "Your password should have a minimum of 6 characters, 1 capital letter, 1 special character e.g @ and 1 number.", e);
+    formValidation(confirmpassword, passwordval, "Please confirm your Password", "Your password don't match", e);
+    pword(e);
     return true;
 }
 
-submitform.addEventListener("submit", validateForm);
+submitform.addEventListener("submit", validatedForm);
