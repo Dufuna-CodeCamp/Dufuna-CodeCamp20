10740afc755006fb2b18cd76c9a62ad04139de4d
Tocyn <okegtosin@gmail.com>
2021-02-16 11:12:49 +0100

    customer list 3

--- submissions/oluwatosinOke/phpDatabases/customerList.php
+++ submissions/oluwatosinOke/phpDatabases/customerList.php
@@ -1,48 +1,21 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "tocyn";
-$dbname = "my_faveorite_online_store";
+require_once("connection.php");
 
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-  // set cookies
-    $cookie_name = "fullname";
-    $cookie_value = "email";
-    setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
-
-  // sql to create table
-  $sql = "CREATE TABLE customer (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  fullname VARCHAR(30) NOT NULL,
-  email VARCHAR(50),
-  actions VARCHAR(30) NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-  )";
-  $conn->exec($sql);
-  echo "Table customers created successfully <br>";
-} catch(PDOException $e) {
-  echo $sql . "<br>" . $e->getMessage();
-}
-
-//insert into table customers
-try {
-    $sql = "INSERT INTO customer (fullname, email, actions)
-    VALUES ('Dangote', 'alikodangote@gmail.com', 'pending'),
-        ('Markle', 'merganmarkle@gmail.comn', 'completed'),
-        ('Kardasian', 'kimk@gmail.comn', 'pending'),
-        ('Adeleke','davidomusic@gmail.com', 'pending')";
-    $conn->exec($sql);
-    echo "New record created successfully";
-} catch(PDOException $e) {
-    echo $sql . "<br>" . $e->getMessage();
+function getCustomers(){
+  try {
+      $sql = "SELECT * FROM customers";
+      $stmt  = $this->connect()->query($sql);
+      $results = $stmt->fetchAll();
+      $this->setData('customers', $results, time() + 3600);
+      return $results;
+  } catch (PDOException $e) {
+      die("ERROR: Could not execute $sql. " . $e->getMessage());
+  }
 }
 
 //create customer list view
 echo "<table style='border: solid 1px black;'>";
-echo "<tr><th>Id</th><th>Fullname</th><th>email</th><th>actions</th><th>created_at</th></tr>";
+echo "<tr><th>Id</th><th>Fullname</th><th>email</th><th>created_at</th></tr>";
 
 class TableRows extends RecursiveIteratorIterator {
   function __construct($it) {
@@ -58,12 +31,12 @@ class TableRows extends RecursiveIteratorIterator {
   }
 
   function endChildren() {
-    echo "</tr>  \n";
+    echo "</tr>" . "\n";
   }
 }
 
 try {
-  $stmt = $conn->prepare("SELECT id, fullname, email, actions, created_at FROM customer");
+  $stmt = $conn->prepare("SELECT id, first_name, email_address, created_at FROM customers");
   $stmt->execute();
 
   // set the resulting array to associative
@@ -74,10 +47,7 @@ try {
 } catch(PDOException $e) {
   echo "Error: " . $e->getMessage();
 }
-$conn = null;
 echo "</table>";
-
-$conn = null;
 
 // Close Connection
 unset($pdo);
