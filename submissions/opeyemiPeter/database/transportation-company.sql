6b0f9a2d97125b8281387d57d6dc3f8c198318c9
Opeyemi Peter <opeyemiadebowale1759@gmail.com>
2021-02-17 15:14:27 -0800

    database-task3

--- submissions/opeyemiPeter/database/transportation-company.sql
+++ submissions/opeyemiPeter/database/transportation-company.sql
@@ -6,72 +6,69 @@ SHOW DATABASES;
 USE transportation;
 
 -- Creating Passenger table 
-CREATE TABLE registeredPassengers (
+CREATE TABLE registered_passengers (
  id INT NOT NULL AUTO_INCREMENT,
- fullName VARCHAR(50) NOT NULL,
+ full_name VARCHAR(50) NOT NULL,
  sex VARCHAR(6) NOT NULL,
  age VARCHAR(4) ,
  PRIMARY KEY(id)
 );
-SELECT * FROM registeredPassengers; 
-DROP TABLE registeredpassengers;
-
+SELECT * FROM registered_passengers;
 -- Creating the Passenger trip details table
-CREATE TABLE tripDetails (
+CREATE TABLE trip_details (
  id INT NOT NULL AUTO_INCREMENT,
- passengerId INT NOT NULL,
- passengerClass INT NOT NULL ,
- passengerTicketNo VARCHAR(50) NOT NULL,
- tripFare DECIMAL(19,4) NOT NULL,
+ passenger_id INT NOT NULL,
+ passenger_class INT NOT NULL ,
+ passenger_ticket_no VARCHAR(50) NOT NULL,
+ trip_fare DECIMAL(19,4) NOT NULL,
  cabin VARCHAR(20),
- numberOfParentsChildren INT NOT NULL DEFAULT  0,
- numberOfSiblingsSpouse INT NOT NULL DEFAULT 0,
- embarkationPoint VARCHAR(10) NOT NULL,
+ number_of_parents_Children INT NOT NULL DEFAULT  0,
+ number_0f_siblings_spouse    INT NOT NULL DEFAULT 0,
+ embarkation_point VARCHAR(10) NOT NULL,
  PRIMARY KEY(id),
- FOREIGN KEY(passengerId) REFERENCES registeredPassengers(id)
+ FOREIGN KEY(passenger_id) REFERENCES registered_passengers(id)
 );
-SELECT * FROM tripDetails;
-DROP TABLE tripdetails;
+SELECT * FROM trip_details;
 
 -- Creating Accident case table
-CREATE TABLE accidentCases(
+CREATE TABLE accident_cases(
  id int not null auto_increment,
- passengerId int not null default(1),
- accidentStatus tinyint not null,
+ passenger_id int not null default(1),
+ accident_status tinyint not null,
  primary key(id),
- foreign key(passengerId) references registeredPassengers(id)
+ foreign key(passenger_id) references registered_passengers(id)
 );
-DROP TABLE accidentcases;
-SELECT * FROM accidentCases;
-
-
+Select * from accident_cases;
+drop tables accident_cases;
 -- Task 3 Transportation Data Query.
 
 # 1. Total number of passengers who survived
-	select count(*) from accidentCases where accidentStatus > 0;
+	select count(*) from accident_cases where accident_status > 0;
 	-- Answers => 342
 
 # 2. total number of unsurvived passengers
-  SELECT COUNT(*) FROM accidentCases WHERE accidentStatus = 0;
+  SELECT COUNT(*) FROM accident_cases WHERE accident_status = 0;
   -- Answers => 549
 
 # 3. Name and sex of passengers under the age of 27 that embarked at Queenstown and Cherbourg
-	SELECT fullName, sex from registeredPassengers
-	inner join tripDetails on 
-    registeredpassengers.id = tripDetails.passengerId 
-    where (age  < '27') and (embarkationPoint = 
+	SELECT full_name, sex from registered_passengers
+	inner join trip_details on 
+    registered_passengers.id = trip_details.passenger_id 
+    where (age  < '27') and (embarkation_point = 
     ( "Q%"| "C%"));
     
 # 4. Number of passengers who embarked at Southampton and survived
-	SELECT count(*) from accidentCases left join tripdetails on accidentcases.passengerId = tripdetails.passengerId where accidentStatus > 0 and embarkationPoint = 'S';
+	SELECT count(*) from accident_cases left join trip_details on 
+    accident_cases.passenger_id = trip_details.passenger_id where 
+    accident_status > 0 and embarkation_point = 'S';
     -- Answers => 218;
 
 # 5. Get the id, name and the total number of passengers 
 -- who paid a fare greater than $100 and above the age of 35 had siblings or spouses on board?
-select registeredPassengers.id, registeredPassengers.fullName, COUNT(*) OVER () AS total_count
-from registeredPassengers 
-join tripDetails on tripDetails.passengerId = registeredPassengers.id
-where tripFare > 100 and age > '35' and numberOfSiblingsSpouse != 0;
+select registered_passengers.id, registered_passengers.full_name, COUNT(*) OVER () AS total_count
+from registered_passengers 
+join trip_details on trip_Details.passenger_id = registered_passengers.id
+where trip_fare > 100 and age > '35' and number_of_siblings_spouse != 0;
 
 select count(*) from registeredpassengers left join tripdetails
 on registeredpassengers.id = tripdetails.passengerId
