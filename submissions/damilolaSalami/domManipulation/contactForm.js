8825cc29232feb6cbb648f84014c64b0faa55bf5
queensaldam <58794483+queensaldam@users.noreply.github.com>
2021-02-18 14:21:38 +0100

    [CSFE-11] Sign-up Form Validation

--- submissions/damilolaSalami/domManipulation/contactForm.js
+++ submissions/damilolaSalami/domManipulation/contactForm.js
@@ -4,9 +4,9 @@ const contactForm = document.getElementById("contactForm"),
     subject = document.getElementById("subject"),
     message = document.getElementById("message");
 
-const nameval =  /^[a-zA-Z ]+$/,
-    emailval = /^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,6})+$/,
-    messageval = /^[a-zA-Z]/;
+const nameVal =  /^[a-zA-Z ]+$/,
+    emailVal = /^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,6})+$/,
+    messageVal = /^[a-zA-Z]/;
 
 
 function formValidation (field, regex, error_one, error_two, event) {
@@ -25,10 +25,10 @@ function formValidation (field, regex, error_one, error_two, event) {
 }
 
 function validation (event) {
-    formValidation(fullName, nameval, "This field is required", "Please enter your name", event);
-    formValidation(email, emailval, "Please enter your Email Address", "Please enter a valid email", event);
-    formValidation(subject, messageval, "This field is required", "Please enter your message subject", event);
-    formValidation(message, messageval, "This field is required", "Please enter your Message", event);
+    formValidation(fullName, nameVal, "This field is required", "Please enter your name", event);
+    formValidation(email, emailVal, "Please enter your Email Address", "Please enter a valid email", event);
+    formValidation(subject, messageVal, "This field is required", "Please enter your message subject", event);
+    formValidation(message, messageVal, "This field is required", "Please enter your Message", event);
     return true;
 }
 
