cb02e5c7c3ed1268183edcef6bd7eb577208f344
Cynthia Nwakaeme <nwakaemecynthia@gmail.com>
2021-02-18 13:45:04 +0100

    Modified Cookies by using existing database

--- submissions/cynthiaNwakaeme/php-database/connection.php
+++ submissions/cynthiaNwakaeme/php-database/connection.php
@@ -1,13 +1,17 @@
 <?php
 
      //connecting to mysql database
-
-     try {
-        $pdo = new PDO('mysql: host=localhost', 'root', 'mysql');
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        echo 'connected succefully. Host info . '.$pdo->getAttribute(constant('PDO::ATTR_CONNECTION_STATUS')).'<br>';
-    } catch (PDOException $err) {
-        die('ERROR: Could not connect '.$err->getMessage()).'<br>';
+    class Connection {
+        public function connect () {
+            try {
+                $pdo = new PDO('mysql: host=localhost; dbname=taries_empire', 'root', 'mysql');
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                echo 'connected succefully. Host info . '.$pdo->getAttribute(constant('PDO::ATTR_CONNECTION_STATUS')).'<br>';
+                return $pdo;
+            } catch (PDOException $err) {
+                die('ERROR: Could not connect '.$err->getMessage()).'<br>';
+            }
+        
+        }
     }
-
+    
--- submissions/cynthiaNwakaeme/php-database/cookies.php
+++ submissions/cynthiaNwakaeme/php-database/cookies.php
@@ -3,53 +3,58 @@ require_once('connection.php');
 
 echo "<h2> List of Customers at Taries Empire </h2>";
 
-try{
-    /* ACCESSING E-COMMERCE DATABASE AND QUERYING THE DATABASE*/
-    $use_db = "USE taries_empire";
-    $sql = "SELECT * FROM customers";
-    $pdo->exec($use_db);
-    $stmt = $pdo->query($sql);
-    
+class Customer extends Connection {
+    public function test()  {
+        try{
+            /* ACCESSING E-COMMERCE DATABASE AND QUERYING THE DATABASE*/
+            $sql = "SELECT * FROM customers";
 
-    /* GENERATING COOKIES
-    1. result is initalized to get all records from database
-    2. This result is serialize to change the array/object to simple string/text file which is required to create cookies.
-     */
-     $result = $stmt->fetchAll();
-    setcookie("customer", serialize($result), time() + (86400 * 30), "/", "", false); //60sec *60mins *24hours/day => 86400 = 1 day
+            $stmt = $this->connect()->query($sql);
+            $result = $stmt->fetchAll();
+            
+            /* GENERATING COOKIES
+            1. result is initalized to get all records from database
+            2. This result is serialize to change the array/object to simple string/text file which is required to create cookies.
+            */
+           
+            setcookie("customer", serialize($result), time() + (86400 * 30), "/", "", false); //60sec *60mins *24hours/day => 86400 = 1 day
+            return $result;
+        } catch (PDOException $err) {
+            die('ERROR: Could not execute' .$sql . $err->getMessage());
+                                    }
+    }
+}
+
+$try = new Customer();
+$result = isset($_COOKIE['customer']) ? unserialize($_COOKIE['customer']) : $try->test();
+
 
     // To display results on the browser as a table, we convert the result to array/object.
-    $output = unserialize($_COOKIE["customer"]);
-
-    if(count($output) > 0){
+    //$output = unserialize($_COOKIE["customer"]);
+    
+    if(count($result) > 0){
         echo"<table>";
             echo"<tr>";
-                echo"<th> S/N <th>";
-                echo"<th> Full Name <th>";
-                echo"<th> Email Address <th>";
-                echo"<th> Created At <th>";
-                echo"<th> Actions <th>";
+                echo"<th> S/N </th>";
+                echo"<th> Full Name </th>";
+                echo"<th> Email Address </th>";
+                echo"<th> Created At </th>";
+                echo"<th> Actions </th>";
             echo"</tr>";
         // Loop through to access records (Objects in an array i.e datatype <=> JSON formatt)
-        foreach($output as $row){
+        foreach($result as $row){
             echo"<tr>";
-                echo "<td>" . $row['id'] ."</td>";
-                echo "<td>" . $row['full_name'] ."</td>";
-                echo "<td>" . $row['email_address'] ."</td>";
+                echo "<td>" . $row['customer_id'] ."</td>";
+                echo "<td>" . $row['first_name']. $row['last_name'] ."</td>";
+                echo "<td>" . $row['email'] ."</td>";
                 echo "<td>" . $row['created_at'] ."</td>";
-                echo "<td>" . '<button>View</button>' . "</td>";
+                echo "<td>" . '<button> <a href="#">View</a> </button>' . "</td>";
             echo"</tr>";
         }
         echo"<table>";
 
         // Close/Free result set
-        unset($output);
+        unset($result);
     }else{
         echo 'No records matching your query were found.';
     }
-
-}catch (PDOException $err) {
-        die('ERROR: Could not execute' .$sql . $err->getMessage());
-}
-
-unset($pdo);
