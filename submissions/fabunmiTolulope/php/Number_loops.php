ac08c7b596b8bb963c6456d35e51ed0c263c4c82
FabunmiTolulopeEniola <70230223+ToluEnny24@users.noreply.github.com>
2021-02-17 17:17:19 -0800

    Update Number_loops.php
    I have effected the necessary changes. The program now runs from 101 and 2 respectively.

--- submissions/fabunmiTolulope/php/Number_loops.php
+++ submissions/fabunmiTolulope/php/Number_loops.php
@@ -1,14 +1,14 @@
 <?php
 
 // This program will display every number between 100 and 150 using while loop
-$x = 100;
+$x = 101;
 while ($x <= 150) {
 	echo "Number: $x <br>";
 	$x++;
 }
 
 // This program will display even numbers between 0 and 50 using for loop
-for($y = 0; $y < 50; $y++){
+for($y = 2; $y < 50; $y++){
     if($y > 0 && $y % 2 === 0){
         echo "Number: $y <br>";
     };
