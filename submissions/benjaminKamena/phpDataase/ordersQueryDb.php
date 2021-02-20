7fb38d758945d72420ae198265817c7f13146ba2
kamena1994 <benjii1964@gmail.com>
2021-02-20 06:19:16 +0200

    refractore my code

--- submissions/benjaminKamena/phpDataase/ordersQueryDb.php
+++ submissions/benjaminKamena/phpDataase/ordersQueryDb.php
@@ -15,45 +15,47 @@ function view($id){
             WHERE orders.customer_id = '$id' ";
             $stmt = $pdo->query($sql);
             $orders = $stmt->fetchAll();
+
             setcookie("orders",  serialize($orders), time() + 3600 );
+
         }catch (PDOException $e) {
             die("ERROR: Could not execute $sql. " . $e->getMessage());
         }
-    };
 
     if(isset($_COOKIE['orders'])){
+
         $orders =  unserialize($_COOKIE['orders']);
     }
-    
-try{
-    if (count($orders) > 0) {
-		echo "<table>";
-		echo "<tr>";
-		echo "<td>ID</td>";
-		echo "<td>Product Name</td>";
-        echo "<td>Quantity</td>";
-        echo "<td>Unit Price</td>";
-        echo "<td>Total Price</td>";
-        echo "<td>Order Date</td>";
-        echo "<td>Customer Location</td>";
-        echo "</tr>";
-     foreach ($orders as $row){
-		echo "<tr>";
-        echo "<td>" . $row['order_items_id'] . "</td>";
-        echo "<td>" . $row['product_name'] . "</td>";
-        echo "<td>" . $row['unit_price'] . "</td>";
-        echo "<td>" . $row['order_item_quantity'] . "</td>";
-        echo "<td>" . $row['total_amount'] . "</td>";
-        echo "<td>" . $row['order_date'] . "</td>";
-        echo "<td>" . $row['customer_street_address'] . ", " . $row['customer_city'] . ", " . $row['customer_state'] . ", "  . $row['customer_country'] ."</td>";
-        echo "</tr>";
-    }
-    echo "</table>";
-    unset($orders);
-} else {
-    echo 'No match';
-};
-} catch (PDOException $e) {
+    try{
+        if (count($orders) > 0) {
+            echo "<table>";
+		    echo "<tr>";
+		    echo "<td>ID</td>";
+		    echo "<td>Product Name</td>";
+            echo "<td>Quantity</td>";
+            echo "<td>Unit Price</td>";
+            echo "<td>Total Price</td>";
+            echo "<td>Order Date</td>";
+            echo "<td>Customer Location</td>";
+            echo "</tr>";
+        foreach ($orders as $row){
+	     	echo "<tr>";
+            echo "<td>" . $row['order_items_id'] . "</td>";
+            echo "<td>" . $row['product_name'] . "</td>";
+            echo "<td>" . $row['unit_price'] . "</td>";
+            echo "<td>" . $row['order_item_quantity'] . "</td>";
+            echo "<td>" . $row['total_amount'] . "</td>";
+            echo "<td>" . $row['order_date'] . "</td>";
+            echo "<td>" . $row['customer_street_address'] . ", " . $row['customer_city'] . ", " . $row['customer_state'] . ", "  . $row['customer_country'] ."</td>";
+            echo "</tr>";
+        };
+        echo "</table>";
+        unset($orders);
+    } else {
+        echo 'No match';
+    };
+} 
+}catch (PDOException $e) {
     die("ERROR: Could not execute $sql. " . $e->getMessage());
 };
 ?>
