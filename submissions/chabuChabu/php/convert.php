4fe3fcd06a5a577013a28b1c9fa96a60b7c387ae
Chabu Chabu <chabuchabu32@gmail.com>
2021-02-25 10:56:48 +0000

    conversion file updated

--- submissions/chabuChabu/php/convert.php
+++ submissions/chabuChabu/php/convert.php
@@ -1,21 +1,15 @@
 <?php
 
-$value = $_REQUEST['value'];
-$unit = $_REQUEST['unit'];
+function performConversion($value){
+    
+    $pounds = $value * 2.2;//Assumption is that 1 Kilo is equivalent to 2.2 pounds
+    $yards = $value * 1.094;//Assumption is that 1 meter is equivalent to 1.094 pounds
+    $fahrenheit = ($value * 9/5) + 32; // fomula used is (0°C × 9/5) + 32
 
-function performConversion($value,$unit){
-    if($unit =='kilogram'){
-        $result = $value * 2.2;//Assumption is that 1 Kilo is equivalent to 2.2 pounds
-        echo"<h1>The conversion of $value kilos is equivalent to $result pounds</h1>";
-    }elseif($unit == 'meters'){
-        $result = $value * 1.094;//Assumption is that 1 meter is equivalent to 1.094 pounds
-        echo"<h1>The conversion of $value Meters is equivalent to $result yards</h1>";
-    }elseif($unit == 'celsius'){
-        $result = ($value * 9/5) + 32; // fomula used is (0°C × 9/5) + 32
-        echo"<h1>The conversion of $value celsius is equivalent to $result fahrenheit </h1>";
-    }else{
-        echo"<h1> Please enter values and choose conversion unit </h1>";
-    }
+    return [$pounds,$yards,$fahrenheit];
 }
+$result = performConversion($value = 37);
 
-performConversion($value,$unit);
+echo"<b>The conversion of ". $value ." kilos is equivalent to ". $result[0]." pounds</b><br>";
+echo"<b>The conversion of ". $value ." Meters is equivalent to ". $result[1]." yards</b><br>";
+echo"<b>The conversion of ". $value ." Celsius is equivalent to ". $result[2]." fahrenheit</b>";
