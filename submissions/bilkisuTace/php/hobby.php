e6ad31095f479ec863a93f014649a3ca6cc1d0f4
bilkisutace <tace.bilkisu@gmail.com>
2021-02-26 13:05:35 +0100

    [CSFS-94] Hobby Arrays

--- submissions/bilkisuTace/php/hobby.php
+++ submissions/bilkisuTace/php/hobby.php
@@ -3,7 +3,7 @@ $firstname = array("Shunom", "Nomsu", "Gyeshimmi", "Haruna", "Patience", "Bilkis
 $hobby = array("Reading", "Drumming", "Singing", "Coding", "Baking", "taking Pictures", "Cooking", "Traveling");
 
 for ($i=0; $i <= 7; $i++) { 
-	echo "My name is " . $firstname[$i]."." . " I love " . $hobby[$i]. ".";
+	echo "My name is $firstname[$i]. I love $hobby[$i]." ;
 	echo "<br>";
 }
 
