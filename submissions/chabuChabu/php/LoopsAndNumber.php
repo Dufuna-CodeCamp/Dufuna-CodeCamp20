fab1651fbc7f7e8fc15061a3baf422c4e7235c77
Chabu Chabu <chabuchabu32@gmail.com>
2021-03-03 20:00:36 +0000

    Loops and Numbers modified

--- submissions/chabuChabu/php/LoopsAndNumber.php
+++ submissions/chabuChabu/php/LoopsAndNumber.php
@@ -1,13 +1,9 @@
 <?php
-
 //Display every number between 100 and 150
-
-for($i = 101;$i < 150;$i++){
+for($i = 101; $i < 150; $i++){
  echo $i."<br>";
 }
-
 //Display even numbers between 0 and 50
-
 $x = 0;
 
 while($x < 50) {
