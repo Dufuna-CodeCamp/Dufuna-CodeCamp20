9f78d16dbe1652c6a77c7c7ffa0d64ac8cf1bb26
kojusola <adeola5678@gmail.com>
2021-03-08 16:00:03 +0100

    corrections1

--- submissions/adeolaFafemi/phpSlim/app/controllers/ArticlesController.php
+++ submissions/adeolaFafemi/phpSlim/app/controllers/ArticlesController.php
@@ -32,7 +32,7 @@ class ArticlesController
         $article = Article::find($id);
         if(!$article){
             $response->getBody()->write(json_encode(["error"=> "Article not found"]));
-            return $response -> withHeader('Content-Type', 'application/json')->withStatus(404);
+            return $response -> withHeader(Constants::CONTENT_TYPE_HEADER, Constants::APPLICATION_JSON)->withStatus(404);
         }
         $article->title = $data['title'];
         $article->description = $data['description'];
@@ -46,7 +46,7 @@ class ArticlesController
         $article = Article::find($id);
         if(!$article){
             $response->getBody()->write(json_encode(["error"=> "Article not found"]));
-            return $response -> withHeader('Content-Type', 'application/json')->withStatus(404);
+            return $response -> withHeader(Constants::CONTENT_TYPE_HEADER, Constants::APPLICATION_JSON)->withStatus(404);
         }
         $article->delete();
         $response->getBody()->write(json_encode(["status"=>"success",'message' => 'This Article was successfully deleted']));
@@ -59,7 +59,7 @@ class ArticlesController
         $article = Article::find($id);
         if(!$article){
             $response->getBody()->write(json_encode(["error"=> "Article not found"]));
-            return $response -> withHeader('Content-Type', 'application/json')->withStatus(404);
+            return $response -> withHeader(Constants::CONTENT_TYPE_HEADER, Constants::APPLICATION_JSON)->withStatus(404);
         }
         $article->status = 'published';
         $article->save();
@@ -71,7 +71,7 @@ class ArticlesController
         $articles = Article::where('status','=', 'published')->get();
         if(!$articles){
             $response->getBody()->write(json_encode(["error"=> "No article published"]));
-            return $response -> withHeader('Content-Type', 'application/json')->withStatus(404);
+            return $response -> withHeader(Constants::CONTENT_TYPE_HEADER, Constants::APPLICATION_JSON)->withStatus(404);
         }
         $response->getBody()->write(json_encode([ 'status'=> 'success','data' => $articles]));
         return $response -> withHeader(Constants::CONTENT_TYPE_HEADER, Constants::APPLICATION_JSON)->withStatus(200);
@@ -82,7 +82,7 @@ class ArticlesController
         $article = Article::find($id);
         if(!$article){
             $response->getBody()->write(json_encode(["error"=> "User not found"]));
-            return $response -> withHeader('Content-Type', 'application/json')->withStatus(404);
+            return $response -> withHeader(Constants::CONTENT_TYPE_HEADER, Constants::APPLICATION_JSON)->withStatus(404);
         }
         $response->getBody()->write(json_encode([ 'status'=> 'success','data' => $article]));
         return $response -> withHeader(Constants::CONTENT_TYPE_HEADER, Constants::APPLICATION_JSON)->withStatus(200);
