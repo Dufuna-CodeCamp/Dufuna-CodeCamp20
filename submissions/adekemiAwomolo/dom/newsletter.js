0df892afccbdea5335e52efb8423dd35d6196495
Adekemi Awomolo <dakemilola@gmail.com>
2021-03-15 12:02:19 +0100

    newsletterjs

---
+++ submissions/adekemiAwomolo/dom/newsletter.js
@@ -0,0 +1,26 @@
+let newsletter = document.getElementById('newsletter');
+newspaper_email = document.getElementById('newsemail');
+emailReg=/^\w+([-]?\w+)*@\w+([-]?\w+)*(\w{2,10})+$/;
+
+function validate_data(field,event){
+    if(field.value ==''){
+        field.nextElementSibling.innerHTML ="Please Enter Your Email Address",
+        event.preventDefault();
+        return false;
+    }else if(!field.value.match(emailReg)){
+        field.nextElementSibling.innerHTML = "Enter a valid email address";
+        event.preventDefault();
+        return false;
+    }else{
+        field.nextElementSibling.innerHTML = '';
+        return true;
+    }
+}
+
+function newsValidation(event){
+    validate_data(newspaper_email, event);
+    return true;
+};
+
+
+newsletter.addEventListener('submit', newsValidation);
