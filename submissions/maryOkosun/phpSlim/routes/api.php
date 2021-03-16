f794b97a64783ecea44fa3c81cc000ae441b7e20
marynoir <okosunmaryeghonghon@gmail.com>
2021-03-16 01:06:28 +0100

    added the files of the publishing company

---
+++ submissions/maryOkosun/phpSlim/routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Controller\ArticleController;
+use App\Middleware\JsonBodyParserMiddleware;
+use Slim\Routing\RouteCollectorProxy;
+
+$app->group('/api', function (RouteCollectorProxy $group) {
+   
+    // Get all Articles
+    $group->get('/articles', ArticleController::class.':index');
+    
+    // Get article by id
+    $group->get('/articles/{id}', ArticleController::class.':getArticle');
+    
+    // Create an article
+    $group->post('/articles',  ArticleController::class.':create');
+    
+    // Update an article
+    $group->put('/articles/{id}', ArticleController::class.':update')
+        ->add(new JsonBodyParserMiddleware());
+    
+    // Delete an article 
+    $group->delete('/articles/{id}', ArticleController::class .':delete');
+
+    //Publish an article
+    $group->put('/publish-articles/{published}/{id}', ArticleController::class.':publishArticle');
+
+    //Get all published articles
+    $group->get('/publish-articles/{published}', ArticleController::class . ':getPublishedArticles');
+
+});
