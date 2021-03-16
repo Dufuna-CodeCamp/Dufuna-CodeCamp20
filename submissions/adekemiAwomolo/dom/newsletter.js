0ad1e11edb81a0d9958d3c82265029d305138a2d
Adekemi Awomolo <67517383+kemysola@users.noreply.github.com>
2021-03-16 10:50:59 +0100

    Effected changes as requested.

--- submissions/adekemiAwomolo/dom/newsletter.js
+++ submissions/adekemiAwomolo/dom/newsletter.js
@@ -2,25 +2,20 @@ let newsletter = document.getElementById('newsletter');
 newspaper_email = document.getElementById('newsemail');
 emailReg=/^\w+([-]?\w+)*@\w+([-]?\w+)*(\w{2,10})+$/;
 
-function validate_data(field,event){
-    if(field.value ==''){
-        field.nextElementSibling.innerHTML ="Please Enter Your Email Address",
+function validate_data(event){
+    if(newspaper_email.value ==''){
+        newspaper_email.nextElementSibling.innerHTML ="Please Enter Your Email Address",
         event.preventDefault();
         return false;
-    }else if(!field.value.match(emailReg)){
-        field.nextElementSibling.innerHTML = "Enter a valid email address";
+    }else if(!newspaper_email.value.match(emailReg)){
+        newspaper_email.nextElementSibling.innerHTML = "Enter a valid email address";
         event.preventDefault();
         return false;
     }else{
-        field.nextElementSibling.innerHTML = '';
+        newspaper_email.nextElementSibling.innerHTML = '';
         return true;
     }
 }
 
-function newsValidation(event){
-    validate_data(newspaper_email, event);
-    return true;
-}
 
-
-newsletter.addEventListener('submit', newsValidation);
+newsletter.addEventListener('submit', validate_data);
