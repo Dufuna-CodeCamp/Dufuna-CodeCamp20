8be43a09c6c5a76311f9270c6d90696cf6cc7753
Opeyemi Peter <opeyemiadebowale1759@gmail.com>
2021-03-19 12:40:24 -0700

    adding the loop and numbers task

---
+++ submissions/opeyemiPeter/PHP/numberLoops.php
@@ -0,0 +1,13 @@
+<?php
+// number between 100 and 150
+   for ($i=101; $i < 150; $i++) { 
+       echo $i . "<br>";
+   }
+
+// even numbers between 0 and 50
+   for ($j=2; $j < 50; $j++) { 
+       if ($j % 2 == 0) {
+           echo $j . "<br>";
+       }
+   }
+?>
