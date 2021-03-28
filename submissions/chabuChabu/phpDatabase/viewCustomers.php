f46511b48a440ddafae6f439d3cf1f2f3643d01d
Chabu Chabu <chabuchabu32@gmail.com>
2021-03-28 20:59:03 +0100

    view customers

--- submissions/chabuChabu/phpDatabase/viewCustomers.php
+++ submissions/chabuChabu/phpDatabase/viewCustomers.php
@@ -11,15 +11,15 @@ if (! empty($_COOKIE[$cookieName])) {
 }
 else {
     // 3. there is no cookie, load data from database
-    $sql = "select id,full_name,email,created_at from customers;";
+    $sql = 'select id,concat(first_name," ",last_name )as full_name,email,created_at from customers';
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
     $result = $stmt->fetchAll();
-    // save in cookie
+    //4. save in cookie
     setcookie($cookieName, serialize($result),time() + 3600,"/","",0);
 }
 
-// 4. display result using $result only
+// 5. display result using $result only
 if (!empty($result)) {
     echo"<h1>List of Customers</h1>";
     echo"<table>";
@@ -41,7 +41,7 @@ if (!empty($result)) {
         echo"</tr>"; 
        }
     echo"</table>";
-    //free result
+//6. free result
     unset($result);
 } else{
     echo "No records from your query were returned";
