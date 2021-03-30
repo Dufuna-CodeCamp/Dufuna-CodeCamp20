2e032b53d4e5f0f081b3dc2971484ae02aa897c1
Adekemi Awomolo <67517383+kemysola@users.noreply.github.com>
2021-03-30 11:48:33 +0100

    Corrected the changes requested.

--- submissions/adekemiAwomolo/database/query.sql
+++ submissions/adekemiAwomolo/database/query.sql
@@ -5,7 +5,7 @@ SELECT count(*) FROM accidents WHERE passengers_survived = 1;
 SELECT count(*) FROM accidents WHERE passengers_survived = 0;
 --549
 --Name and sex of Passengers under the age of 27 that embarked Queenstown and Cherbourg
-SELECT passengers.full_name, passengers.sex FROM passengers LEFT JOIN passengers_details ON passengers.id = passengers_details.passengers_id
+SELECT passengers.fullname, passengers.sex FROM passengers LEFT JOIN passengers_details ON passengers.id = passengers_details.passengers_id
 WHERE age < 27 AND point_of_embarkation != "s";
 
 --the number of people who embarked to Southhampton and passengers_survived
@@ -14,7 +14,7 @@ WHERE point_of_embarkation = 's' AND passengers_survived > 0;
 --218
 
 --Id, name and the total number of passengers who paid  a fare greater than 100 and aged 35 wth siblings/spouses aboard
-SELECT passengers.id,passengers.full_name FROM passengers LEFT JOIN passengers_details ON passengers.id
+SELECT passengers.id,passengers.fullname FROM passengers LEFT JOIN passengers_details ON passengers.id
 =passengers_details.passengers_id WHERE trip_fare >100 AND numbers_of_siblings_and_spouses_aboard >0 AND passengers.age >35;
 --Total number of passengers who paid above 100 and had siblings and spouses aboard.
 SELECT COUNT(*) FROM passengers LEFT JOIN passengers_details ON passengers.id =passengers_details.passengers_id
