ebd3914b2c95ce13937f70d9d1f4249ee0f3114b
MayowaShoyinka <47593314+MayowaShoyinka@users.noreply.github.com>
2021-04-04 13:24:55 -1100

    Update contactValidation.js
    closed all space

--- submissions/mayowaShoyinka/domManipulation/contactValidation.js
+++ submissions/mayowaShoyinka/domManipulation/contactValidation.js
@@ -30,7 +30,6 @@ function validateMessage(field, errorOne, event){
   }
 }
 
-
 let nameRegex =  /^[a-zA-Z]{4,}(?: [a-zA-Z]+){0,2}$/,
     emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,10})+$/;
 
