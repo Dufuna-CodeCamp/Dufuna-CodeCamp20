c1ea62bf2155511ba7833197ece3e4f37da80fcd
dejonear <77005420+dejonear@users.noreply.github.com>
2021-04-14 18:14:30 +0100

    New js file

---
+++ submissions/DejoneaRoberts/DOM/newsletter.js
@@ -0,0 +1,26 @@
+let newsForm = document.getElementById('news-form');
+let emailNews = document.getElementById('email-sign');
+
+let emailRegex = "/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/";
+
+function newsValidation(field, fieldRegex, error1, error2, event) {
+
+    if(field.value === "") {
+        field.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML = error1;
+        event.preventDefault();
+    } else if (!field.value.match(fieldRegex)) {
+        field.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML = error2;
+        event.preventDefault();
+        return false;
+    } else {
+        field.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML = "";
+        return true;
+    }
+}
+
+function validationsubmitForm(event) {
+    newsValidation( emailNews, emailRegex, "Please enter your email address", "Please enter a valid email", event);
+    return true;
+}
+
+newsForm.addEventListener("submit", validationsubmitForm);
