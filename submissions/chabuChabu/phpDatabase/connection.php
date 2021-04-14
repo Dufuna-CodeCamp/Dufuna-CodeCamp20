7871b90b09ea9bc28bd828816b80f112fda18f78
Chabu Chabu <chabuchabu32@gmail.com>
2021-04-14 19:41:13 +0100

    Customer Orders

--- submissions/chabuChabu/phpDatabase/connection.php
+++ submissions/chabuChabu/phpDatabase/connection.php
@@ -1,9 +1,9 @@
 <?php
-// 1. Attempt MySQL server connection.
+/*Attempt MySQL server connection.*/
 try{
     $pdo = new PDO ("mysql:host=localhost;dbname=kwathu","root","mysql");
-    
-// 2. Set the PDO error mode to exception
+   
+/*Set the PDO error mode to exception*/
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch(PDOException $e){
