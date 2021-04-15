f208a4e9991fd67fdc5ffb359af8182f49e45225
marynoir <okosunmaryeghonghon@gmail.com>
2021-04-15 10:57:37 +0100

    updated react app

--- submissions/maryOkosun/react/src/App.js
+++ submissions/maryOkosun/react/src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react'; 
 import './App.css';
 
-function Todo({ todo, index, completeTodo, removeTodo }) {
+function Todo({ todo, index, completeTodo}) {
   return (
     <div className="todo"
-    style={{ textDecoration: todo.isCompleted ? "line-through" : "",color: todo.isCompleted ? "green" : ""}}
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "",color: todo.isCompleted ? "green" : ""}}
     >
-      
       {todo.text}
       <div>
         <label className='container'>
@@ -32,25 +31,25 @@ function TodoForm({ addTodo }) {
     <div className='todo-header'>
       <h1>TODO</h1>
       <div className='todo-create'>
-    <form onSubmit={handleSubmit}>
-      <input
-        placeholder="Create a new todo..."
-        type="text"
-        className="input"
-        value={value}
-        onChange={e => setValue(e.target.value)}
-      />
-    <div className='todo-button'>
-      <button type='submit'>+</button>
+        <form onSubmit={handleSubmit}>
+          <input
+            placeholder="Create a new todo..."
+            type="text"
+            className="input"
+            value={value}
+            onChange={e => setValue(e.target.value)}
+          />
+          <div className='todo-button'>
+            <button type='submit'>+</button>
+          </div>
+        </form>
       </div>
-      </form>
-    </div>
-   
     </div>
   );
 }
 
 function App() {
+  const [todosRemaining,setTodosRemaining] = useState(0); 
   const [todos, setTodos] = React.useState([
     { 
       text: "Learn about React" ,
@@ -74,6 +73,10 @@ function App() {
     }
   ]);
 
+  useEffect(() => {
+    setTodosRemaining(todos.filter(todo => !todo.isCompleted).length)
+  });
+
   const addTodo = text => {
     const newTodos = [...todos, { text }];
     setTodos(newTodos);
@@ -85,46 +88,37 @@ function App() {
     setTodos(newTodos);
   };
 
-  const removeTodo = index => {
-    const newTodos = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
-  };
-
   return (
     <div className="app">
       <TodoForm addTodo={addTodo} />
       <div className="todo-list">
-      
         {todos.map((todo, index) => (
           <Todo
             key={index}
             index={index}
             todo={todo}
             completeTodo={completeTodo}
-            removeTodo={removeTodo}
           />
         ))}
       </div>
       <div className='todo-footer'>
         <div className='todo-left-footer'>
-          <p>3 items left</p>
+          <p>{todosRemaining} items left</p>
         </div>
-          <div className='todo-middle-footer'>
+        <div className='todo-middle-footer'>
           <p>All items</p>
           <p>Active</p>
           <p>Completed</p>
-          </div>
-          <div className='todo-right-footer'>
+        </div>
+        <div className='todo-right-footer'>
           <p>X Clear Completed</p>
-          </div>
-          
+        </div>  
       </div>
       <div className="hide-display">
           <p>All items</p>
           <p>Active</p>
           <p>Completed</p>
-          </div>
+      </div>
     </div>
   );
 }
