31f0ca772e6cc72e2337057d94b054b541982009
Adekemi Awomolo <dakemilola@gmail.com>
2021-04-15 12:06:56 +0100

    loopphp

---
+++ submissions/adekemiAwomolo/php/loops.php
@@ -0,0 +1,22 @@
+<?php 
+
+//loop through 100 to 150
+echo "******** Result ******* <br>";
+
+for($i = 101; $i <150; $i++){
+    echo $i. "<br>";
+}
+echo"<br>";
+/*loops through even numbers*/
+echo "********* Result For Even Numbers ********* <br>";
+for($num = 2; $num < 50; $num++){
+    if($num % 2 == 0){
+        echo $num. "<br>";
+        
+        }
+        
+}
+
+
+
+?>
