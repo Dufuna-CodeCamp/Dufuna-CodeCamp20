95cb10a209d199b98f28378b6aac942373518ea3
ToluEnny24 <ogunronbitolulope@gmail.com>
2021-04-15 07:07:24 -0700

    I have effected the changes.

--- submissions/fabunmiTolulope/phpDatabase/customersOrders.php
+++ submissions/fabunmiTolulope/phpDatabase/customersOrders.php
@@ -1,27 +1,37 @@
 <?php
 
 include('connection.php');
- if( isset($_GET['id']) && strlen($_GET['id']) > 0 ) {
  $id =  $_GET['id'];
-}
 
-try{
-    $sql = "SELECT orders.id, products.name, order_items.unit_price, order_items.quantity, order_items.total_amount, order_items.created_at,
-    customers_addresses.street, customers_addresses.city, customers_addresses.state, customers_addresses.country
+ class orderList{
+    function setData($name, $value, $expire){
+        setcookie($name, serialize($value), $expire);
+    }
+    function list($id){
+        try{
+            $sql = "SELECT orders.id, products.name, order_items.unit_price, order_items.quantity, order_items.total_amount, order_items.created_at,
+            customers_addresses.street, customers_addresses.city, customers_addresses.state, customers_addresses.country
             FROM order_items 
             LEFT JOIN orders ON orders.id = order_items.order_id
             LEFT JOIN customers on orders.customer_id = customers.id
             LEFT JOIN products ON products.id = order_items.product_id 
             WHERE orders.customer_id = '$id' ";
-            $stmt = $pdo->query($sql);
-            $orders = $stmt->fetchAll();
-            setcookie("orders",  serialize($orders), time() + 3600 );
-
-
-    if( isset($_COOKIE['orders']) ) {
-       $orders =  unserialize($_COOKIE['orders']);
+            $stmt = $this->connect()->query($sql);
+            $result = $stmt->fetchAll();
+            $this->setData('orders', $result, time() + 3600);
+            return $results;
         }
+        catch(PDOException $e){
+            die("ERROR:Could not connect $sql." . $e->getMessage());
+        } 
+    }    
+}
 
+
+$query = new orderList();
+$result = isset($_COOKIE['orders']) ? unserialize($_COOKIE['orders']) : $query->list($id);
+            
+try{
     if (count($orders) > 0) {
         echo "<table>";
             echo "<tr>";
@@ -43,14 +53,18 @@ try{
             echo "<td>" . $row['created_at'] . "</td>";
             echo "<td>" . $row['street_address'] . ", " . $row['city'] . ", " . $row['state'] . ", "  . $row['country'] ."</td>";
         echo "</tr>";
-    };
-        echo "</table>";
-        unset($orders);
-} else {
+        }
+    echo "</table>";
+    unset($orders);
+    } 
+    else {
     echo 'No records matching your query were found';
+    }
 }
-
-}catch (PDOException $e) {
+catch (PDOException $e) {
     die("ERROR: Could not execute $sql. " . $e->getMessage());
-}
+    }
+
+unset($pdo);
+
 ?>
