5e53f6095b0718b58f9527397b3d21d54a5329cf
Chabu Chabu <chabuchabu32@gmail.com>
2021-05-14 13:29:28 +0100

    Article Management Endpoints

---
+++ submissions/chabuChabu/prestige/bootstrap/database.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Database\Capsule\Manager;
+
+$settings = $container->get('settings');
+$db_connection = $settings['db'];
+
+$capsule = new Illuminate\Database\Capsule\Manager;
+$capsule->addConnection($db_connection);
+
+$capsule->bootEloquent();
+$capsule->setAsGlobal();
