70b88e15ee6ceb86cd8c1346b14b9f913365c22a
Keicee32 <kelss451@gmail.com>
2021-05-18 10:01:54 +0100

    js task 1

---
+++ submissions/egekenzeKelechi/javascript/script.js
@@ -0,0 +1,19 @@
+let productList = [
+    {
+        "productName": "Samsung Galaxy s21",
+        "imageUrl": "https://",
+        "productPrice": 1500
+    },
+    {
+        "productName": "MacBook M1",
+        "imageUrl": "https://",
+        "productPrice": 2500
+    },
+    {
+        "productName": "Lamboghini Aventador",
+        "imageUrl": "https://",
+        "productPrice": 500000
+    }
+];
+
+console.log("The " + productList[0].productName + " with the link " + productList[0].imageUrl + " is sold for " + productList[0].productPrice);
