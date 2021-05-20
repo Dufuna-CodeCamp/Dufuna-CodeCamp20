85698c84a56a66bba7d50d377c85ec1c5c34c379
Keicee32 <kelss451@gmail.com>
2021-05-20 12:32:40 +0100

    second js task

---
+++ submissions/egekenzeKelechi/javascript/conditionals.js
@@ -0,0 +1,16 @@
+let result = 123 - 7;
+
+function checkMultiples(result) {
+    for (let index = 1; index < result; index++) {
+        if(index % 3 === 0 && index % 5 === 0){
+            console.log("SOFTWARE DEVELOPER ");
+        }else if(index % 5 === 0){
+            console.log("DEVELOPER ");
+        }else if(index % 3 === 0){
+            console.log("SOFTWARE ");
+        }
+        
+    }
+}
+
+checkMultiples(result);
