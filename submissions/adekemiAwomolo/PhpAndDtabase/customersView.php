545fcbb22683fc58f273c13f2c1a84170f3dacc4
Adekemi Awomolo <dakemilola@gmail.com>
2021-05-31 13:45:41 +0100

    formatted and indented

--- submissions/adekemiAwomolo/PhpAndDtabase/customersView.php
+++ submissions/adekemiAwomolo/PhpAndDtabase/customersView.php
@@ -25,42 +25,42 @@ function load() {
     }
     
 function display() {
-global $info;
-if(count($info) > 0){
-echo "<table>";
-echo "<tr>";
-echo "<th>ID</th>";
-echo "<th>Product_Name</th>";
-echo "<th>Unit_Price</th>";
-echo "<th>Quantity</th>";
-echo "<th>Total_Price</th>";
-echo "<th>Created_Date</th>";
-echo "<th>Address</th>";
-echo "</tr>";
-foreach ($info as $value){
-echo "<tr>";
-echo "<td>". $value['id']. "</td>";
-echo "<td>". $value['product_name']. "</td>";
-echo "<td>". $value['unit_price']. "</td>";
-echo "<td>". $value['quantity']."</td>";
-echo "<td>". $value['total_amount']. "</td>";
-echo "<td>". $value['created_at'] . "</td>" ;
-echo "<td>" . $value['street_address']. $value['city']. $value['state'].$value['country']. "</td>";
-echo "</tr>";
-}
-echo "</table>";
-
-unset($result);
-
-} else{
-echo "record not found";
-}
+    global $info;
+    if(count($info) > 0){
+        echo "<table>";
+        echo "<tr>";
+        echo "<th>ID</th>";
+        echo "<th>Product_Name</th>";
+        echo "<th>Unit_Price</th>";
+        echo "<th>Quantity</th>";
+        echo "<th>Total_Price</th>";
+        echo "<th>Created_Date</th>";
+        echo "<th>Address</th>";
+        echo "</tr>";
+        foreach ($info as $value){
+            echo "<tr>";
+            echo "<td>". $value['id']. "</td>";
+            echo "<td>". $value['product_name']. "</td>";
+            echo "<td>". $value['unit_price']. "</td>";
+            echo "<td>". $value['quantity']."</td>";
+            echo "<td>". $value['total_amount']. "</td>";
+            echo "<td>". $value['created_at'] . "</td>" ;
+            echo "<td>" . $value['street_address']. $value['city']. $value['state'].$value['country']. "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        unset($result);} 
+    else{  
+        echo "record not found";
+    }
 } 
+
 try {
 if ($notSet) $info = load(); //Fetch query and set cookie
 else $info = unserialize($_COOKIE["orders"]); //Fetch cookie 
 display();
-} catch(PDOException $e) {
+}
+catch(PDOException $e) {
 die ("ERROR: $sql " . $e->getMessage()) . "<br>";
 }
 
