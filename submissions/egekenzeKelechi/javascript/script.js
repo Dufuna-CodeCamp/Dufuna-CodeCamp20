3027e271876eb662b23386d7906659b5c106d545
Keicee32 <kelss451@gmail.com>
2021-06-06 13:16:48 +0100

    DOM Manipulation

--- submissions/egekenzeKelechi/javascript/script.js
+++ submissions/egekenzeKelechi/javascript/script.js
@@ -16,4 +16,99 @@ let productList = [
     }
 ];
 
-console.log("The " + productList[0].productName + " with the link " + productList[0].imageUrl + " is sold for " + productList[0].productPrice);
+// console.log("The " + productList[0].productName + " with the link " + productList[0].imageUrl + " is sold for " + productList[0].productPrice);
+
+// Product Calculation
+let productQuantity = document.querySelector("#product-qty"),
+    button = document.querySelector("#btn"),
+    price = document.querySelector(".product-price"),
+    shipping = document.querySelector(".shipping"),
+    totalCost = document.querySelector(".totalCost"),
+    error = document.querySelector(".error-message");
+
+let oldShippingPrice = shipping.innerHTML,
+    oldCost = totalCost.innerHTML,
+    oldPrice = price.innerHTML;
+
+
+window.addEventListener('load', function(){
+    if(productQuantity.value >= 1 && price.innerHTML < 1000){
+        // price.innerHTML = productQuantity.value * price.innerHTML;
+        shipping.innerHTML = 100;
+        totalCost.innerHTML = parseInt(shipping.innerHTML) + parseInt(price.innerHTML);
+    }
+
+    // if(productQuantity.value == 1 && price.innerHTML == 1000 || price.innerHTML <= 20000){
+    //     // price.innerHTML = productQuantity.value * price.innerHTML;
+    //     result = 0.15 * price.innerHTML;
+    //     shipping.innerHTML = result;
+    //     totalCost.innerHTML = parseInt(price.innerHTML) + parseInt(shipping.innerHTML);
+    // }
+
+    // if(productQuantity.value == 1 && price.innerHTML >= 20000 && price.innerHTML <= 100000){
+    //     // price.innerHTML = productQuantity.value * price.innerHTML;
+    //     result = 0.20 * price.innerHTML;
+    //     shipping.innerHTML = result;
+    //     totalCost.innerHTML = parseInt(price.innerHTML) + parseInt(shipping.innerHTML);
+    // }
+
+    // if(productQuantity.value == 1 &&  price.innerHTML > 100000){
+    //     // price.innerHTML = productQuantity.value * price.innerHTML;
+    //     shipping.innerHTML = 25000;
+    //     totalCost.innerHTML = parseInt(price.innerHTML) + parseInt(shipping.innerHTML);
+    // }
+})
+
+
+productQuantity.addEventListener('keyup', function(){
+
+    if(productQuantity.value < 1 || isNaN(productQuantity.value)){
+        error.innerHTML = "Please enter a valid quantity";
+        button.disabled = true;
+        button.style.backgroundColor = "grey";
+        shipping.innerHTML = "";
+        totalCost.innerHTML = "";
+
+    }else if(price.innerHTML <= 1000  && productQuantity.value >= 1){
+        price.innerHTML = productQuantity.value * price.innerHTML;
+        shipping.innerHTML = oldShippingPrice;
+        totalCost.innerHTML = parseInt(price.innerHTML) + parseInt(shipping.innerHTML);
+        error.innerHTML = "";
+        button.disabled = false;
+        button.style.backgroundColor = null;
+        price.innerHTML = oldPrice;
+
+    }else if(price.innerHTML >= 1000 && price.innerHTML <= 20000 && productQuantity.value >= 1){
+        price.innerHTML = productQuantity.value * price.innerHTML;
+        result = 0.15 * price.innerHTML;
+        shipping.innerHTML = result;
+        totalCost.innerHTML = parseInt(price.innerHTML) + parseInt(shipping.innerHTML);
+        error.innerHTML = "";
+        button.disabled = false;
+        button.style.backgroundColor = null;
+        price.innerHTML = oldPrice;
+
+    }else if(price.innerHTML >= 20000 && price.innerHTML <= 100000 && productQuantity.value >= 1){
+        price.innerHTML = productQuantity.value * price.innerHTML;
+        result = 0.20 * price.innerHTML;
+        shipping.innerHTML = result;
+        totalCost.innerHTML = parseInt(price.innerHTML) + parseInt(shipping.innerHTML);
+        error.innerHTML = "";
+        button.disabled = false;
+        button.style.backgroundColor = null;
+        price.innerHTML = oldPrice;
+        // return oldPrice;
+        
+    }else{
+        error.innerHTML = "";
+        button.disabled = false;
+        button.style.backgroundColor = null;
+        shipping.innerHTML = "";
+        totalCost.innerHTML = "";
+        // price.innerHTML = oldPrice;
+
+        // return oldPrice;
+    }
+
+    
+});
