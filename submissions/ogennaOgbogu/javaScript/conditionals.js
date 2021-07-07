51cccb332d6d3b6e8d3d2e9648d490d458289a51
oge-nna <ogennaogbogu@gmail.com>
2021-07-07 21:23:42 +0100

    minor changes to js file

--- submissions/ogennaOgbogu/javaScript/conditionals.js
+++ submissions/ogennaOgbogu/javaScript/conditionals.js
@@ -1,12 +1,12 @@
 var number = 123 - 7;
 
 for (var number = 1; number <= 123 - 7; number++) {
-    if (number % 3 == 0) {
-        console.log("Software");
+    if ((number % 5 == 0) && (number % 3 == 0)) {
+        console.log("Software Developer");
     } else if (number % 5 == 0) {
         console.log("Developer");
-    } else if (number % 5 == 0 && number % 3 == 0) {
-        console.log("Software Developer");
+    } else if (number % 3 == 0) {
+        console.log("Software");
     } else (console.log(number));
 }
 
