34936b6ce5835155c48cc40b84d6a5fc8bdb50a2
Faith <faithakinbobola2@gmail.com>
2022-06-20 12:43:03 +0100

    Todo Application

---
+++ submissions/akinbobolaFaith/react/to-do-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ToDo from './components/ToDo';
+
+function App() {
+  return (
+    <div className="container">
+      <ToDo />
+    </div>
+  );
+}
+
+export default App;
---
+++ submissions/akinbobolaFaith/react/to-do-app/src/components/ToDoItems.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const ToDoItems = (props) => {
+    return(
+        <div className="lists">
+            {props.entries.map((item) => {
+                return (
+                    <ul className="the-list">
+                        <li 
+                            key={item.key}
+                            style={{textDecoration: props.elementKeys.includes(String(item.key)) && 'line-through', 
+                            color: props.elementKeys.includes(String(item.key)) && 'green'}}
+                        >
+                        {item.text}
+                        <input
+                            id={item.key}
+                            type={props.inputType}
+                            onClick={props.handleClick}
+                            value={item.key}
+                            // handleFilter={props.handleFilter}
+                        />
+                        <label htmlFor={item.key}></label>
+                        </li>
+                    </ul>
+                );
+            })}
+        </div>
+    )
+}
+
+
+export default ToDoItems;
